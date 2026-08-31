cfbb0e364474677791b9233e1f6d43670ea9cff6
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:09 +0800

    adding "s00b/ai_s00b_01_03.py" on 31/Aug/2026

--- s00b/ai_s00b_01_03.py
+++ s00b/ai_s00b_01_03.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:14:40 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:19:26 (UT+08:00) daisuke>
 #
 
-# two numbers
-a, b = 19, 2
+# main function
+def main ():
+    # two numbers
+    a, b = 19, 2
 
-# calculation
-c = a / b
+    # calculation
+    c = a / b
 
-# printing result of calculation
-print (f'a = {a}')
-print (f'b = {b}')
-print (f'c = a / b = {a} / {b} = {c}')
+    # printing result of calculation
+    print (f'a = {a}')
+    print (f'b = {b}')
+    print (f'c = a / b = {a} / {b} = {c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
