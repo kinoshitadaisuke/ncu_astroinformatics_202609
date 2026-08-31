a0ece2f5a16b9b14c67238bd8e0e0b11ed1db4e9
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:36 +0800

    adding "s00b/ai_s00b_04_03.py" on 31/Aug/2026

--- s00b/ai_s00b_04_03.py
+++ s00b/ai_s00b_04_03.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:19:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:30:37 (UT+08:00) daisuke>
 #
 
-# making a dictionary
-star_mag = {
-    "Sirius":          -1.46,
-    "Canopus":         -0.74,
-    "Rigil Kentaurus": -0.27,
-    "Arcturus":        -0.05,
-    "Vega":            +0.03,
-    "Capella":         +0.08,
-    "Rigel":           +0.13,
-    "Procyon":         +0.34,
-    "Achernar":        +0.46,
-    "Betelgeuse":      +0.50,
-}
+# main function
+def main ():
+    # making a dictionary
+    star_mag = {
+        "Sirius":          -1.46,
+        "Canopus":         -0.74,
+        "Rigil Kentaurus": -0.27,
+        "Arcturus":        -0.05,
+        "Vega":            +0.03,
+        "Capella":         +0.08,
+        "Rigel":           +0.13,
+        "Procyon":         +0.34,
+        "Achernar":        +0.46,
+        "Betelgeuse":      +0.50,
+    }
 
-# type of "star_mag"
-print ("type of star_mag:", type (star_mag) )
+    # type of "star_mag"
+    print ("type of star_mag:", type (star_mag) )
 
-# printing dictionary "star_mag"
-print (star_mag)
+    # printing dictionary "star_mag"
+    print (star_mag)
 
-# accessing to an element
-print ("magnitude of Vega =", star_mag['Vega'])
+    # accessing to an element
+    print ("magnitude of Vega =", star_mag['Vega'])
 
-# printing all the data
-for star in sorted (star_mag.keys ()):
-    print (f'{star:16} : {star_mag[star]:+5.2f}')
+    # printing all the data
+    for star in sorted (star_mag.keys ()):
+        print (f'{star:16} : {star_mag[star]:+5.2f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
