62b9bf8d4dd0be889cafe17f0b77cb542f1b1380
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:13 +0800

    adding "s00b/ai_s00b_02_08.py" on 31/Aug/2026

--- s00b/ai_s00b_02_08.py
+++ s00b/ai_s00b_02_08.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:18:13 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:26:53 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# initialisation of a variable "a"
-a_deg = 0.0
+# main function
+def main ():
+    # initialisation of a variable "a"
+    a_deg = 0.0
 
-# calculating sin (0 deg), sin (15 deg), sin (30 deg), ..., sin (180 deg)
-while (True):
-    # stop "while" loop if "a_deg" is greater than 180.0 deg
-    if (a_deg > 180.0):
-        break
-    # converting from degree into radian
-    a_rad = math.radians (a_deg)
-    # calculation of sine
-    sin_a = math.sin (a_rad)
-    # printing result of calculation
-    print (f'sin ({a_deg:5.1f} deg) = {sin_a:8.6f}')
-    # incrementing variable "a"
-    a_deg += 15.0
+    # calculating sin (0 deg), sin (15 deg), sin (30 deg), ..., sin (180 deg)
+    while (True):
+        # stop "while" loop if "a_deg" is greater than 180.0 deg
+        if (a_deg > 180.0):
+            break
+        # converting from degree into radian
+        a_rad = math.radians (a_deg)
+        # calculation of sine
+        sin_a = math.sin (a_rad)
+        # printing result of calculation
+        print (f'sin ({a_deg:5.1f} deg) = {sin_a:8.6f}')
+        # incrementing variable "a"
+        a_deg += 15.0
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
