984f9ab64b2c25379a1bff9a2c0959bab2a1ce05
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:08:09 +0800

    adding "s00b/ai_s00b_02_03.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_02_03.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:17:43 (UT+08:00) daisuke>
+#
+
+# initialisation of a variable "total"
+total = 0
+
+# data
+list_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# calculating 1 + 2 + 3 + ... + 10 using "for" statement
+for i in list_data:
+    # adding "i" to "total"
+    total = total + i
+
+# printing result of calculation
+print (f'1 + 2 + 3 + ... + 8 + 9 + 10 = {total}')
