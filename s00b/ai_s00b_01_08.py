94cc0c4e046e0618a892494d7d87e44aa5e13c2d
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:25 +0800

    adding "s00b/ai_s00b_01_08.py" on 31/Aug/2026

--- s00b/ai_s00b_01_08.py
+++ s00b/ai_s00b_01_08.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:15:25 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:21:21 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# a number
-a = 2.7183
+# main function
+def main ():
+    # a number
+    a = 2.7183
 
-# calculation of natural logarithm
-b = math.log (a)
+    # calculation of natural logarithm
+    b = math.log (a)
 
-# printing result of calculation
-print (f'a = {a}')
-print (f'b = log (a) = log ({a}) = {b}')
+    # printing result of calculation
+    print (f'a = {a}')
+    print (f'b = log (a) = log ({a}) = {b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
