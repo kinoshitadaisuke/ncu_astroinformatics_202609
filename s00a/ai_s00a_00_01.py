f5a3a80be3ba19a32d767f2edf40d45c09372729
Daisuke <mhdf23888@gmail.com>
2026-08-31 17:27:15 +0800

    adding "s00a/ai_s00a_00_01.py" on 31/Aug/2026

---
+++ s00a/ai_s00a_00_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/08/31 17:17:56 (UT+08:00) daisuke>
+#
+
+# main function
+def main ():
+    # calculation
+    a = 3
+    b = 5
+    c = a + b
+
+    # printing result of calculation
+    print (f'{a} + {b} = {c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
