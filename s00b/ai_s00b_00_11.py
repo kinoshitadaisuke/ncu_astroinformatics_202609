0a622c01baea74c4da1378a77bb93975a7e914f2
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:06:52 +0800

    adding "s00b/ai_s00b_00_11.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_00_11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:13:20 (UT+08:00) daisuke>
+#
+
+# assignment of a value to a variable
+pi = 3.141592653589793
+
+# fancy formatting using formatted string literals
+print (f'pi = {pi:8.6f}')
