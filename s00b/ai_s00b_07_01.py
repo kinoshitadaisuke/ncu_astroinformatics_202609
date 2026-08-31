622bf070556278a5731260d794a3fc7d93a0f26c
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:09:14 +0800

    adding "s00b/ai_s00b_07_01.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_07_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:20:29 (UT+08:00) daisuke>
+#
+
+# two numbers
+a = 3
+
+# calculation
+c = a / b
+
+# printing result of calculation
+print (f'{a} / {b} = {c}')
