19736d9dd8cee8d6131aa8ef0897ff07561daa46
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:29 +0800

    adding "s00b/ai_s00b_04_01.py" on 31/Aug/2026

--- s00b/ai_s00b_04_01.py
+++ s00b/ai_s00b_04_01.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:19:06 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:29:27 (UT+08:00) daisuke>
 #
 
-# initialisation of a tuple
-planet = ( 'Mercury', 'Venus', 'Earth', 'Mars', \
-           'Jupiter', 'Saturn', 'Uranus', 'Neptune' )
+# main function
+def main ():
+    # initialisation of a tuple
+    planet = ( 'Mercury', 'Venus', 'Earth', 'Mars', \
+               'Jupiter', 'Saturn', 'Uranus', 'Neptune' )
 
-# type of "planet"
-print ("type of planet:", type (planet) )
+    # type of "planet"
+    print ("type of planet:", type (planet) )
 
-# printing the tuple "tuple_a"
-print ("planet:\n", planet)
+    # printing the tuple "tuple_a"
+    print ("planet:\n", planet)
 
-# counting number of elements in the tuple "planet"
-n = len (planet)
+    # counting number of elements in the tuple "planet"
+    n = len (planet)
 
-# printing number of elements in the tuple "planet"
-print ("len (planet) =", n)
+    # printing number of elements in the tuple "planet"
+    print ("len (planet) =", n)
 
-# accessing to an element using index
-print ("planet[2]    =", planet[2])
-print ("planet[7]    =", planet[7])
-print ("planet[-3]   =", planet[-3])
+    # accessing to an element using index
+    print ("planet[2]    =", planet[2])
+    print ("planet[7]    =", planet[7])
+    print ("planet[-3]   =", planet[-3])
 
-# accessing to elements using slicing
-print ("planet[2:5]  =", planet[2:5])
-print ("planet[:4]   =", planet[:4])
-print ("planet[6:]   =", planet[6:])
+    # accessing to elements using slicing
+    print ("planet[2:5]  =", planet[2:5])
+    print ("planet[:4]   =", planet[:4])
+    print ("planet[6:]   =", planet[6:])
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
