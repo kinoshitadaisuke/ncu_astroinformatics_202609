7919dcda3432e341ff8b869a2d596e76d9ef8a32
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:06:34 +0800

    adding "s00b/ai_s00b_00_06.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_00_06.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:12:37 (UT+08:00) daisuke>
+#
+
+# assignment of a value to a variable
+a = 1.23 * 10**12
+
+# printing the value of a variable using built-in function "print ()"
+# and printf-style string formatting
+print ('a = %g' % a)
