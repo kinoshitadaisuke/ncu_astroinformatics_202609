3752e8f8fe14638cb57b9e5a5fbb499e42450ffc
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:46 +0800

    adding "s00b/ai_s00b_05_01.py" on 31/Aug/2026

--- s00b/ai_s00b_05_01.py
+++ s00b/ai_s00b_05_01.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:19:49 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:34:06 (UT+08:00) daisuke>
 #
 
-# start and end numbers
-n_start = 2
-n_end   = 100
+# main function
+def main ():
+    # start and end numbers
+    n_start = 2
+    n_end   = 100
 
-# output file name
-file_output = "primenumbers.data"
+    # output file name
+    file_output = "primenumbers.data"
 
-# opening file for writing
-with open (file_output, 'w') as fh:
-    # checking numbers from n_start to n_end
-    for i in range (n_start, n_end + 1):
-        # resetting parameter "count"
-        count = 0
-        # examining if the number is divisible by numbers between 2 and (i-1)
-        for j in range (2, i):
-            # if the number is divisible, then adding 1 to "count"
-            if (i % j == 0):
-                count += 1
-                # if count is >= 1, the number is not a prime number
-                break
-        if (count == 0):
-            # if the number is not divisible by numbers between 2 and (i-1),
-            # then it is a prime number
-            result = f'{i} is a prime number.\n'
-        else:
-            # if the number is divisible by at least one of numbers between
-            # 2 and (i-1), then it is not a prime number
-            result = f'{i} is NOT a prime number.\n'
-        # writing result into output file
-        fh.write (result)
+    # opening file for writing
+    with open (file_output, 'w') as fh:
+        # checking numbers from n_start to n_end
+        for i in range (n_start, n_end + 1):
+            # resetting parameter "count"
+            count = 0
+            # examining if the number is divisible by numbers between 2 and (i-1)
+            for j in range (2, i):
+                # if the number is divisible, then adding 1 to "count"
+                if (i % j == 0):
+                    count += 1
+                    # if count is >= 1, the number is not a prime number
+                    break
+            if (count == 0):
+                # if the number is not divisible by numbers between 2 and (i-1),
+                # then it is a prime number
+                result = f'{i} is a prime number.\n'
+            else:
+                # if the number is divisible by at least one of numbers between
+                # 2 and (i-1), then it is not a prime number
+                result = f'{i} is NOT a prime number.\n'
+            # writing result into output file
+            fh.write (result)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
