f365094eb0a89bf87942764f5e2c916e9a4040ec
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:08:39 +0800

    adding "s00b/ai_s00b_03_01.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_03_01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:18:46 (UT+08:00) daisuke>
+#
+
+# defining a function to add two numbers
+def add_two_numbers (a, b):
+    # adding two numbers
+    c = a + b
+    # returning result of calculation
+    return (c)
+
+# two numbers
+n1 = 23
+n2 = 47
+
+# using the function "add_two_numbers"
+n3 = add_two_numbers (n1, n2)
+
+# printing result
+print (f'n1 = {n1}')
+print (f'n2 = {n2}')
+print (f'n3 = n1 + n2 = {n3}')
