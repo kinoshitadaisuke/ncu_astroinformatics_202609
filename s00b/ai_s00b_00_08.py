1aea08114a27038bdb6fc2d1166da08283edd11e
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:06:41 +0800

    adding "s00b/ai_s00b_00_08.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_00_08.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:12:54 (UT+08:00) daisuke>
+#
+
+# assignment of a value to a variable
+a = 3 + 4j
+
+# printing the value of a variable using built-in function "print ()"
+print ('a = %f + %fi' % (a.real, a.imag))
