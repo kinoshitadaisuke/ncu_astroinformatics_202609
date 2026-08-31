f68e948f031e90cd5623f891ee2e30ded8258762
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:50:19 +0800

    adding "s00b/ai_s00b_00_03.py" on 31/Aug/2026

--- s00b/ai_s00b_00_03.py
+++ s00b/ai_s00b_00_03.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:12:10 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:14:02 (UT+08:00) daisuke>
 #
 
-# assignment of a value to a variable
-a = 123
+# main function
+def main ():
+    # assignment of a value to a variable
+    a = 123
 
-# printing the value of a variable using built-in function "print ()"
-print ('a =', a)
+    # printing the value of a variable using built-in function "print ()"
+    print ('a =', a)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
