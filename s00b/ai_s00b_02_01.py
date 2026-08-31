5bc55fd195721fe780e2ce337e8462dea16f64b4
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:49 +0800

    adding "s00b/ai_s00b_02_01.py" on 31/Aug/2026

--- s00b/ai_s00b_02_01.py
+++ s00b/ai_s00b_02_01.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:17:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:24:18 (UT+08:00) daisuke>
 #
 
 # importing sys module
 import sys
 
-# reading an integer number from keyboard typing
-a_str = input ('Type one integer number: ')
+# main function
+def main ():
+    # reading an integer number from keyboard typing
+    a_str = input ('Type one integer number: ')
 
-# converting a string into integer
-try:
-    a = int (a_str)
-except:
-    print (f'ERROR: failed to convert string "{a_str}" into integer!')
-    sys.exit (1)
+    # converting a string into integer
+    try:
+        a = int (a_str)
+    except:
+        print (f'ERROR: failed to convert string "{a_str}" into integer!')
+        sys.exit (1)
 
-# if and else statements
-if (a > 0):
-    print ("The number you type is a positive number.")
-elif (a < 0):
-    print ("The number you type is a negative number.")
-else:
-    print ("The number you type is zero.")
+    # if and else statements
+    if (a > 0):
+        print ("The number you type is a positive number.")
+    elif (a < 0):
+        print ("The number you type is a negative number.")
+    else:
+        print ("The number you type is zero.")
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
