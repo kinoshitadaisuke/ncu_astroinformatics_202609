b77c7341562096788bcd518d13f80ab0a4368ec0
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:38 +0800

    adding "s00b/ai_s00b_01_12.py" on 31/Aug/2026

--- s00b/ai_s00b_01_12.py
+++ s00b/ai_s00b_01_12.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:15:58 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:22:53 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# value of pi
-pi = math.pi
+# main function
+def main ():
+    # value of pi
+    pi = math.pi
 
-# angle in degree
-a_deg = 60.0
+    # angle in degree
+    a_deg = 60.0
 
-# angle in radian
-a_rad = a_deg / 180.0 * pi
+    # angle in radian
+    a_rad = a_deg / 180.0 * pi
 
-# calculation of sine
-tan_a = math.tan (a_rad)
+    # calculation of sine
+    tan_a = math.tan (a_rad)
 
-# printing result of calculation
-print (f'pi          = {pi}')
-print (f'a_deg       = {a_deg} deg')
-print (f'a_rad       = {a_rad} rad')
-print (f'tan (a_rad) = tan ({a_rad}) = {tan_a}')
+    # printing result of calculation
+    print (f'pi          = {pi}')
+    print (f'a_deg       = {a_deg} deg')
+    print (f'a_rad       = {a_rad} rad')
+    print (f'tan (a_rad) = tan ({a_rad}) = {tan_a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
