2212583b0a1eb02b931c1e01d780aef82e67b3e0
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:07:33 +0800

    adding "s00b/ai_s00b_01_07.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_01_07.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:15:16 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = -1.0
+
+# calculation of exponential function
+b = math.exp (a)
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = exp (a) = {math.e}^{a} = {b}')
