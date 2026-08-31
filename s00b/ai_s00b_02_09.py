be46c3fc94eb41d592409ac951b50643daeef11c
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:16 +0800

    adding "s00b/ai_s00b_02_09.py" on 31/Aug/2026

--- s00b/ai_s00b_02_09.py
+++ s00b/ai_s00b_02_09.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:18:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:27:14 (UT+08:00) daisuke>
 #
 
-# printing odd number between 0 and 30
-for i in range (30):
-    # if number is divisible by 2, then skipping to next number
-    if (i % 2 == 0):
-        continue
-    # if not, then it is an odd number
-    print (f'{i} is an odd number.')
+# main function
+def main ():
+    # printing odd number between 0 and 30
+    for i in range (30):
+        # if number is divisible by 2, then skipping to next number
+        if (i % 2 == 0):
+            continue
+        # if not, then it is an odd number
+        print (f'{i} is an odd number.')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
