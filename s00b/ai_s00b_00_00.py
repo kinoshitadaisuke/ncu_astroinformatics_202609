2095994482d8db3a753b265b96913784ca07fc02
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:50:09 +0800

    adding "s00b/ai_s00b_00_00.py" on 31/Aug/2026

--- s00b/ai_s00b_00_00.py
+++ s00b/ai_s00b_00_00.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:11:37 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:12:43 (UT+08:00) daisuke>
 #
 
-# printing a sentence using built-in function "print ()"
-print ('I love astrophysics! I love Python!')
+# main function
+def main ():
+    # printing a sentence using built-in function "print ()"
+    print ('I love astrophysics! I love Python!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
