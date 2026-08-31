1420b6082bbae59ebee957f640ae3cad253aac67
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:50:55 +0800

    adding "s00b/ai_s00b_00_14.py" on 31/Aug/2026

--- s00b/ai_s00b_00_14.py
+++ s00b/ai_s00b_00_14.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:13:50 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:17:57 (UT+08:00) daisuke>
 #
 
-# assignment of variables
-c  = 299792458
-au = 149597870700
-pc = 3.08567758 * 10**16
+# main function
+def main ():
+    # assignment of variables
+    c  = 299792458
+    au = 149597870700
+    pc = 3.08567758 * 10**16
 
-# fancy formatting using .format () method
-print ('c    = {:g} [m/s]\n1 au = {:g} [m]\n1 pc = {:g} [m]'.format (c, au, pc))
+    # fancy formatting using .format () method
+    print ('c    = {:g} [m/s]\n1 au = {:g} [m]\n1 pc = {:g} [m]'.format (c, au, pc))
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
