16eced5d8fc837f430b98b5df3152a54f11c8a99
Daisuke <mhdf23888@gmail.com>
2026-08-31 17:27:12 +0800

    adding "s00a/ai_s00a_00_00.py" on 31/Aug/2026

---
+++ s00a/ai_s00a_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/08/31 17:16:58 (UT+08:00) daisuke>
+#
+
+# main function
+def main ():
+    # printing a sentence
+    print ("This is my very first Python script!")
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
