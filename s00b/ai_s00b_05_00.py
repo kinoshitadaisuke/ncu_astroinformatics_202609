165d59c8d08e07bbfdc6903472b4dde3c29c9474
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:43 +0800

    adding "s00b/ai_s00b_05_00.py" on 31/Aug/2026

--- s00b/ai_s00b_05_00.py
+++ s00b/ai_s00b_05_00.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:19:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:31:47 (UT+08:00) daisuke>
 #
 
-# name of data file
-file_data = 'fruits.data'
+# main function
+def main ():
+    # name of data file
+    file_data = 'fruits.data'
 
-# a variable for total price
-total = 0
+    # a variable for total price
+    total = 0
 
-# opening file with read mode
-with open (file_data, 'r') as fh:
-    # reading file line-by-line
-    for line in fh:
-        # if line starts with '#', then it is a header line and skip
-        if (line[0] == '#'):
-            continue
-        # splitting line
-        (name, unit_price_str, quantity_str) = line.split ()
-        # converting from string into integer
-        unit_price = int (unit_price_str)
-        quantity   = int (quantity_str)
-        # calculation of sub-total
-        subtotal = unit_price * quantity
-        # adding "subtotal" to "total"
-        total += subtotal
-        # printing information
-        print (f'{name:10} {unit_price:3d} {quantity:2d} {subtotal:3d}')
+    # opening file with read mode
+    with open (file_data, 'r') as fh:
+        # reading file line-by-line
+        for line in fh:
+            # if line starts with '#', then it is a header line and skip
+            if (line[0] == '#'):
+                continue
+            # splitting line
+            (name, unit_price_str, quantity_str) = line.split ()
+            # converting from string into integer
+            unit_price = int (unit_price_str)
+            quantity   = int (quantity_str)
+            # calculation of sub-total
+            subtotal = unit_price * quantity
+            # adding "subtotal" to "total"
+            total += subtotal
+            # printing information
+            print (f'{name:10} {unit_price:3d} {quantity:2d} {subtotal:3d}')
 
-# printing total price
-print (f'\ntotal price: {total:4d}\n')
+    # printing total price
+    print (f'\ntotal price: {total:4d}\n')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
