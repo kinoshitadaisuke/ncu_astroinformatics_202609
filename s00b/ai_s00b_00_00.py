755845435015042d9176c97f3443440a52d1dd99
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:06:13 +0800

    adding "s00b/ai_s00b_00_00.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_00_00.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:11:37 (UT+08:00) daisuke>
+#
+
+# printing a sentence using built-in function "print ()"
+print ('I love astrophysics! I love Python!')
