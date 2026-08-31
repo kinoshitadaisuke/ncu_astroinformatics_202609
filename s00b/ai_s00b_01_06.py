ed99a122a740e1281a3dba1d035b7a7d1a27e7bc
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:07:29 +0800

    adding "s00b/ai_s00b_01_06.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_01_06.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:15:08 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+# a number
+a = 3.0
+
+# calculation of square root
+b = math.sqrt (a)
+c = a**0.5
+
+# printing result of calculation
+print (f'a = {a}')
+print (f'b = math.sqrt ({a}) = {b}')
+print (f'c = a^0.5 = {a}^0.5 = {c}')
