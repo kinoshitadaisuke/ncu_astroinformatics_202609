750445e03e33bcc62e95811a597130f4df6e085e
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:06:44 +0800

    adding "s00b/ai_s00b_00_09.py" on 31/Aug/2026

---
+++ s00b/ai_s00b_00_09.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/21 11:13:02 (UT+08:00) daisuke>
+#
+
+# assignment of values to variables
+a = 1.2
+b = 3.4
+
+# calculation
+c = a * b
+
+# printing values of variables using built-in function "print ()"
+# and printf-style string formatting
+print ('%f * %f = %f' % (a, b, c) )
