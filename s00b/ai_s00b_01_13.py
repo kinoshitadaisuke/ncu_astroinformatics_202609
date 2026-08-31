65edb3fe2eee7b28c728b33a04a86df96256f972
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:42 +0800

    adding "s00b/ai_s00b_01_13.py" on 31/Aug/2026

--- s00b/ai_s00b_01_13.py
+++ s00b/ai_s00b_01_13.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:17:04 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:23:16 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# (x, y) coordinate
-x, y = +1.0, -1.0
+# main function
+def main ():
+    # (x, y) coordinate
+    x, y = +1.0, -1.0
 
-# calculation of arctangent
-a_rad = math.atan2 (y, x)
+    # calculation of arctangent
+    a_rad = math.atan2 (y, x)
 
-# conversion from radian into degree
-a_deg = math.degrees (a_rad)
+    # conversion from radian into degree
+    a_deg = math.degrees (a_rad)
 
-# printing result of calculation
-print (f'x            = {x}')
-print (f'y            = {y}')
-print (f'atan2 (y, x) = {a_rad} rad = {a_deg} deg')
+    # printing result of calculation
+    print (f'x            = {x}')
+    print (f'y            = {y}')
+    print (f'atan2 (y, x) = {a_rad} rad = {a_deg} deg')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
