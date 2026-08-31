ace58c592390a9e1c6f25ff554512bc386da7070
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:03 +0800

    adding "s00b/ai_s00b_02_05.py" on 31/Aug/2026

--- s00b/ai_s00b_02_05.py
+++ s00b/ai_s00b_02_05.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:17:57 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:25:46 (UT+08:00) daisuke>
 #
 
-# initialisation of a variable "total"
-total = 0
+# main function
+def main ():
+    # initialisation of a variable "total"
+    total = 0
 
-# calculating 1 + 2 + 3 + ... + 10 using "for" statement
-for i in range (1, 11, 1):
-    # adding "i" to "total"
-    total += i
+    # calculating 1 + 2 + 3 + ... + 10 using "for" statement
+    for i in range (1, 11, 1):
+        # adding "i" to "total"
+        total += i
 
-# printing result of calculation
-print (f'1 + 2 + 3 + ... + 8 + 9 + 10 = {total}')
+    # printing result of calculation
+    print (f'1 + 2 + 3 + ... + 8 + 9 + 10 = {total}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
