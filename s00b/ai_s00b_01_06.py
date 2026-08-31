2d125c8767858ef9ae78dc2aac74e2d0591f5656
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:18 +0800

    adding "s00b/ai_s00b_01_06.py" on 31/Aug/2026

--- s00b/ai_s00b_01_06.py
+++ s00b/ai_s00b_01_06.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:15:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:20:32 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# a number
-a = 3.0
+# main function
+def main ():
+    # a number
+    a = 3.0
 
-# calculation of square root
-b = math.sqrt (a)
-c = a**0.5
+    # calculation of square root
+    b = math.sqrt (a)
+    c = a**0.5
 
-# printing result of calculation
-print (f'a = {a}')
-print (f'b = math.sqrt ({a}) = {b}')
-print (f'c = a^0.5 = {a}^0.5 = {c}')
+    # printing result of calculation
+    print (f'a = {a}')
+    print (f'b = math.sqrt ({a}) = {b}')
+    print (f'c = a^0.5 = {a}^0.5 = {c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
