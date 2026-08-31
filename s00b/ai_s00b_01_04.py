660c80df565158bcd5607f9d5524d0099408ec04
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:51:12 +0800

    adding "s00b/ai_s00b_01_04.py" on 31/Aug/2026

--- s00b/ai_s00b_01_04.py
+++ s00b/ai_s00b_01_04.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:14:48 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:19:49 (UT+08:00) daisuke>
 #
 
-# two numbers
-a, b = 23, 7
+# main function
+def main ():
+    # two numbers
+    a, b = 23, 7
 
-# calculation
-quotient  = a // b
-remainder = a % b
+    # calculation
+    quotient  = a // b
+    remainder = a % b
 
-# printing result of calculation
-print (f'a         = {a}')
-print (f'b         = {b}')
-print (f'quotient  = {quotient}')
-print (f'remainder = {remainder}')
-print (f'{b} * {quotient} + {remainder} = {b * quotient + remainder}')
+    # printing result of calculation
+    print (f'a         = {a}')
+    print (f'b         = {b}')
+    print (f'quotient  = {quotient}')
+    print (f'remainder = {remainder}')
+    print (f'{b} * {quotient} + {remainder} = {b * quotient + remainder}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
