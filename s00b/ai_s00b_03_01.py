ccd4303056ae621044668fa475f818dd1aaa2db4
Daisuke <mhdf23888@gmail.com>
2026-08-31 19:52:23 +0800

    adding "s00b/ai_s00b_03_01.py" on 31/Aug/2026

--- s00b/ai_s00b_03_01.py
+++ s00b/ai_s00b_03_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/21 11:18:46 (UT+08:00) daisuke>
+# Time-stamp: <2026/08/31 19:28:16 (UT+08:00) daisuke>
 #
 
 # defining a function to add two numbers
@@ -11,14 +11,20 @@ def add_two_numbers (a, b):
     # returning result of calculation
     return (c)
 
-# two numbers
-n1 = 23
-n2 = 47
+# main function
+def main ():
+    # two numbers
+    n1 = 23
+    n2 = 47
 
-# using the function "add_two_numbers"
-n3 = add_two_numbers (n1, n2)
+    # using the function "add_two_numbers"
+    n3 = add_two_numbers (n1, n2)
 
-# printing result
-print (f'n1 = {n1}')
-print (f'n2 = {n2}')
-print (f'n3 = n1 + n2 = {n3}')
+    # printing result
+    print (f'n1 = {n1}')
+    print (f'n2 = {n2}')
+    print (f'n3 = n1 + n2 = {n3}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
