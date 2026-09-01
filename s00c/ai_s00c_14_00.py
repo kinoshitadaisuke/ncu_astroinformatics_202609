7c09eb6f5e315b6f47217f442133724dc0253b87
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:28 +0800

    adding "s00c/ai_s00c_14_00.py" on  1/Sep/2026

--- s00c/ai_s00c_14_00.py
+++ s00c/ai_s00c_14_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:05:59 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:59:12 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,33 +10,39 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of Yale Bright Star Catalogue
-url_bsc = 'https://cdsarc.cds.unistra.fr/ftp/V/50/catalog.gz'
+    # URL of Yale Bright Star Catalogue
+    url_bsc = 'https://cdsarc.cds.unistra.fr/ftp/V/50/catalog.gz'
 
-# output file name
-file_bsc = 'catalog.gz'
+    # output file name
+    file_bsc = 'catalog.gz'
 
-# printing status
-print (f'Now, opening {url_bsc}...')
+    # printing status
+    print (f'Now, opening {url_bsc}...')
 
-# opening URL
-with urllib.request.urlopen (url_bsc) as fh_read:
-    # reading data
-    data_byte = fh_read.read ()
+    # opening URL
+    with urllib.request.urlopen (url_bsc) as fh_read:
+        # reading data
+        data_byte = fh_read.read ()
 
-# printing status
-print (f'Retrieved data from {url_bsc}!')
-    
-# printing status
-print (f'Now, writing data to file "{file_bsc}"...')
+    # printing status
+    print (f'Retrieved data from {url_bsc}!')
 
-# opening file for writing
-with open (file_bsc, 'wb') as fh_write:
-    # writing data
-    fh_write.write (data_byte)
+    # printing status
+    print (f'Now, writing data to file "{file_bsc}"...')
 
-# printing status
-print (f'Finished writing data to file "{file_bsc}"!')
+    # opening file for writing
+    with open (file_bsc, 'wb') as fh_write:
+        # writing data
+        fh_write.write (data_byte)
+
+    # printing status
+    print (f'Finished writing data to file "{file_bsc}"!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
