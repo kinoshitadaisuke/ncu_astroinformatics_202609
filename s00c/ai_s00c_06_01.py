697cc4cb7a98684810d597b72ba228c30369d1f9
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:08:31 +0800

    adding "s00c/ai_s00c_06_01.py" on  1/Sep/2026

--- s00c/ai_s00c_06_01.py
+++ s00c/ai_s00c_06_01.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:03:48 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:49:47 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
 import subprocess
 
-# command to be executed
-command = 'uname -srm'
+# main function
+def main ():
+    # command to be executed
+    command = 'uname -srm'
 
-# executing a command "uname" and capturing output
-result = subprocess.run (command, shell=True, capture_output=True)
+    # executing a command "uname" and capturing output
+    result = subprocess.run (command, shell=True, capture_output=True)
 
-# stdout of command execution
-output = result.stdout.decode ('utf-8')
+    # stdout of command execution
+    output = result.stdout.decode ('utf-8')
 
-# printing result of command execution
-print (f'Outputs from the execution of command "{command}":')
-print (f'{output}')
+    # printing result of command execution
+    print (f'Outputs from the execution of command "{command}":')
+    print (f'{output}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
