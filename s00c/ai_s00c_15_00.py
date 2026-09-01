dbb4d8a6cc86bec21f585d556995275e8586646e
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:57 +0800

    adding "s00c/ai_s00c_15_00.py" on  1/Sep/2026

--- s00c/ai_s00c_15_00.py
+++ s00c/ai_s00c_15_00.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:05 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:01:33 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-#
-# some constants
-#
+# main function
+def main ():
+    #
+    # some constants
+    #
 
-# pi
-pi = math.pi
-print (f'pi   = {pi}')
+    # pi
+    pi = math.pi
+    print (f'pi   = {pi}')
 
-# tau
-tau = math.tau
-print (f'tau  = 2.0 * pi\n     = {tau}')
+    # tau
+    tau = math.tau
+    print (f'tau  = 2.0 * pi\n     = {tau}')
 
-# e
-e = math.e
-print (f'e    = {e}')
+    # e
+    e = math.e
+    print (f'e    = {e}')
 
-# positive infinity
-pinf = math.inf
-print (f'+inf = {pinf}')
+    # positive infinity
+    pinf = math.inf
+    print (f'+inf = {pinf}')
 
-# negative infinity
-ninf = -math.inf
-print (f'-inf = {ninf}')
+    # negative infinity
+    ninf = -math.inf
+    print (f'-inf = {ninf}')
 
-# NaN (not a number)
-nan = math.nan
-print (f'NaN  = {nan}')
+    # NaN (not a number)
+    nan = math.nan
+    print (f'NaN  = {nan}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
