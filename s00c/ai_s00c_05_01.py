7304c312676ffb526383e0fc33e63a8b63588da2
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:08:23 +0800

    adding "s00c/ai_s00c_05_01.py" on  1/Sep/2026

--- s00c/ai_s00c_05_01.py
+++ s00c/ai_s00c_05_01.py
@@ -1,44 +1,50 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/03 11:12:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:49:00 (UT+08:00) daisuke>
 #
 
 # importing shutil module
 import shutil
 
-# finding the location of executable "sh"
-location_sh = shutil.which ('sh')
+# main function
+def main ():
+    # finding the location of executable "sh"
+    location_sh = shutil.which ('sh')
 
-# printing location of executable "sh"
-print (f'location of command "sh"         = "{location_sh}"')
+    # printing location of executable "sh"
+    print (f'location of command "sh"         = "{location_sh}"')
 
-# finding the location of executable "cp"
-location_cp = shutil.which ('cp')
+    # finding the location of executable "cp"
+    location_cp = shutil.which ('cp')
 
-# printing location of executable "cp"
-print (f'location of command "cp"         = "{location_cp}"')
+    # printing location of executable "cp"
+    print (f'location of command "cp"         = "{location_cp}"')
 
-# finding the location of executable "gcc"
-location_gcc = shutil.which ('gcc')
+    # finding the location of executable "gcc"
+    location_gcc = shutil.which ('gcc')
 
-# printing location of executable "gcc"
-print (f'location of command "gcc"        = "{location_gcc}"')
+    # printing location of executable "gcc"
+    print (f'location of command "gcc"        = "{location_gcc}"')
 
-# finding the location of executable "xterm"
-location_xterm = shutil.which ('xterm')
+    # finding the location of executable "xterm"
+    location_xterm = shutil.which ('xterm')
 
-# printing location of executable "xterm"
-print (f'location of command "xterm"      = "{location_xterm}"')
+    # printing location of executable "xterm"
+    print (f'location of command "xterm"      = "{location_xterm}"')
 
-# finding the location of executable "emacs"
-location_emacs = shutil.which ('emacs')
+    # finding the location of executable "emacs"
+    location_emacs = shutil.which ('emacs')
 
-# printing location of executable "emacs"
-print (f'location of command "emacs"      = "{location_emacs}"')
+    # printing location of executable "emacs"
+    print (f'location of command "emacs"      = "{location_emacs}"')
 
-# finding the location of executable "python3.14"
-location_python314 = shutil.which ('python3.14')
+    # finding the location of executable "python3.14"
+    location_python314 = shutil.which ('python3.14')
 
-# printing location of executable "python3.14"
-print (f'location of command "python3.14" = "{location_python314}"')
+    # printing location of executable "python3.14"
+    print (f'location of command "python3.14" = "{location_python314}"')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
