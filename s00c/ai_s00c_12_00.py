8a2d4e4fed4c21524c51407ada6ecf746531347f
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:02 +0800

    adding "s00c/ai_s00c_12_00.py" on  1/Sep/2026

--- s00c/ai_s00c_12_00.py
+++ s00c/ai_s00c_12_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/02 21:52:49 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 12:01:24 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,21 +10,27 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of data file
-url_tarfile = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/files.tar'
+    # URL of data file
+    url_tarfile = 'https://s3b.astro.ncu.edu.tw/ai_202609/data/files.tar'
 
-# output data file
-file_tarfile = 'files.tar'
+    # output data file
+    file_tarfile = 'files.tar'
 
-# opening URL
-with urllib.request.urlopen (url_tarfile) as fh_in:
-    # reading data
-    data_tarfile = fh_in.read ()
+    # opening URL
+    with urllib.request.urlopen (url_tarfile) as fh_in:
+        # reading data
+        data_tarfile = fh_in.read ()
 
-# opening file for writing
-with open (file_tarfile, 'wb') as fh_out:
-    # writing data into file
-    fh_out.write (data_tarfile)
+    # opening file for writing
+    with open (file_tarfile, 'wb') as fh_out:
+        # writing data into file
+        fh_out.write (data_tarfile)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
