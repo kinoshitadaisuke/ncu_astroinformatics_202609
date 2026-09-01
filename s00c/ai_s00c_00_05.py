540ec3e14705272e562eccaf437e8771bd275538
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:28:51 +0800

    adding "s00c/ai_s00c_00_05.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_00_05.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 15:59:41 (UT+08:00) daisuke>
+#
+
+# importing os module
+import os
+
+# target file
+file_target = '/etc/fstab'
+
+# file information
+stat = os.stat (file_target)
+
+# printing file information
+print (f'status of file "{file_target}":')
+print (f'  file owner UID  = {stat.st_uid}')
+print (f'  file owner GID  = {stat.st_gid}')
+print (f'  file mode       = {oct (stat.st_mode)}')
+print (f'  file size       = {stat.st_size} byte')
+print (f'  number of links = {stat.st_nlink}')
