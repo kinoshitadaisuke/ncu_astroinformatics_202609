fec2ffda7583f98d4098deb13b257e14509e26ff
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:15 +0800

    adding "s00c/ai_s00c_15_04.py" on  1/Sep/2026

--- s00c/ai_s00c_15_04.py
+++ s00c/ai_s00c_15_04.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:40 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:06:29 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# two floats "a" and "b"
-a = 12.34
-b = -56.78
+# main function
+def main ():
+    # two floats "a" and "b"
+    a = 12.34
+    b = -56.78
 
-# modf
-(a_fractional, a_integer) = math.modf (a)
-(b_fractional, b_integer) = math.modf (b)
+    # modf
+    (a_fractional, a_integer) = math.modf (a)
+    (b_fractional, b_integer) = math.modf (b)
 
-# printing results
-print (f'Use of modf () function:')
-print (f'  a                    = {a}')
-print (f'  integer part of a    = {a_integer}')
-print (f'  fractional part of a = {a_fractional}')
-print (f'  b                    = {b}')
-print (f'  integer part of b    = {b_integer}')
-print (f'  fractional part of b = {b_fractional}')
-print (f'')
+    # printing results
+    print (f'Use of modf () function:')
+    print (f'  a                    = {a}')
+    print (f'  integer part of a    = {a_integer}')
+    print (f'  fractional part of a = {a_fractional}')
+    print (f'  b                    = {b}')
+    print (f'  integer part of b    = {b_integer}')
+    print (f'  fractional part of b = {b_fractional}')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
