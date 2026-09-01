7a54943d2f7ddc79d9e48816670b9c281128eb22
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:51 +0800

    adding "s00c/ai_s00c_22_05.py" on  1/Sep/2026

--- s00c/ai_s00c_22_05.py
+++ s00c/ai_s00c_22_05.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:13:48 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:17:17 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# variable x
-x = sympy.symbols ('x')
+# main function
+def main ():
+    # variable x
+    x = sympy.symbols ('x')
 
-# function f
-f = sympy.sin (x)
+    # function f
+    f = sympy.sin (x)
 
-# differentiation of f(x)
-df_dx = sympy.diff (f, x)
+    # differentiation of f(x)
+    df_dx = sympy.diff (f, x)
 
-# printing result
-print (f'f(x)  = {f}')
-print (f'df/dx = {df_dx}')
+    # printing result
+    print (f'f(x)  = {f}')
+    print (f'df/dx = {df_dx}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
