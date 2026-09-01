e385798dd45b65cd9c62227211beb31cf2d9c9da
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:12:47 +0800

    adding "s00c/ai_s00c_18_02.py" on  1/Sep/2026

--- s00c/ai_s00c_18_02.py
+++ s00c/ai_s00c_18_02.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:11:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:12:53 (UT+08:00) daisuke>
 #
 
 # importing decimal module
 import decimal
 
-# calculation of sqrt (2.0)
-a = decimal.Decimal ('2.0')
-b = a.sqrt ()
+# main function
+def main ():
+    # calculation of sqrt (2.0)
+    a = decimal.Decimal ('2.0')
+    b = a.sqrt ()
 
-# result of calculation
-print (f'a = {a}')
-print (f'b = sqrt (a) = sqrt ({a}) = {b}')
+    # result of calculation
+    print (f'a = {a}')
+    print (f'b = sqrt (a) = sqrt ({a}) = {b}')
 
-# calculation of log10 (12.3)
-c = decimal.Decimal ('12.3')
-d = c.log10 ()
+    # calculation of log10 (12.3)
+    c = decimal.Decimal ('12.3')
+    d = c.log10 ()
 
-# result of calculation
-print (f'c = {c}')
-print (f'd = log10 (c) = log10 ({c}) = {d}')
+    # result of calculation
+    print (f'c = {c}')
+    print (f'd = log10 (c) = log10 ({c}) = {d}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
