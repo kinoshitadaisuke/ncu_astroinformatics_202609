2ce6b18fa39a7d5173af00edf1cbf71e75ac4c16
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:53 +0800

    adding "s00c/ai_s00c_11_02.py" on  1/Sep/2026

--- s00c/ai_s00c_11_02.py
+++ s00c/ai_s00c_11_02.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/01 18:35:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:56:49 (UT+08:00) daisuke>
 #
 
 # importing gzip module
 import gzip
 
-# compressed data file
-file_numbers = 'numbers.data.gz'
+# main function
+def main ():
+    # compressed data file
+    file_numbers = 'numbers.data.gz'
 
-# output data file
-file_output = 'numbers.data'
+    # output data file
+    file_output = 'numbers.data'
 
-# opening compressed file for reading
-with gzip.open (file_numbers, 'rb') as fh_in:
-    # reading file
-    data_byte = fh_in.read ()
+    # opening compressed file for reading
+    with gzip.open (file_numbers, 'rb') as fh_in:
+        # reading file
+        data_byte = fh_in.read ()
 
-# decoding raw byte data into UTF-8 text
-data_utf8 = data_byte.decode ('utf-8')
+    # decoding raw byte data into UTF-8 text
+    data_utf8 = data_byte.decode ('utf-8')
 
-# opening file for writing
-with open (file_output, 'w') as fh_out:
-    # writing data into file
-    fh_out.write (data_utf8)
+    # opening file for writing
+    with open (file_output, 'w') as fh_out:
+        # writing data into file
+        fh_out.write (data_utf8)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
