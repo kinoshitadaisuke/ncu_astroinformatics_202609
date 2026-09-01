91fca5a16d0dfb11b0c6843d2a86a9c13c50302c
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:49 +0800

    adding "s00c/ai_s00c_11_01.py" on  1/Sep/2026

--- s00c/ai_s00c_11_01.py
+++ s00c/ai_s00c_11_01.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/01 17:47:24 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:56:31 (UT+08:00) daisuke>
 #
 
 # importing gzip module
 import gzip
 
-# compressed data file
-file_numbers = 'numbers.data.gz'
+# main function
+def main ():
+    # compressed data file
+    file_numbers = 'numbers.data.gz'
 
-# opening compressed file for reading
-with gzip.open (file_numbers, 'rb') as fh_in:
-    # reading file
-    data_byte = fh_in.read ()
+    # opening compressed file for reading
+    with gzip.open (file_numbers, 'rb') as fh_in:
+        # reading file
+        data_byte = fh_in.read ()
 
-# decoding raw byte data into UTF-8 text
-data_utf8 = data_byte.decode ('utf-8')
+    # decoding raw byte data into UTF-8 text
+    data_utf8 = data_byte.decode ('utf-8')
 
-# printing data
-print (data_utf8)
+    # printing data
+    print (data_utf8)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
