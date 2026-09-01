276292930f68376da69ed1be17e71b6e75454736
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:58 +0800

    adding "s00c/ai_s00c_04_00.py" on  1/Sep/2026

--- s00c/ai_s00c_04_00.py
+++ s00c/ai_s00c_04_00.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:02:15 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:46:54 (UT+08:00) daisuke>
 #
 
 # importing pathlib module
 import pathlib
 
-# file name
-file_hosts = '/etc/hosts'
+# main function
+def main ():
+    # file name
+    file_hosts = '/etc/hosts'
 
-# making a pathlib object
-path_hosts = pathlib.Path (file_hosts)
+    # making a pathlib object
+    path_hosts = pathlib.Path (file_hosts)
 
-# existence check
-if (path_hosts.exists ()):
-    print (f'The file "{path_hosts}" exists!')
-else:
-    print (f'The file "{path_hosts}" DOES NOT exist!')
+    # existence check
+    if (path_hosts.exists ()):
+        print (f'The file "{path_hosts}" exists!')
+    else:
+        print (f'The file "{path_hosts}" DOES NOT exist!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
