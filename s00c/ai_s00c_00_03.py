24a170cee0927c8d0bc91365c3578c9c08b6bd1a
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:06:48 +0800

    adding "s00c/ai_s00c_00_03.py" on  1/Sep/2026

--- s00c/ai_s00c_00_03.py
+++ s00c/ai_s00c_00_03.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 15:59:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:26:11 (UT+08:00) daisuke>
 #
 
 # importing os module
 import os
 
-# target directory
-dir_target = '/bin'
+# main function
+def main ():
+    # target directory
+    dir_target = '/bin'
 
-# obtaining a list of files and directories at the directory "dir_target"
-list_files = os.listdir (path=dir_target)
+    # obtaining a list of files and directories at the directory "dir_target"
+    list_files = os.listdir (path=dir_target)
 
-# printing files and directories
-print (f'list of files and directories at "{dir_target}":')
-# for each file (or directory) in the list
-for filename in list_files:
-    # printing name of file (or directory)
-    print (f'  {filename}')
+    # printing files and directories
+    print (f'list of files and directories at "{dir_target}":')
+    # for each file (or directory) in the list
+    for filename in list_files:
+        # printing name of file (or directory)
+        print (f'  {filename}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
