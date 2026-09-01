06116d4c3e62a192520e85343b14df23449e41e4
Daisuke <mhdf23888@gmail.com>
2026-09-01 14:29:57 +0800

    adding "s00d/ai_s00d_07_01.py" on  1/Sep/2026

---
+++ s00d/ai_s00d_07_01.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/04 16:25:46 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making Numpy array
+a = numpy.linspace (0.0, 10.0, 11)
+
+# printing "a"
+print (f'a:\n{a}')
+
+# trying a.copy ()
+b = a.copy ()
+
+# printing "b"
+print (f'b:\n{b}')
+
+# IDs of "a" and "b"
+print (f'id (a) = {id (a)}')
+print (f'id (b) = {id (b)}')
+
+# changing "a[5]"
+a[5] += 10
+
+# printing "a"
+print (f'a:\n{a}')
+
+# printing "b"
+print (f'b:\n{b}')
+
+# IDs of "a" and "b"
+print (f'id (a) = {id (a)}')
+print (f'id (b) = {id (b)}')
