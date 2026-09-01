579d4c432c30251045767891307efec5746f21ef
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:03 +0800

    adding "s00c/ai_s00c_19_01.py" on  1/Sep/2026

--- s00c/ai_s00c_19_01.py
+++ s00c/ai_s00c_19_01.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:12:03 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:14:16 (UT+08:00) daisuke>
 #
 
 # importing uncertainties module
 import uncertainties
 
-# quantity "a": 8.0 +/- 0.8
-a = uncertainties.ufloat (8.0, 0.8)
+# main function
+def main ():
+    # quantity "a": 8.0 +/- 0.8
+    a = uncertainties.ufloat (8.0, 0.8)
 
-# quantity "b": 4.0 +/- 0.6
-b = uncertainties.ufloat (4.0, 0.6)
+    # quantity "b": 4.0 +/- 0.6
+    b = uncertainties.ufloat (4.0, 0.6)
 
-# calculation of a / b
-c = a / b
+    # calculation of a / b
+    c = a / b
 
-# printing value of "c"
-print (f'a = {a}')
-print (f'b = {b}')
-print (f'c = a / b = {a} / {b} = {c}')
+    # printing value of "c"
+    print (f'a = {a}')
+    print (f'b = {b}')
+    print (f'c = a / b = {a} / {b} = {c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
