37775e4800ddb1fb46c1dc931633cfc65667399e
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:53 +0800

    adding "s00c/ai_s00c_15_13.py" on  1/Sep/2026

--- s00c/ai_s00c_15_13.py
+++ s00c/ai_s00c_15_13.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:09:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:09:03 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# log () function
-c = math.log (100)
+# main function
+def main ():
+    # log () function
+    c = math.log (100)
 
-# printing result
-print (f'log (100)   = {c}')
+    # printing result
+    print (f'log (100)   = {c}')
 
-# log () function
-d = math.log10 (100)
+    # log () function
+    d = math.log10 (100)
 
-# printing result
-print (f'log10 (100) = {d}')
+    # printing result
+    print (f'log10 (100) = {d}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
