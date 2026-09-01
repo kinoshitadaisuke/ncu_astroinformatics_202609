0018709c7b9d68487722e3263130a386a0f58434
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:06:39 +0800

    adding "s00c/ai_s00c_00_01.py" on  1/Sep/2026

--- s00c/ai_s00c_00_01.py
+++ s00c/ai_s00c_00_01.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 15:59:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:25:27 (UT+08:00) daisuke>
 #
 
 # importing os module
 import os
 
-# knowing where I am now
-cwd = os.getcwd ()
+# main function
+def main ():
+    # knowing where I am now
+    cwd = os.getcwd ()
 
-# printing where I am now
-print (f'currently working directory = "{cwd}"')
+    # printing where I am now
+    print (f'currently working directory = "{cwd}"')
 
-# target directory
-dir_target = '/etc'
+    # target directory
+    dir_target = '/etc'
 
-# printing status
-print (f'now, changing directory to "{dir_target}"...')
+    # printing status
+    print (f'now, changing directory to "{dir_target}"...')
 
-# changing directory
-os.chdir (dir_target)
+    # changing directory
+    os.chdir (dir_target)
 
-# printing status
-print (f'finished changing directory to "{dir_target}"!')
+    # printing status
+    print (f'finished changing directory to "{dir_target}"!')
 
-# knowing where I am now
-cwd = os.getcwd ()
+    # knowing where I am now
+    cwd = os.getcwd ()
 
-# printing where I am now
-print (f'currently working directory = "{cwd}"')
+    # printing where I am now
+    print (f'currently working directory = "{cwd}"')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
