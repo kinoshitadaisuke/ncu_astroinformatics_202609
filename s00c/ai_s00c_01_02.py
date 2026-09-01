7c57733c9595b085e8ea5e0697342404a62fc3f1
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:29:16 +0800

    adding "s00c/ai_s00c_01_02.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_01_02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:00:58 (UT+08:00) daisuke>
+#
+
+# importing sys module
+import sys
+
+# receiving command-line arguments
+args = sys.argv
+
+# number of command-line arguments
+n_args = len (args)
+
+# printing number of command-line arguments
+print (f'number of command-line arguments = {n_args}')
+
+# printing command-line arguments
+print (f'arguments = {args}')
