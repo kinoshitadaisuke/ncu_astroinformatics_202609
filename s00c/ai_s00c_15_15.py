119bccba0e5184203352b3a76c90fbc7e31eab73
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:12:02 +0800

    adding "s00c/ai_s00c_15_15.py" on  1/Sep/2026

--- s00c/ai_s00c_15_15.py
+++ s00c/ai_s00c_15_15.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:09:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:09:35 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# conversion from radian into degree
-f_rad = math.pi / 2.0
-f_deg = math.degrees (f_rad)
+# main function
+def main ():
+    # conversion from radian into degree
+    f_rad = math.pi / 2.0
+    f_deg = math.degrees (f_rad)
 
-# printing result
-print (f'{f_rad} rad = {f_deg} deg')
+    # printing result
+    print (f'{f_rad} rad = {f_deg} deg')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
