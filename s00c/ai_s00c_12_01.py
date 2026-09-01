cc93811950bb667c09bee94f924d414dea7fafc4
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:06 +0800

    adding "s00c/ai_s00c_12_01.py" on  1/Sep/2026

--- s00c/ai_s00c_12_01.py
+++ s00c/ai_s00c_12_01.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/02 22:12:29 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:57:43 (UT+08:00) daisuke>
 #
 
 # importing tarfile module
 import tarfile
 
-# tar file
-file_tar = 'files.tar'
+# main function
+def main ():
+    # tar file
+    file_tar = 'files.tar'
 
-# opening file for reading
-with tarfile.open (file_tar, 'r:*') as fh:
-    # getting file names in tar file
-    list_files = fh.getnames ()
+    # opening file for reading
+    with tarfile.open (file_tar, 'r:*') as fh:
+        # getting file names in tar file
+        list_files = fh.getnames ()
 
-# for each file name in list
-for filename in list_files:
-    # printing file name
-    print (filename)
+    # for each file name in list
+    for filename in list_files:
+        # printing file name
+        print (filename)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
