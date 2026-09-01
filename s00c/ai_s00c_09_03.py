63b6d5d9eedf208710e4e3c0419b65e98b15937b
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:15 +0800

    adding "s00c/ai_s00c_09_03.py" on  1/Sep/2026

--- s00c/ai_s00c_09_03.py
+++ s00c/ai_s00c_09_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 21:19:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:59:00 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,21 +10,27 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of data file
-url_planets = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/planets_solsys.data'
+    # URL of data file
+    url_planets = 'https://s3b.astro.ncu.edu.tw/ai_202609/data/planets_solsys.data'
 
-# output data file
-file_planets = 'planets_solsys.data'
+    # output data file
+    file_planets = 'planets_solsys.data'
 
-# opening URL
-with urllib.request.urlopen (url_planets) as fh_in:
-    # reading data
-    data_planets = fh_in.read ()
+    # opening URL
+    with urllib.request.urlopen (url_planets) as fh_in:
+        # reading data
+        data_planets = fh_in.read ()
 
-# opening file for writing
-with open (file_planets, 'w') as fh_out:
-    # writing data into file
-    fh_out.write (data_planets.decode ('utf-8'))
+    # opening file for writing
+    with open (file_planets, 'w') as fh_out:
+        # writing data into file
+        fh_out.write (data_planets.decode ('utf-8'))
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
