4aa380fbd1020b51ed26cd85e8e2d45cc8cec098
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:11 +0800

    adding "s00c/ai_s00c_09_02.py" on  1/Sep/2026

--- s00c/ai_s00c_09_02.py
+++ s00c/ai_s00c_09_02.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 21:18:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:53:01 (UT+08:00) daisuke>
 #
 
 # importing csv module
 import csv
 
-# CSV file name
-file_csv = 'stars.csv'
+# main function
+def main ():
+    # CSV file name
+    file_csv = 'stars.csv'
 
-# opening file for reading
-with open (file_csv, 'r') as fh:
-    data_elements = csv.DictReader (fh)
-    # printing data
-    for line in data_elements:
-        print (f'{line["spectral type"]}')
-        print (f'  temperature [K]               : {line["temperature [K]"]}')
-        print (f'  absolute magnitude            : {line["absolute magnitude"]}')
-        print (f'  luminosity [solar luminosity] : {line["luminosity [solar luminosity]"]}')
+    # opening file for reading
+    with open (file_csv, 'r') as fh:
+        data_elements = csv.DictReader (fh)
+        # printing data
+        for line in data_elements:
+            print (f'{line["spectral type"]}')
+            print (f'  temperature [K]               : {line["temperature [K]"]}')
+            print (f'  absolute magnitude            : {line["absolute magnitude"]}')
+            print (f'  luminosity [solar luminosity] : {line["luminosity [solar luminosity]"]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
