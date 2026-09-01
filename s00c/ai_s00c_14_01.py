daff698e0abde787f2eb1d5d832a58b02b1a61d6
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:32 +0800

    adding "s00c/ai_s00c_14_01.py" on  1/Sep/2026

--- s00c/ai_s00c_14_01.py
+++ s00c/ai_s00c_14_01.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:06:06 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:59:32 (UT+08:00) daisuke>
 #
 
 # importing pathlib module
 import pathlib
 
-# file of Yale Bright Star Catalogue
-file_bsc = 'catalog.gz'
+# main function
+def main ():
+    # file of Yale Bright Star Catalogue
+    file_bsc = 'catalog.gz'
 
-# making pathlib object
-path_bsc = pathlib.Path (file_bsc)
+    # making pathlib object
+    path_bsc = pathlib.Path (file_bsc)
 
-# existence check of file
-if (path_bsc.exists ()):
-    print (f'File "{file_bsc}" exists.')
-    print (f'Downloading of Yale Bright Star Catalogue was successfully done!')
-else:
-    print (f'File "{file_bsc}" DOES NOT exist.')
-    print (f'Download Yale Bright Star Catalogue!')
+    # existence check of file
+    if (path_bsc.exists ()):
+        print (f'File "{file_bsc}" exists.')
+        print (f'Downloading of Yale Bright Star Catalogue was successfully done!')
+    else:
+        print (f'File "{file_bsc}" DOES NOT exist.')
+        print (f'Download Yale Bright Star Catalogue!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
