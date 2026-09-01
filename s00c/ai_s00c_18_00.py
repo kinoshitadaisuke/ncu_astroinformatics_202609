470f559a07739bb1732f71faa0bfe8cf5756f163
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:12:39 +0800

    adding "s00c/ai_s00c_18_00.py" on  1/Sep/2026

--- s00c/ai_s00c_18_00.py
+++ s00c/ai_s00c_18_00.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:11:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:12:21 (UT+08:00) daisuke>
 #
 
-# two floating point numbers "a" and "b"
-a = 1.2
-b = 2.4
+# main function
+def main ():
+    # two floating point numbers "a" and "b"
+    a = 1.2
+    b = 2.4
 
-# calculation of c = a + b
-c = a + b
+    # calculation of c = a + b
+    c = a + b
 
-# printing result of calculation
-print (f'{a} + {b}             = {c}')
+    # printing result of calculation
+    print (f'{a} + {b}             = {c}')
 
-# the other calculations
-d = 1.1
-e = 1.1
-f = 1.1
-g = d + e
-h = d + e + f
-i = d + e + f - 3.3
-print (f'{d} + {e}             = {g}')
-print (f'{d} + {e} + {f}       = {h}')
-print (f'{d} + {e} + {f} - 3.3 = {i}')
+    # the other calculations
+    d = 1.1
+    e = 1.1
+    f = 1.1
+    g = d + e
+    h = d + e + f
+    i = d + e + f - 3.3
+    print (f'{d} + {e}             = {g}')
+    print (f'{d} + {e} + {f}       = {h}')
+    print (f'{d} + {e} + {f} - 3.3 = {i}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
