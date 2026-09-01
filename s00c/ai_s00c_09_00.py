7aa3b581b54334919a48b41d91c4b16c45553868
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:30:33 +0800

    adding "s00c/ai_s00c_09_00.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_09_00.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 21:17:51 (UT+08:00) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_stars = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/stars.csv'
+
+# output data file
+file_stars = 'stars.csv'
+
+# opening URL
+with urllib.request.urlopen (url_stars) as fh_in:
+    # reading data
+    data_stars = fh_in.read ()
+
+# opening file for writing
+with open (file_stars, 'w') as fh_out:
+    # writing data into file
+    fh_out.write (data_stars.decode ('utf-8'))
