09ff955297a9bbad0cad4b357bc72ceacf11fa6d
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:07 +0800

    adding "s00c/ai_s00c_20_00.py" on  1/Sep/2026

--- s00c/ai_s00c_20_00.py
+++ s00c/ai_s00c_20_00.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:12:27 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:14:35 (UT+08:00) daisuke>
 #
 
 # importing pint module
 import pint
 
-# units
-ur       = pint.UnitRegistry ()
-u_deg    = ur.degree
-u_rad    = ur.radian
-u_arcmin = ur.arcmin
-u_arcsec = ur.arcsec
-u_mas    = ur.mas
+# main function
+def main ():
+    # units
+    ur       = pint.UnitRegistry ()
+    u_deg    = ur.degree
+    u_rad    = ur.radian
+    u_arcmin = ur.arcmin
+    u_arcsec = ur.arcsec
+    u_mas    = ur.mas
 
-# angle a in deg
-a_deg = 180.0 * u_deg
+    # angle a in deg
+    a_deg = 180.0 * u_deg
 
-# converting from deg to rad
-a_rad = a_deg.to (u_rad)
+    # converting from deg to rad
+    a_rad = a_deg.to (u_rad)
 
-# printing result
-print (f'a = {a_deg}')
-print (f'  = {a_rad}')
+    # printing result
+    print (f'a = {a_deg}')
+    print (f'  = {a_rad}')
 
-# angle b
-b_deg    = 1.0 * u_deg
-b_arcmin = b_deg.to (u_arcmin)
-b_arcsec = b_deg.to (u_arcsec)
-b_mas    = b_deg.to (u_mas)
+    # angle b
+    b_deg    = 1.0 * u_deg
+    b_arcmin = b_deg.to (u_arcmin)
+    b_arcsec = b_deg.to (u_arcsec)
+    b_mas    = b_deg.to (u_mas)
 
-# printing results
-print (f'b = {b_deg}')
-print (f'  = {b_arcmin}')
-print (f'  = {b_arcsec}')
-print (f'  = {b_mas}')
+    # printing results
+    print (f'b = {b_deg}')
+    print (f'  = {b_arcmin}')
+    print (f'  = {b_arcsec}')
+    print (f'  = {b_mas}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
