36fec8430a0ee852f007d6f0628c15dfd67270a3
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:01 +0800

    adding "s00c/ai_s00c_00_06.py" on  1/Sep/2026

--- s00c/ai_s00c_00_06.py
+++ s00c/ai_s00c_00_06.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 15:59:51 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:29:08 (UT+08:00) daisuke>
 #
 
 # importing os module
 import os
 
-# obtaining the value of environmental variable "LANG"
-env_lang = os.environ['LANG']
+# main function
+def main ():
+    # obtaining the value of environmental variable "LANG"
+    env_lang = os.environ['LANG']
 
-# printing the value of environmental variable "LANG"
-print (f'LANG = {env_lang}')
+    # printing the value of environmental variable "LANG"
+    print (f'LANG = {env_lang}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
