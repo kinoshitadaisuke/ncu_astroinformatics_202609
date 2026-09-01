725f0a9d0f32519b92c060a51ba55b6df9b0c88f
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:58 +0800

    adding "s00c/ai_s00c_11_03.py" on  1/Sep/2026

--- s00c/ai_s00c_11_03.py
+++ s00c/ai_s00c_11_03.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/01 18:34:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:57:05 (UT+08:00) daisuke>
 #
 
 # importing gzip module
 import gzip
 
-# data file
-file_data = 'numbers.data'
+# main function
+def main ():
+    # data file
+    file_data = 'numbers.data'
 
-# output file
-file_output = 'numbers_2.data.gz'
+    # output file
+    file_output = 'numbers_2.data.gz'
 
-# opening file for reading
-with open (file_data, 'r') as fh_in:
-    # reading file
-    data_str = fh_in.read ()
+    # opening file for reading
+    with open (file_data, 'r') as fh_in:
+        # reading file
+        data_str = fh_in.read ()
 
-# converting string into raw byte
-data_byte = data_str.encode ('utf-8')
+    # converting string into raw byte
+    data_byte = data_str.encode ('utf-8')
 
-# opening file for writing
-with gzip.open (file_output, 'wb') as fh_out:
-    # writing compressed data into file
-    fh_out.write (data_byte)
+    # opening file for writing
+    with gzip.open (file_output, 'wb') as fh_out:
+        # writing compressed data into file
+        fh_out.write (data_byte)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
