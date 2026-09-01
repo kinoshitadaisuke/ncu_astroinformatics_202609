8238fa68e6813da7ac2e6868dc590cbaac7e75e2
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:09:37 +0800

    adding "s00c/ai_s00c_10_01.py" on  1/Sep/2026

--- s00c/ai_s00c_10_01.py
+++ s00c/ai_s00c_10_01.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/01 11:07:40 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:55:18 (UT+08:00) daisuke>
 #
 
 # importing json module
 import json
 
-# json file
-file_json = 'stars.json'
+# main function
+def main ():
+    # json file
+    file_json = 'stars.json'
 
-# opening file for reading
-with open (file_json, 'r') as fh:
-    # reading json file
-    dic_data = json.load (fh)
+    # opening file for reading
+    with open (file_json, 'r') as fh:
+        # reading json file
+        dic_data = json.load (fh)
 
-# printing data
-for key1 in sorted (dic_data.keys ()):
-    print (f'{key1}')
-    for key2 in sorted (dic_data[key1].keys ()):
-        print (f'  {key2} : {dic_data[key1][key2]}')
+    # printing data
+    for key1 in sorted (dic_data.keys ()):
+        print (f'{key1}')
+        for key2 in sorted (dic_data[key1].keys ()):
+            print (f'  {key2} : {dic_data[key1][key2]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
