10898a40fc2c04bbe0b7c542b1ad56f29abc934c
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:44 +0800

    adding "s00c/ai_s00c_03_00.py" on  1/Sep/2026

--- s00c/ai_s00c_03_00.py
+++ s00c/ai_s00c_03_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/03 11:07:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:45:56 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,42 +10,48 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of a resource
-url_pi3 = 'https://www.exeter.ac.uk/v8media/facultysites/ese/physics/pi3.txt'
+    # URL of a resource
+    url_pi3 = 'https://www.exeter.ac.uk/v8media/facultysites/ese/physics/pi3.txt'
 
-# output file name
-file_output = 'pi_1000.txt'
+    # output file name
+    file_output = 'pi_1000.txt'
 
-# printing status
-print (f'Now, opening {url_pi3}...')
+    # printing status
+    print (f'Now, opening {url_pi3}...')
 
-# opening URL
-with urllib.request.urlopen (url_pi3) as fh_read:
-    # reading data
-    data_byte = fh_read.read ()
+    # opening URL
+    with urllib.request.urlopen (url_pi3) as fh_read:
+        # reading data
+        data_byte = fh_read.read ()
 
-# printing status
-print (f'Retrieved data from {url_pi3}!')
-    
-# printing type of "data_byte"
-print (f'type of "data_byte" = {type (data_byte)}')
-    
-# converting raw byte data into string
-data_str = data_byte.decode ('utf-8')
+    # printing status
+    print (f'Retrieved data from {url_pi3}!')
 
-# printing type of "data_str"
-print (f'type of "data_str"  = {type (data_str)}')
+    # printing type of "data_byte"
+    print (f'type of "data_byte" = {type (data_byte)}')
 
-# printing status
-print (f'Now, writing data to file "{file_output}"...')
+    # converting raw byte data into string
+    data_str = data_byte.decode ('utf-8')
 
-# opening file for writing
-with open (file_output, 'w') as fh_write:
-    # writing data into file
-    fh_write.write (data_str)
+    # printing type of "data_str"
+    print (f'type of "data_str"  = {type (data_str)}')
 
-# printing status
-print (f'Finished writing data to file "{file_output}"!')
+    # printing status
+    print (f'Now, writing data to file "{file_output}"...')
+
+    # opening file for writing
+    with open (file_output, 'w') as fh_write:
+        # writing data into file
+        fh_write.write (data_str)
+
+    # printing status
+    print (f'Finished writing data to file "{file_output}"!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
