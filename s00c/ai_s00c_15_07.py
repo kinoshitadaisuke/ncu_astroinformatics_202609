667ae51ccd10c6cfc0e08eb454d7886f17db3e3c
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:28 +0800

    adding "s00c/ai_s00c_15_07.py" on  1/Sep/2026

--- s00c/ai_s00c_15_07.py
+++ s00c/ai_s00c_15_07.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:08:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:07:22 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# two integers "c" and "d"
-c = 30
-d = 45
+# main function
+def main ():
+    # two integers "c" and "d"
+    c = 30
+    d = 45
 
-# lcm
-lcm_c_d = math.lcm (c, d)
+    # lcm
+    lcm_c_d = math.lcm (c, d)
 
-# printing results
-print (f'Use of lcm () function:')
-print (f'  c                                = {c}')
-print (f'  d                                = {d}')
-print (f'  least common multiple of c and d = {lcm_c_d}')
-print (f'')
+    # printing results
+    print (f'Use of lcm () function:')
+    print (f'  c                                = {c}')
+    print (f'  d                                = {d}')
+    print (f'  least common multiple of c and d = {lcm_c_d}')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
