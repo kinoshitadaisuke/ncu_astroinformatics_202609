ea8dfa68564ebfee0bf153b98eee9f4c9d0a1417
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:06 +0800

    adding "s00c/ai_s00c_15_02.py" on  1/Sep/2026

--- s00c/ai_s00c_15_02.py
+++ s00c/ai_s00c_15_02.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:20 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:05:54 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# two floats "a" and "b"
-a = 12.34
-b = -56.78
+# main function
+def main ():
+    # two floats "a" and "b"
+    a = 12.34
+    b = -56.78
 
-# ceil
-a_ceil = math.ceil (a)
-b_ceil = math.ceil (b)
+    # ceil
+    a_ceil = math.ceil (a)
+    b_ceil = math.ceil (b)
 
-# printing results
-print (f'Use of ceil () function:')
-print (f'  a         = {a}')
-print (f'  ceil (a)  = {a_ceil}')
-print (f'  {a_ceil} is the smallest integer greater than or equal to {a}.')
-print (f'  b         = {b}')
-print (f'  ceil (b)  = {b_ceil}')
-print (f'  {b_ceil} is the smallest integer greater than or equal to {b}.')
-print (f'')
+    # printing results
+    print (f'Use of ceil () function:')
+    print (f'  a         = {a}')
+    print (f'  ceil (a)  = {a_ceil}')
+    print (f'  {a_ceil} is the smallest integer greater than or equal to {a}.')
+    print (f'  b         = {b}')
+    print (f'  ceil (b)  = {b_ceil}')
+    print (f'  {b_ceil} is the smallest integer greater than or equal to {b}.')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
