e4d0803701468d9b1cbbbb6c4038fb92ac82d75a
Daisuke <mhdf23888@gmail.com>
2026-09-01 14:31:14 +0800

    adding "s00d/ai_s00d_11_01.py" on  1/Sep/2026

---
+++ s00d/ai_s00d_11_01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/04 16:28:34 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# sample Numpy array
+a = numpy.array ([10.0, 10.1, 9.9, 10.2, 9.8, 10.3, 9.7, \
+                  300.0, 10.0, 10.0, 9.9, 9.9, 10.1, 10.1, 10.0])
+
+# making a mask
+mask = numpy.array ([False, False, False, False, False, False, False, \
+                     True, False, False, False, False, False, False, False])
+
+# making a masked array
+a_masked = numpy.ma.array (a, mask=mask)
+
+# printing array "a"
+print (f'a:')
+print (f'{a}')
+
+# printing mask "mask"
+print (f'mask:')
+print (f'{mask}')
+
+# printing masked array "a_masked"
+print (f'a_masked:')
+print (f'{a_masked}')
