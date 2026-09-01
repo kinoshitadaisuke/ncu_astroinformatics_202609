f921511fbd2ace31be35739b66d6cfbeb2d63305
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:08:53 +0800

    adding "s00c/ai_s00c_08_02.py" on  1/Sep/2026

--- s00c/ai_s00c_08_02.py
+++ s00c/ai_s00c_08_02.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:05:39 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:51:43 (UT+08:00) daisuke>
 #
 
 # importing datetime module
 import datetime
 
-# current time in UTC
-time_now_utc = datetime.datetime.now (tz=datetime.timezone.utc)
+# main function
+def main ():
+    # current time in UTC
+    time_now_utc = datetime.datetime.now (tz=datetime.timezone.utc)
 
-# printing result
-print (f'current time in UTC = {time_now_utc}')
+    # printing result
+    print (f'current time in UTC = {time_now_utc}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
