cacbfca2f85658c5bc41554f986b17b95d85f6db
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:28:34 +0800

    adding "s00c/ai_s00c_00_00.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 15:58:50 (UT+08:00) daisuke>
+#
+
+# importing os module
+import os
+
+# knowing where I am now
+cwd = os.getcwd ()
+
+# printing where you are now
+print (f'currently working directory = "{cwd}"')
