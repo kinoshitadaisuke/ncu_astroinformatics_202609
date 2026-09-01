3f715b972d9b83246aa11faa9d970195ca0c00b4
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:36 +0800

    adding "s00c/ai_s00c_14_02.py" on  1/Sep/2026

--- s00c/ai_s00c_14_02.py
+++ s00c/ai_s00c_14_02.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:06:14 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:59:48 (UT+08:00) daisuke>
 #
 
 # importing gzip module
 import gzip
 
-# file of Yale Bright Star Catalogue
-file_bsc = 'catalog.gz'
+# main function
+def main ():
+    # file of Yale Bright Star Catalogue
+    file_bsc = 'catalog.gz'
 
-# output file
-file_output = 'bsc.data'
+    # output file
+    file_output = 'bsc.data'
 
-# opening a compressed file
-with gzip.open (file_bsc, 'rb') as fh_read:
-    # reading file
-    data_byte = fh_read.read ()
+    # opening a compressed file
+    with gzip.open (file_bsc, 'rb') as fh_read:
+        # reading file
+        data_byte = fh_read.read ()
 
-# converting byte data into string
-data_str = data_byte.decode ('utf-8')
+    # converting byte data into string
+    data_str = data_byte.decode ('utf-8')
 
-# opening new file
-with open (file_output, 'w') as fh_write:
-    # writing catalogue into a new file
-    fh_write.write (data_str)
+    # opening new file
+    with open (file_output, 'w') as fh_write:
+        # writing catalogue into a new file
+        fh_write.write (data_str)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
