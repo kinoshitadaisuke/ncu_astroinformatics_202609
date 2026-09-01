31e5b52131d1aaaa16f8331ab1572212b245605e
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:45 +0800

    adding "s00c/ai_s00c_15_11.py" on  1/Sep/2026

--- s00c/ai_s00c_15_11.py
+++ s00c/ai_s00c_15_11.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:08:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:08:32 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# pow () function
-a = math.pow (3, 4)
+# main function
+def main ():
+    # pow () function
+    a = math.pow (3, 4)
 
-# printing result
-print (f'3^4 = 3 * 3 * 3 * 3 = {a}')
+    # printing result
+    print (f'3^4 = 3 * 3 * 3 * 3 = {a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
