18241bbe3eaa01ed29dbeb4c7f291ac796ce7a02
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:06:57 +0800

    adding "s00c/ai_s00c_00_05.py" on  1/Sep/2026

--- s00c/ai_s00c_00_05.py
+++ s00c/ai_s00c_00_05.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 15:59:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:26:55 (UT+08:00) daisuke>
 #
 
 # importing os module
 import os
 
-# target file
-file_target = '/etc/fstab'
+# main function
+def main ():
+    # target file
+    file_target = '/etc/fstab'
 
-# file information
-stat = os.stat (file_target)
+    # file information
+    stat = os.stat (file_target)
 
-# printing file information
-print (f'status of file "{file_target}":')
-print (f'  file owner UID  = {stat.st_uid}')
-print (f'  file owner GID  = {stat.st_gid}')
-print (f'  file mode       = {oct (stat.st_mode)}')
-print (f'  file size       = {stat.st_size} byte')
-print (f'  number of links = {stat.st_nlink}')
+    # printing file information
+    print (f'status of file "{file_target}":')
+    print (f'  file owner UID  = {stat.st_uid}')
+    print (f'  file owner GID  = {stat.st_gid}')
+    print (f'  file mode       = {oct (stat.st_mode)}')
+    print (f'  file size       = {stat.st_size} byte')
+    print (f'  number of links = {stat.st_nlink}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
