f64771c64916adcbbdbde372a0d51dcac1131c4e
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:14:12 +0800

    adding "s00c/ai_s00c_22_10.py" on  1/Sep/2026

--- s00c/ai_s00c_22_10.py
+++ s00c/ai_s00c_22_10.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:14:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:18:40 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# function y
-y = sympy.Function ('y')
+# main function
+def main ():
+    # function y
+    y = sympy.Function ('y')
 
-# variable x
-x, k = sympy.symbols ('x k')
+    # variable x
+    x, k = sympy.symbols ('x k')
 
-# dy/dx
-dy_dx = sympy.diff (y(x), x)
+    # dy/dx
+    dy_dx = sympy.diff (y(x), x)
 
-# differential equation
-eq = sympy.Eq (dy_dx, -k*y(x))
+    # differential equation
+    eq = sympy.Eq (dy_dx, -k*y(x))
 
-# solving dy/dx = -ky
-sol = sympy.dsolve (eq, y(x))
+    # solving dy/dx = -ky
+    sol = sympy.dsolve (eq, y(x))
 
-# printing result
-print (f'equation: {eq}')
-print (f'solution: {sol}')
+    # printing result
+    print (f'equation: {eq}')
+    print (f'solution: {sol}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
