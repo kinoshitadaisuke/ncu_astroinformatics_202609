a5723bbccf3cb647eace997a56a1dea603bfcbd9
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:26 +0800

    adding "s00c/ai_s00c_22_00.py" on  1/Sep/2026

--- s00c/ai_s00c_22_00.py
+++ s00c/ai_s00c_22_00.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:13:11 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:15:56 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# variable x
-x = sympy.symbols ('x')
+# main function
+def main ():
+    # variable x
+    x = sympy.symbols ('x')
 
-# function f
-f = (x + 1)**2
+    # function f
+    f = (x + 1)**2
 
-# expansion of (x+1)**2
-f2 = sympy.expand (f)
+    # expansion of (x+1)**2
+    f2 = sympy.expand (f)
 
-# printing result
-print (f'{f} = {f2}')
+    # printing result
+    print (f'{f} = {f2}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
