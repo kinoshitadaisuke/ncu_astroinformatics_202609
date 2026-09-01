ae4b798160ea28b00616690938a23a745e0819f2
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:49 +0800

    adding "s00c/ai_s00c_15_12.py" on  1/Sep/2026

--- s00c/ai_s00c_15_12.py
+++ s00c/ai_s00c_15_12.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:08:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:08:47 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# sqrt () function
-b = math.sqrt (3)
+# main function
+def main ():
+    # sqrt () function
+    b = math.sqrt (3)
 
-# printing result
-print (f'sqrt (3) = {b}')
+    # printing result
+    print (f'sqrt (3) = {b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
