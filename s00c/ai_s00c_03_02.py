5d7ec8cfef085a6a773bae309e7c5ac194eacf1c
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:53 +0800

    adding "s00c/ai_s00c_03_02.py" on  1/Sep/2026

--- s00c/ai_s00c_03_02.py
+++ s00c/ai_s00c_03_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/03 11:11:05 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:46:35 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,33 +10,39 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of a resource
-url_map = 'https://www.ncu.edu.tw/var/file/0/1000/img/3/230501932.png'
+    # URL of a resource
+    url_map = 'https://www.ncu.edu.tw/var/file/0/1000/img/3/230501932.png'
 
-# output file name
-file_output = 'ncu_map.png'
+    # output file name
+    file_output = 'ncu_map.png'
 
-# printing status
-print (f'Now, opening {url_map}...')
+    # printing status
+    print (f'Now, opening {url_map}...')
 
-# opening URL
-with urllib.request.urlopen (url_map) as fh_read:
-    # reading data
-    data_byte = fh_read.read ()
+    # opening URL
+    with urllib.request.urlopen (url_map) as fh_read:
+        # reading data
+        data_byte = fh_read.read ()
 
-# printing status
-print (f'Retrieved data from {url_map}!')
-    
-# printing status
-print (f'Now, writing data to file "{file_output}"...')
+    # printing status
+    print (f'Retrieved data from {url_map}!')
 
-# opening file for writing
-with open (file_output, 'wb') as fh_write:
-    # writing data into file
-    fh_write.write (data_byte)
+    # printing status
+    print (f'Now, writing data to file "{file_output}"...')
 
-# printing status
-print (f'Finished writing data to file "{file_output}"!')
+    # opening file for writing
+    with open (file_output, 'wb') as fh_write:
+        # writing data into file
+        fh_write.write (data_byte)
+
+    # printing status
+    print (f'Finished writing data to file "{file_output}"!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
