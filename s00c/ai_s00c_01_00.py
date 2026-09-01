9664e4381cba38fb02859358f4134d5417e12f62
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:29:09 +0800

    adding "s00c/ai_s00c_01_00.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_01_00.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:00:34 (UT+08:00) daisuke>
+#
+
+# importing sys module
+import sys
+
+# getting Python version
+version = sys.version
+
+# printing Python version
+print (f'Python version = "{version}"')
