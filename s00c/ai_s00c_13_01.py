113e6aa71a13f55d42758d082d376b225ac170b2
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:19 +0800

    adding "s00c/ai_s00c_13_01.py" on  1/Sep/2026

--- s00c/ai_s00c_13_01.py
+++ s00c/ai_s00c_13_01.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/03 10:07:13 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:58:35 (UT+08:00) daisuke>
 #
 
 # importing zipfile module
 import zipfile
 
-# zip file
-file_zip = 'files2.zip'
+# main function
+def main ():
+    # zip file
+    file_zip = 'files2.zip'
 
-# opening file for reading
-with zipfile.ZipFile (file_zip, 'r') as fh:
-    # list of files in zip file
-    list_files = fh.namelist ()
+    # opening file for reading
+    with zipfile.ZipFile (file_zip, 'r') as fh:
+        # list of files in zip file
+        list_files = fh.namelist ()
 
-# printing list of files in zip file
-for filename in list_files:
-    # printing file name
-    print (f'{filename}')
+    # printing list of files in zip file
+    for filename in list_files:
+        # printing file name
+        print (f'{filename}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
