dbeb7ecc3648f1420a2a49c8abdcaff2a7ba03e9
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:32:13 +0800

    adding "s00c/ai_s00c_15_00.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_15_00.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:07:05 (UT+08:00) daisuke>
+#
+
+# importing math module
+import math
+
+#
+# some constants
+#
+
+# pi
+pi = math.pi
+print (f'pi   = {pi}')
+
+# tau
+tau = math.tau
+print (f'tau  = 2.0 * pi\n     = {tau}')
+
+# e
+e = math.e
+print (f'e    = {e}')
+
+# positive infinity
+pinf = math.inf
+print (f'+inf = {pinf}')
+
+# negative infinity
+ninf = -math.inf
+print (f'-inf = {ninf}')
+
+# NaN (not a number)
+nan = math.nan
+print (f'NaN  = {nan}')
