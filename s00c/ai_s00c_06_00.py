8d10f34757d79ae89b4a1ce9e38ff7fbd71b22b7
Daisuke <mhdf23888@gmail.com>
2026-09-01 10:30:05 +0800

    adding "s00c/ai_s00c_06_00.py" on  1/Sep/2026

---
+++ s00c/ai_s00c_06_00.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/02/28 16:03:39 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# executing a command "date"
+subprocess.run ('date')
