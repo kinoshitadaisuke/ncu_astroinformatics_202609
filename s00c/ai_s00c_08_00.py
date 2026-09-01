599ef290ca898bf222cc0a7eb51b99ee7fb2e734
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:08:44 +0800

    adding "s00c/ai_s00c_08_00.py" on  1/Sep/2026

--- s00c/ai_s00c_08_00.py
+++ s00c/ai_s00c_08_00.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:04:22 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:51:06 (UT+08:00) daisuke>
 #
 
 # importing datetime module
 import datetime
 
-# time offset from UTC
-#   local time in Taiwan = UT + 8-hr
-dt = datetime.timedelta (hours=8)
+# main function
+def main ():
+    # time offset from UTC
+    #   local time in Taiwan = UT + 8-hr
+    dt = datetime.timedelta (hours=8)
 
-# current time in local time
-time_now_local = datetime.datetime.now (tz=datetime.timezone (dt))
+    # current time in local time
+    time_now_local = datetime.datetime.now (tz=datetime.timezone (dt))
 
-# printing result
-print (f'current local time: {time_now_local}')
+    # printing result
+    print (f'current local time: {time_now_local}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
