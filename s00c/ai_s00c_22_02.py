3896349627af8a3c8ee17b7f759fe74bcbe59aab
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:38 +0800

    adding "s00c/ai_s00c_22_02.py" on  1/Sep/2026

--- s00c/ai_s00c_22_02.py
+++ s00c/ai_s00c_22_02.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:13:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:16:28 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# variable x
-x = sympy.symbols ('x')
+# main function
+def main ():
+    # variable x
+    x = sympy.symbols ('x')
 
-# function f
-f = (1 + 1/x)**x
+    # function f
+    f = (1 + 1/x)**x
 
-# limit x --> infinity
-lim_f = sympy.limit (f, x, sympy.oo)
+    # limit x --> infinity
+    lim_f = sympy.limit (f, x, sympy.oo)
 
-# printing result
-print (f'lim x->infty [{f}] = {lim_f}')
+    # printing result
+    print (f'lim x->infty [{f}] = {lim_f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
