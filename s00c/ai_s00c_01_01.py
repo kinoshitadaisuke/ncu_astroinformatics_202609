2a66807b6ec631f3dcb7e492b0d2f62cf8a50d08
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:22 +0800

    adding "s00c/ai_s00c_01_01.py" on  1/Sep/2026

--- s00c/ai_s00c_01_01.py
+++ s00c/ai_s00c_01_01.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:00:47 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:43:49 (UT+08:00) daisuke>
 #
 
 # importing sys module
 import sys
 
-while (True):
-    # receiving an integer from keyboard input
-    number_str = input ('Input an integer: ')
-    # conversion from string into integer
-    try:
-        number = int (number_str)
-    except:
-        # printing message
-        print (f'"{number_str}" is not a number!')
-        # skipping current iteration
-        continue
-    # if the integer is greater than zero, then receive the other integer
-    if (number > 0):
-        # skipping current iteration
-        continue
-    # if the integer is equal to zero or less than zero, then stop the script
-    else:
-        # exit Python interpreter using sys.exit ()
-        sys.exit (0)
+# main function
+def main ():
+    while (True):
+        # receiving an integer from keyboard input
+        number_str = input ('Input an integer: ')
+        # conversion from string into integer
+        try:
+            number = int (number_str)
+        except:
+            # printing message
+            print (f'"{number_str}" is not a number!')
+            # skipping current iteration
+            continue
+        # if the integer is greater than zero, then receive the other integer
+        if (number > 0):
+            # skipping current iteration
+            continue
+        # if the integer is equal to zero or less than zero, then stop the script
+        else:
+            # exit Python interpreter using sys.exit ()
+            sys.exit (0)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
