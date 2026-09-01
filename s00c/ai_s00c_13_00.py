a4aabdeb38b087d021b112c92393ffd3e6bb1a2d
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:10:15 +0800

    adding "s00c/ai_s00c_13_00.py" on  1/Sep/2026

--- s00c/ai_s00c_13_00.py
+++ s00c/ai_s00c_13_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/03 10:00:24 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 12:01:48 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,21 +10,27 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of data file
-url_zipfile = 'https://s3b.astro.ncu.edu.tw/appy_202602/data/files2.zip'
+    # URL of data file
+    url_zipfile = 'https://s3b.astro.ncu.edu.tw/ai_202609/data/files2.zip'
 
-# output data file
-file_zipfile = 'files2.zip'
+    # output data file
+    file_zipfile = 'files2.zip'
 
-# opening URL
-with urllib.request.urlopen (url_zipfile) as fh_in:
-    # reading data
-    data_zipfile = fh_in.read ()
+    # opening URL
+    with urllib.request.urlopen (url_zipfile) as fh_in:
+        # reading data
+        data_zipfile = fh_in.read ()
 
-# opening file for writing
-with open (file_zipfile, 'wb') as fh_out:
-    # writing data into file
-    fh_out.write (data_zipfile)
+    # opening file for writing
+    with open (file_zipfile, 'wb') as fh_out:
+        # writing data into file
+        fh_out.write (data_zipfile)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
