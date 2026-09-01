61fbc9c35932a6be97a11004e942f7b90d8f743f
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:10 +0800

    adding "s00c/ai_s00c_15_03.py" on  1/Sep/2026

--- s00c/ai_s00c_15_03.py
+++ s00c/ai_s00c_15_03.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:06:10 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# two floats "a" and "b"
-a = 12.34
-b = -56.78
+# main function
+def main ():
+    # two floats "a" and "b"
+    a = 12.34
+    b = -56.78
 
-# trunc
-a_trunc = math.trunc (a)
-b_trunc = math.trunc (b)
+    # trunc
+    a_trunc = math.trunc (a)
+    b_trunc = math.trunc (b)
 
-# printing results
-print (f'Use of trunc () function:')
-print (f'  a         = {a}')
-print (f'  trunc (a) = {a_trunc}')
-print (f'  {a_trunc} is the integer part of {a}.')
-print (f'  b         = {b}')
-print (f'  trunc (b) = {b_trunc}')
-print (f'  {b_trunc} is the integer part of {b}.')
-print (f'')
+    # printing results
+    print (f'Use of trunc () function:')
+    print (f'  a         = {a}')
+    print (f'  trunc (a) = {a_trunc}')
+    print (f'  {a_trunc} is the integer part of {a}.')
+    print (f'  b         = {b}')
+    print (f'  trunc (b) = {b_trunc}')
+    print (f'  {b_trunc} is the integer part of {b}.')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
