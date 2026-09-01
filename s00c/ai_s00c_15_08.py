012325158339d5579c970c6c10b0e5560ddbfdd9
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:32 +0800

    adding "s00c/ai_s00c_15_08.py" on  1/Sep/2026

--- s00c/ai_s00c_15_08.py
+++ s00c/ai_s00c_15_08.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:08:18 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:07:38 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# factorial
-factorial_5 = math.factorial (5)
-factorial_8 = math.factorial (8)
+# main function
+def main ():
+    # factorial
+    factorial_5 = math.factorial (5)
+    factorial_8 = math.factorial (8)
 
-# printing results
-print (f'Use of factorial () function:')
-print (f'  5! = 1 * 2 * 3 * 4 * 5\n     = {factorial_5}')
-print (f'  8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8\n     = {factorial_8}')
-print (f'')
+    # printing results
+    print (f'Use of factorial () function:')
+    print (f'  5! = 1 * 2 * 3 * 4 * 5\n     = {factorial_5}')
+    print (f'  8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8\n     = {factorial_8}')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
