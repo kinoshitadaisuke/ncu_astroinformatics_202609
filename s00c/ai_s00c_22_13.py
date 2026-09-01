c8f0c421a16b0a86f576ea16d48fda58e6133dd7
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:14:24 +0800

    adding "s00c/ai_s00c_22_13.py" on  1/Sep/2026

--- s00c/ai_s00c_22_13.py
+++ s00c/ai_s00c_22_13.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:14:53 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:19:27 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# variable x
-x = sympy.symbols ('x')
+# main function
+def main ():
+    # variable x
+    x = sympy.symbols ('x')
 
-# function f(x)
-f = sympy.sqrt (4 - x**2)
+    # function f(x)
+    f = sympy.sqrt (4 - x**2)
 
-# integration of f(x) from 0 to 2
-I = sympy.integrate (f, (x, 0, 2))
+    # integration of f(x) from 0 to 2
+    I = sympy.integrate (f, (x, 0, 2))
 
-# printing result
-print (f'I = {I}')
+    # printing result
+    print (f'I = {I}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
