954deeaf2d26514f1659e9fae39b893c0cf2e492
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:13:42 +0800

    adding "s00c/ai_s00c_22_03.py" on  1/Sep/2026

--- s00c/ai_s00c_22_03.py
+++ s00c/ai_s00c_22_03.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:13:34 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:16:45 (UT+08:00) daisuke>
 #
 
 # importing sympy module
 import sympy
 
-# variable x
-x, a, b, c = sympy.symbols ('x a b c')
+# main function
+def main ():
+    # variable x
+    x, a, b, c = sympy.symbols ('x a b c')
 
-# function f
-f = a * x**2 + b * x + c
+    # function f
+    f = a * x**2 + b * x + c
 
-# solving f(x) = 0
-sol = sympy.solve (f, x)
+    # solving f(x) = 0
+    sol = sympy.solve (f, x)
 
-# printing result
-print (f'solution of {f}=0:')
-print (f'x = {sol}')
+    # printing result
+    print (f'solution of {f}=0:')
+    print (f'x = {sol}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
