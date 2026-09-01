cd2cf0acea460e324567e576f5484911e3b1ffe8
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:11:01 +0800

    adding "s00c/ai_s00c_15_01.py" on  1/Sep/2026

--- s00c/ai_s00c_15_01.py
+++ s00c/ai_s00c_15_01.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:07:13 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 11:05:35 (UT+08:00) daisuke>
 #
 
 # importing math module
 import math
 
-# two floats "a" and "b"
-a = 12.34
-b = -56.78
+# main function
+def main ():
+    # two floats "a" and "b"
+    a = 12.34
+    b = -56.78
 
-# floor
-a_floor = math.floor (a)
-b_floor = math.floor (b)
+    # floor
+    a_floor = math.floor (a)
+    b_floor = math.floor (b)
 
-# printing results
-print (f'Use of floor () function:')
-print (f'  a         = {a}')
-print (f'  floor (a) = {a_floor}')
-print (f'  {a_floor} is the largest integer less than or equal to {a}.')
-print (f'  b         = {b}')
-print (f'  floor (b) = {b_floor}')
-print (f'  {b_floor} is the largest integer less than or equal to {b}.')
-print (f'')
+    # printing results
+    print (f'Use of floor () function:')
+    print (f'  a         = {a}')
+    print (f'  floor (a) = {a_floor}')
+    print (f'  {a_floor} is the largest integer less than or equal to {a}.')
+    print (f'  b         = {b}')
+    print (f'  floor (b) = {b_floor}')
+    print (f'  {b_floor} is the largest integer less than or equal to {b}.')
+    print (f'')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
