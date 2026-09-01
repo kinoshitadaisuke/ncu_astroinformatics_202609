1c3e28721789649b778838b9f2e17a6456a91c68
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:08:07 +0800

    adding "s00c/ai_s00c_04_02.py" on  1/Sep/2026

--- s00c/ai_s00c_04_02.py
+++ s00c/ai_s00c_04_02.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:02:32 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:47:28 (UT+08:00) daisuke>
 #
 
 # importing pathlib module
 import pathlib
 
-# directory name
-dir_zone = '/usr/share/zoneinfo'
+# main function
+def main ():
+    # directory name
+    dir_zone = '/usr/share/zoneinfo'
 
-# making a pathlib object
-path_zone = pathlib.Path (dir_zone)
+    # making a pathlib object
+    path_zone = pathlib.Path (dir_zone)
 
-# finding directory contents
-list_files = path_zone.iterdir ()
+    # finding directory contents
+    list_files = path_zone.iterdir ()
 
-# printing directory contents
-print (f'directory contents of "{path_zone}":')
-for filename in list_files:
-    # printing file name
-    print (f'  {filename}')
+    # printing directory contents
+    print (f'directory contents of "{path_zone}":')
+    for filename in list_files:
+        # printing file name
+        print (f'  {filename}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
