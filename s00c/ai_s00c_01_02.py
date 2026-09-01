d7d6e6d485d7d3d3a226459a33a4797e4bb432df
Daisuke <mhdf23888@gmail.com>
2026-09-01 12:07:27 +0800

    adding "s00c/ai_s00c_01_02.py" on  1/Sep/2026

--- s00c/ai_s00c_01_02.py
+++ s00c/ai_s00c_01_02.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/02/28 16:00:58 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 10:44:09 (UT+08:00) daisuke>
 #
 
 # importing sys module
 import sys
 
-# receiving command-line arguments
-args = sys.argv
+# main function
+def main ():
+    # receiving command-line arguments
+    args = sys.argv
 
-# number of command-line arguments
-n_args = len (args)
+    # number of command-line arguments
+    n_args = len (args)
 
-# printing number of command-line arguments
-print (f'number of command-line arguments = {n_args}')
+    # printing number of command-line arguments
+    print (f'number of command-line arguments = {n_args}')
 
-# printing command-line arguments
-print (f'arguments = {args}')
+    # printing command-line arguments
+    print (f'arguments = {args}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
