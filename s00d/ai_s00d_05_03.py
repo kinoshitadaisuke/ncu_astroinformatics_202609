1f5eae5bc3d43b2d6ad09bc3ac377d80f151be0d
Daisuke <mhdf23888@gmail.com>
2026-09-01 14:28:49 +0800

    adding "s00d/ai_s00d_05_03.py" on  1/Sep/2026

---
+++ s00d/ai_s00d_05_03.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/04 16:23:17 (UT+08:00) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making Numpy arrays using numpy.linspace ()
+a = numpy.linspace (4.0, 40.0, 10)
+b = numpy.linspace (2.0, 20.0, 10)
+
+# printing a and b
+print (f'a = {a}')
+print (f'b = {b}')
+
+# calculation
+# no need of using "for"
+c = a / b
+
+# printing c
+print (f'c = a / b = {c}')
