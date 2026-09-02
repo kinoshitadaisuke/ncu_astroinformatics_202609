591a02bb69336dc6934cb0a066d36df1ed1d4812
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:29 +0800

    adding "s00d/ai_s00d_12_13.py" on  2/Sep/2026

--- s00d/ai_s00d_12_13.py
+++ s00d/ai_s00d_12_13.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:31:37 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:42:20 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_04.npz'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_04.npz'
 
-# loading Numpy arrays from npy file
-arrays = numpy.load (file_input)
+    # loading Numpy arrays from npy file
+    arrays = numpy.load (file_input)
 
-# printing "arrays"
-print (f'{arrays}')
+    # printing "arrays"
+    print (f'{arrays}')
 
-# printing object type of "arrays"
-print (f'{type (arrays)}')
+    # printing object type of "arrays"
+    print (f'{type (arrays)}')
 
-# first Numpy array in "arrays"
-array_c = arrays['array_c']
+    # first Numpy array in "arrays"
+    array_c = arrays['array_c']
 
-# second Numpy array in "arrays"
-array_d = arrays['array_d']
+    # second Numpy array in "arrays"
+    array_d = arrays['array_d']
 
-# printing array_c and array_d
-print (f'array_c:')
-print (array_c)
-print (f'array_d:')
-print (array_d)
+    # printing array_c and array_d
+    print (f'array_c:')
+    print (array_c)
+    print (f'array_d:')
+    print (array_d)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
