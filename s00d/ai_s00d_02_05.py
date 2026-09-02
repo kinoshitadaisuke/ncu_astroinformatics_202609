c6cc5bb67155e8cf7a119dcf86e191fbecdc06e6
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:47:16 +0800

    adding "s00d/ai_s00d_02_05.py" on  2/Sep/2026

--- s00d/ai_s00d_02_05.py
+++ s00d/ai_s00d_02_05.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/07 21:36:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:44:54 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) of a specified data type
-# numpy.dtype ('i1') : 8-bit integer
-array_f = numpy.array ([0, 1, 2, 3, 4, 5, -1, -2, -3, -4, -129], \
-                       dtype=numpy.dtype ('i1') )
+# main function
+def main ():
+    # making a Numpy array (ndarray) of a specified data type
+    # numpy.dtype ('i1') : 8-bit integer
+    array_f = numpy.array ([0, 1, 2, 3, 4, 5, -1, -2, -3, -4, -129], \
+                           dtype=numpy.dtype ('i1') )
 
-# printing Numpy array
-print (f'array_f:\n{array_f}')
+    # printing Numpy array
+    print (f'array_f:\n{array_f}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_f.ndim}')
-print (f'  size     = {array_f.size}')
-print (f'  shape    = {array_f.shape}')
-print (f'  dtype    = {array_f.dtype}')
-print (f'  itemsize = {array_f.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_f.ndim}')
+    print (f'  size     = {array_f.size}')
+    print (f'  shape    = {array_f.shape}')
+    print (f'  dtype    = {array_f.dtype}')
+    print (f'  itemsize = {array_f.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
