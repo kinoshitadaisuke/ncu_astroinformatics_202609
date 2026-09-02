55fab6331a6230d07781b7a05a10c1c2aa9e0be7
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:36 +0800

    adding "s00d/ai_s00d_05_05.py" on  2/Sep/2026

--- s00d/ai_s00d_05_05.py
+++ s00d/ai_s00d_05_05.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:23:32 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:21:19 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays using numpy.linspace ()
-a = numpy.linspace (0.0, 10.0, 11)
+# main function
+def main ():
+    # making Numpy arrays using numpy.linspace ()
+    a = numpy.linspace (0.0, 10.0, 11)
 
-# printing a
-print (f'a = {a}')
+    # printing a
+    print (f'a = {a}')
 
-# calculation
-# no need of using "for"
-b = numpy.sqrt (a)
+    # calculation
+    # no need of using "for"
+    b = numpy.sqrt (a)
 
-# printing b
-print (f'b = sqrt (a)\n  = {b}')
+    # printing b
+    print (f'b = sqrt (a)\n  = {b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
