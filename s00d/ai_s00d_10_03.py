6920cab261ef10124f44cf876c2c2134774f296b
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:48 +0800

    adding "s00d/ai_s00d_10_03.py" on  2/Sep/2026

--- s00d/ai_s00d_10_03.py
+++ s00d/ai_s00d_10_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:28:16 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:34:26 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -10,39 +10,45 @@ import numpy
 # importing statistics module
 import statistics
 
-# random number generator
-rng = numpy.random.Generator (numpy.random.PCG64DXSM ())
+# main function
+def main ():
+    # random number generator
+    rng = numpy.random.Generator (numpy.random.PCG64DXSM ())
 
-# generating 10^8 random numbers of Gaussian distribution 
-# of mean = 10000.0 and stddev = 300.0
-array_x = rng.normal (10000.0, 300.0, 10**8)
+    # generating 10^8 random numbers of Gaussian distribution 
+    # of mean = 10000.0 and stddev = 300.0
+    array_x = rng.normal (10000.0, 300.0, 10**8)
 
-# printing generated random numbers
-print (f'{array_x}')
+    # printing generated random numbers
+    print (f'{array_x}')
 
-# printing number of data
-print (f'number of data = {len (array_x):g}')
+    # printing number of data
+    print (f'number of data = {len (array_x):g}')
 
-# statistical values calculated by numpy module
-mean_n     = numpy.mean (array_x)
-median_n   = numpy.median (array_x)
-variance_n = numpy.var (array_x)
-stddev_n   = numpy.std (array_x)
+    # statistical values calculated by numpy module
+    mean_n     = numpy.mean (array_x)
+    median_n   = numpy.median (array_x)
+    variance_n = numpy.var (array_x)
+    stddev_n   = numpy.std (array_x)
 
-# printing statistical values
-print (f'statistical values by Numpy:')
-print (f'  mean     = {mean_n:10.3f}')
-print (f'  median   = {median_n:10.3f}')
-print (f'  variance = {variance_n:10.3f}')
-print (f'  stddev   = {stddev_n:10.3f}')
+    # printing statistical values
+    print (f'statistical values by Numpy:')
+    print (f'  mean     = {mean_n:10.3f}')
+    print (f'  median   = {median_n:10.3f}')
+    print (f'  variance = {variance_n:10.3f}')
+    print (f'  stddev   = {stddev_n:10.3f}')
 
-# finding maximum value
-maximum = numpy.amax (array_x)
+    # finding maximum value
+    maximum = numpy.amax (array_x)
 
-# finding minimum value
-minimum = numpy.amin (array_x)
+    # finding minimum value
+    minimum = numpy.amin (array_x)
 
-# printing maximum and minimum
-print (f'maximum and minimum:')
-print (f'  maximum = {maximum:10.3f}')
-print (f'  minimum = {minimum:10.3f}')
+    # printing maximum and minimum
+    print (f'maximum and minimum:')
+    print (f'  maximum = {maximum:10.3f}')
+    print (f'  minimum = {minimum:10.3f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
