ebdc196d741cf44a06bbb7b2a78337e625b17ae7
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:47:47 +0800

    adding "s00d/ai_s00d_02_13.py" on  2/Sep/2026

--- s00d/ai_s00d_02_13.py
+++ s00d/ai_s00d_02_13.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:19:31 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:47:05 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) of a specified data type
-# numpy.dtype ('float32') : 32-bit floating point number
-array_h = numpy.array ([-9.8, -7.6, -5.4, -3.2, -1.0, \
-                        1.2, 3.4, 5.6, 7.8, 9.0], \
-                       dtype=numpy.dtype ('float32') )
+# main function
+def main ():
+    # making a Numpy array (ndarray) of a specified data type
+    # numpy.dtype ('float32') : 32-bit floating point number
+    array_h = numpy.array ([-9.8, -7.6, -5.4, -3.2, -1.0, \
+                            1.2, 3.4, 5.6, 7.8, 9.0], \
+                           dtype=numpy.dtype ('float32') )
 
-# printing Numpy array
-print (f'array_h:\n{array_h}')
+    # printing Numpy array
+    print (f'array_h:\n{array_h}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_h.ndim}')
-print (f'  size     = {array_h.size}')
-print (f'  shape    = {array_h.shape}')
-print (f'  dtype    = {array_h.dtype}')
-print (f'  itemsize = {array_h.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_h.ndim}')
+    print (f'  size     = {array_h.size}')
+    print (f'  shape    = {array_h.shape}')
+    print (f'  dtype    = {array_h.dtype}')
+    print (f'  itemsize = {array_h.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
