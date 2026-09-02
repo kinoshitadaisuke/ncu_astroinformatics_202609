62ee81d746b0df4fd7e0dd1cbbd322d3c30b509a
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:11 +0800

    adding "s00d/ai_s00d_05_14.py" on  2/Sep/2026

--- s00d/ai_s00d_05_14.py
+++ s00d/ai_s00d_05_14.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:23:47 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array (2x2 matrix)
-A = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
+# main function
+def main ():
+    # making Numpy array (2x2 matrix)
+    A = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
 
-# printing A
-print (f'A:\n{A}')
+    # printing A
+    print (f'A:\n{A}')
 
-# making Numpy array (2x2 unit matrix)
-E2 = numpy.identity (2)
+    # making Numpy array (2x2 unit matrix)
+    E2 = numpy.identity (2)
 
-# printing E2
-print (f'E2:\n{E2}')
+    # printing E2
+    print (f'E2:\n{E2}')
 
-# calculation
-B = A @ E2
+    # calculation
+    B = A @ E2
 
-# printing B
-print (f'B = A @ E2:\n{B}')
+    # printing B
+    print (f'B = A @ E2:\n{B}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
