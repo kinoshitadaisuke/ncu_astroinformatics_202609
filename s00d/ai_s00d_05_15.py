53e9b077ce83a870b7a0da7dc82984e0ebea1651
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:14 +0800

    adding "s00d/ai_s00d_05_15.py" on  2/Sep/2026

--- s00d/ai_s00d_05_15.py
+++ s00d/ai_s00d_05_15.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:48 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:24:04 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array (3x3 matrix)
-C = numpy.array ([ [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0] ])
+# main function
+def main ():
+    # making Numpy array (3x3 matrix)
+    C = numpy.array ([ [1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0] ])
 
-# printing C
-print (f'C:\n{C}')
+    # printing C
+    print (f'C:\n{C}')
 
-# making Numpy array (3x3 unit matrix)
-E3 = numpy.identity (3)
+    # making Numpy array (3x3 unit matrix)
+    E3 = numpy.identity (3)
 
-# printing E3
-print (f'E3:\n{E3}')
+    # printing E3
+    print (f'E3:\n{E3}')
 
-# calculation
-D = E3 @ C
+    # calculation
+    D = E3 @ C
 
-# printing D
-print (f'D = E3 @ C:\n{D}')
+    # printing D
+    print (f'D = E3 @ C:\n{D}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
