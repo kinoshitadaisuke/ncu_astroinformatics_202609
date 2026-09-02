17e2f9ec186fe8ddb25858de28724a42f62e8e74
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:52:46 +0800

    adding "s00d/ai_s00d_12_01.py" on  2/Sep/2026

--- s00d/ai_s00d_12_01.py
+++ s00d/ai_s00d_12_01.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:30:13 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:38:48 (UT+08:00) daisuke>
 #
 
-# input data file
-file_input = 'numpy_00.data'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_00.data'
 
-# opening file for reading
-with open (file_input, 'r') as fh_in:
-    # reading data in the file line-by-line
-    for line in fh_in:
-        # removing line feed at the end of the line
-        line = line.strip ()
-        # printing data
-        print (f'{line}')
+    # opening file for reading
+    with open (file_input, 'r') as fh_in:
+        # reading data in the file line-by-line
+        for line in fh_in:
+            # removing line feed at the end of the line
+            line = line.strip ()
+            # printing data
+            print (f'{line}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
