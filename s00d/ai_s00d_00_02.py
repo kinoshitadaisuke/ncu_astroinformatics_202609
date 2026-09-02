335cdacab1a2c2f0ba553dad98e43c4be8f4b031
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:46:39 +0800

    adding "s00d/ai_s00d_00_02.py" on  2/Sep/2026

--- s00d/ai_s00d_00_02.py
+++ s00d/ai_s00d_00_02.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:17:24 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:41:47 (UT+08:00) daisuke>
 #
 
 # importing numpy
 import numpy
 
-# making a list
-list_a = [0.1, 2.3, 4.5, 6.7, 8.9]
+# main function
+def main ():
+    # making a list
+    list_a = [0.1, 2.3, 4.5, 6.7, 8.9]
 
-# making a Numpy array (ndarray)
-array_a = numpy.array (list_a)
+    # making a Numpy array (ndarray)
+    array_a = numpy.array (list_a)
 
-# type of "list_a"
-type_list_a = type (list_a)
+    # type of "list_a"
+    type_list_a = type (list_a)
 
-# type of "array_a"
-type_array_a = type (array_a)
+    # type of "array_a"
+    type_array_a = type (array_a)
 
-# printing list
-print (f'list_a:')
-print (f'  list_a  = {list_a}')
-print (f'  type    = {type_list_a}')
+    # printing list
+    print (f'list_a:')
+    print (f'  list_a  = {list_a}')
+    print (f'  type    = {type_list_a}')
 
-# printing Numpy array
-print (f'array_a:')
-print (f'  array_a = {array_a}')
-print (f'  type    = {type_array_a}')
+    # printing Numpy array
+    print (f'array_a:')
+    print (f'  array_a = {array_a}')
+    print (f'  type    = {type_array_a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
