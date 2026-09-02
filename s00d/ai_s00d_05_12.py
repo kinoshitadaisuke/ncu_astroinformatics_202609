fc7769649f7c5780029417ddf4178b0b5cf23cac
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:03 +0800

    adding "s00d/ai_s00d_05_12.py" on  2/Sep/2026

--- s00d/ai_s00d_05_12.py
+++ s00d/ai_s00d_05_12.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:22 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:23:15 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays (2x2 matrix)
-A = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
-B = numpy.array ([ [4.0, 2.0], [1.0, 3.0] ])
+# main function
+def main ():
+    # making Numpy arrays (2x2 matrix)
+    A = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
+    B = numpy.array ([ [4.0, 2.0], [1.0, 3.0] ])
 
-# printing A and B
-print (f'A:\n{A}')
-print (f'B:\n{B}')
+    # printing A and B
+    print (f'A:\n{A}')
+    print (f'B:\n{B}')
 
-# the other way to take a matrix product
-C = numpy.matmul (A, B)
+    # the other way to take a matrix product
+    C = numpy.matmul (A, B)
 
-# printing C
-print (f'C = matmul (A, B):\n{C}')
+    # printing C
+    print (f'C = matmul (A, B):\n{C}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
