9b07fe2a7a14b596d877c6733c86cbdcc150f095
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:46:31 +0800

    adding "s00d/ai_s00d_00_00.py" on  2/Sep/2026

--- s00d/ai_s00d_00_00.py
+++ s00d/ai_s00d_00_00.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:17:04 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:41:09 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# making a Numpy array (ndarray)
-array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
+# main function
+def main ():
+    # making a Numpy array (ndarray)
+    array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
 
-# printing Numpy array
-print (f'array_a = {array_a}')
+    # printing Numpy array
+    print (f'array_a = {array_a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
