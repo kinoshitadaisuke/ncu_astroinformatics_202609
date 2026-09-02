58e5af8316e10a97c99f61080dbae64ca3e388a5
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:32 +0800

    adding "s00d/ai_s00d_06_03.py" on  2/Sep/2026

--- s00d/ai_s00d_06_03.py
+++ s00d/ai_s00d_06_03.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:25:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:25:26 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.array ([ [0.0, 1.0, 2.0, 3.0], [4.0, 5.0, 6.0, 7.0], \
-                   [8.0, 9.0, 10.0, 11.0], [12.0, 13.0, 14.0, 15.0] ])
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.array ([ [0.0, 1.0, 2.0, 3.0], [4.0, 5.0, 6.0, 7.0], \
+                       [8.0, 9.0, 10.0, 11.0], [12.0, 13.0, 14.0, 15.0] ])
 
-# printing A
-print (f'a:\n{a}')
+    # printing A
+    print (f'a:\n{a}')
 
-# accessing to an element by indexing
-print (f'a[0:2,1:3]:\n{a[0:2,1:3]}')
-print (f'a[1:3,:]:\n{a[1:3,:]}')
-print (f'a[:,1:3]:\n{a[:,1:3]}')
+    # accessing to an element by indexing
+    print (f'a[0:2,1:3]:\n{a[0:2,1:3]}')
+    print (f'a[1:3,:]:\n{a[1:3,:]}')
+    print (f'a[:,1:3]:\n{a[:,1:3]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
