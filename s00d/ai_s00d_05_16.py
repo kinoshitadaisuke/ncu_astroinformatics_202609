0c03d1f1a9c44b735c77e6e9e809d66042ef2873
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:17 +0800

    adding "s00d/ai_s00d_05_16.py" on  2/Sep/2026

--- s00d/ai_s00d_05_16.py
+++ s00d/ai_s00d_05_16.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:24:20 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array (2x2 matrix)
-A = numpy.array ([ [5.0, 3.0], [6.0, 4.0] ])
+# main function
+def main ():
+    # making Numpy array (2x2 matrix)
+    A = numpy.array ([ [5.0, 3.0], [6.0, 4.0] ])
 
-# printing A
-print (f'A:\n{A}')
+    # printing A
+    print (f'A:\n{A}')
 
-# calculating inverse matrix of A
-B = numpy.linalg.inv (A)
+    # calculating inverse matrix of A
+    B = numpy.linalg.inv (A)
 
-# priting B
-print (f'B = A^-1:\n{B}')
+    # priting B
+    print (f'B = A^-1:\n{B}')
 
-# calculation of A @ B
-C = A @ B
+    # calculation of A @ B
+    C = A @ B
 
-# printing C
-print (f'C = A @ B:\n{C}')
+    # printing C
+    print (f'C = A @ B:\n{C}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
