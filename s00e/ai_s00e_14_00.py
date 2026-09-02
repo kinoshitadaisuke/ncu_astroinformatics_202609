6749c2c2a89ed42e0ba54f29b985bd63a0ceefc0
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:06:48 +0800

    adding "s00e/ai_s00e_14_00.py" on  2/Sep/2026

--- s00e/ai_s00e_14_00.py
+++ s00e/ai_s00e_14_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:24:42 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:58:04 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -20,102 +20,108 @@ import numpy
 import matplotlib.backends.backend_agg
 import matplotlib.figure
 
-# constructing a parser object
-descr  = f'Making a pie chart'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = f'Making a pie chart'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-i', '--input', default='', \
-                     help='input file name')
-parser.add_argument ('-o', '--output', default='', \
-                     help='output file name')
-parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
-                     help='resolution of plot in DPI (default: 225.0)')
+    # adding arguments
+    parser.add_argument ('-i', '--input', default='', \
+                         help='input file name')
+    parser.add_argument ('-o', '--output', default='', \
+                         help='output file name')
+    parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
+                         help='resolution of plot in DPI (default: 225.0)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-file_input     = args.input
-file_output    = args.output
-resolution_dpi = args.resolution
+    # parameters
+    file_input     = args.input
+    file_output    = args.output
+    resolution_dpi = args.resolution
 
-# if "file_input" is empty, then stop the script
-if (file_input == ''):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: need to specify input file name!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # if "file_input" is empty, then stop the script
+    if (file_input == ''):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: need to specify input file name!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# if "file_output" is empty, then stop the script
-if (file_output == ''):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: need to specify output file name!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # if "file_output" is empty, then stop the script
+    if (file_output == ''):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: need to specify output file name!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# making pathlib object
-path_input  = pathlib.Path (file_input)
-path_output = pathlib.Path (file_output)
+    # making pathlib object
+    path_input  = pathlib.Path (file_input)
+    path_output = pathlib.Path (file_output)
 
-# existence check of input file
-if not (path_input.exists ()):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: input file "{file_input}" does not exist!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # existence check of input file
+    if not (path_input.exists ()):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: input file "{file_input}" does not exist!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# existence check of output file
-if (path_output.exists ()):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: output file "{file_output}" already exists!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # existence check of output file
+    if (path_output.exists ()):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: output file "{file_output}" already exists!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# making empty lists for storing data
-list_element      = []
-list_massfraction = []
-    
-# opening input data file
-with open (file_input, 'r') as fh_in:
-    # reading file line-by-line
-    for line in fh_in:
-        # if the line starts with '#', then skip
-        if (line[0] == '#'):
-            continue
-        # splitting the line
-        (element, massfraction_str) = line.split ()
-        # converting string into float
-        massfraction = float (massfraction_str)
-        # appending data into lists
-        list_element.append (element)
-        list_massfraction.append (massfraction)
+    # making empty lists for storing data
+    list_element      = []
+    list_massfraction = []
 
-# making numpy arrays
-array_massfraction = numpy.array (list_massfraction)
+    # opening input data file
+    with open (file_input, 'r') as fh_in:
+        # reading file line-by-line
+        for line in fh_in:
+            # if the line starts with '#', then skip
+            if (line[0] == '#'):
+                continue
+            # splitting the line
+            (element, massfraction_str) = line.split ()
+            # converting string into float
+            massfraction = float (massfraction_str)
+            # appending data into lists
+            list_element.append (element)
+            list_massfraction.append (massfraction)
 
-# making a fig object
-fig = matplotlib.figure.Figure ()
+    # making numpy arrays
+    array_massfraction = numpy.array (list_massfraction)
 
-# making a canvas object
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    # making a fig object
+    fig = matplotlib.figure.Figure ()
 
-# making an axes object
-ax = fig.add_subplot (111)
+    # making a canvas object
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
 
-# making a pie chart
-ax.pie (array_massfraction, labels=list_element)
+    # making an axes object
+    ax = fig.add_subplot (111)
 
-# adding legend to the plot
-ax.legend ()
+    # making a pie chart
+    ax.pie (array_massfraction, labels=list_element)
 
-# saving a plot as a file
-fig.savefig (file_output, dpi=resolution_dpi)
+    # adding legend to the plot
+    ax.legend ()
+
+    # saving a plot as a file
+    fig.savefig (file_output, dpi=resolution_dpi)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
