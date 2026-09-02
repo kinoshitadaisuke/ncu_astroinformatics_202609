80e7120d66f2c5d7555c44517fae42a1380235c2
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:14 +0800

    adding "s00d/ai_s00d_04_05.py" on  2/Sep/2026

--- s00d/ai_s00d_04_05.py
+++ s00d/ai_s00d_04_05.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:22:37 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:53:35 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array
-a = numpy.array ([ [1.0, 2.0, 3.0], [100.0, 200.0, 300.0] ])
+# main function
+def main ():
+    # making a Numpy array
+    a = numpy.array ([ [1.0, 2.0, 3.0], [100.0, 200.0, 300.0] ])
 
-# printing Numpy array "a"
-print (f'a:')
-print (f'{a}')
+    # printing Numpy array "a"
+    print (f'a:')
+    print (f'{a}')
 
-# making a transposed array
-b = a.T
+    # making a transposed array
+    b = a.T
 
-# printing Numpy array "b"
-print (f'b:')
-print (f'{b}')
+    # printing Numpy array "b"
+    print (f'b:')
+    print (f'{b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
