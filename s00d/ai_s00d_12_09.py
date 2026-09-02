438052bb897a64ea6867138de504386e323bbc28
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:15 +0800

    adding "s00d/ai_s00d_12_09.py" on  2/Sep/2026

--- s00d/ai_s00d_12_09.py
+++ s00d/ai_s00d_12_09.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:31:07 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:41:14 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# output file name
-file_output = 'numpy_03.npy'
+# main function
+def main ():
+    # output file name
+    file_output = 'numpy_03.npy'
 
-# a list
-list_a = [ 5.0, 4.0, 3.0, 2.0, 1.0, 0.0, -1.0, -2.0, -3.0, -4.0 ]
+    # a list
+    list_a = [ 5.0, 4.0, 3.0, 2.0, 1.0, 0.0, -1.0, -2.0, -3.0, -4.0 ]
 
-# making a Numpy array from a list
-array_a = numpy.array (list_a)
+    # making a Numpy array from a list
+    array_a = numpy.array (list_a)
 
-# printing Numpy array
-print (f'{array_a}')
+    # printing Numpy array
+    print (f'{array_a}')
 
-# saving Numpy array into "npy" file
-numpy.save (file_output, array_a)
+    # saving Numpy array into "npy" file
+    numpy.save (file_output, array_a)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
