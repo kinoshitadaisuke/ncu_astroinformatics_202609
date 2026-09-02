f2c1cdfc2168158ab6975020750610016b554b11
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:22 +0800

    adding "s00d/ai_s00d_12_11.py" on  2/Sep/2026

--- s00d/ai_s00d_12_11.py
+++ s00d/ai_s00d_12_11.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:31:24 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:41:46 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# output file name
-file_output = 'numpy_04.npz'
+# main function
+def main ():
+    # output file name
+    file_output = 'numpy_04.npz'
 
-# lists
-list_c = [ -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0 ]
-list_d = [
-    [10.0, 20.0, 30.0],
-    [40.0, 50.0, 60.0],
-    [70.0, 80.0, 90.0],
-]
+    # lists
+    list_c = [ -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0 ]
+    list_d = [
+        [10.0, 20.0, 30.0],
+        [40.0, 50.0, 60.0],
+        [70.0, 80.0, 90.0],
+    ]
 
-# making Numpy arrays from lists
-array_c = numpy.array (list_c)
-array_d = numpy.array (list_d)
+    # making Numpy arrays from lists
+    array_c = numpy.array (list_c)
+    array_d = numpy.array (list_d)
 
-# printing Numpy arrays
-print (f'array_c:')
-print (f'{array_c}')
-print (f'array_d:')
-print (f'{array_d}')
+    # printing Numpy arrays
+    print (f'array_c:')
+    print (f'{array_c}')
+    print (f'array_d:')
+    print (f'{array_d}')
 
-# saving Numpy array into "npz" file
-numpy.savez (file_output, array_c=array_c, array_d=array_d)
+    # saving Numpy array into "npz" file
+    numpy.savez (file_output, array_c=array_c, array_d=array_d)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
