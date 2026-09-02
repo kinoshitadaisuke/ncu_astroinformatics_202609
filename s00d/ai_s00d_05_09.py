d512296a589eca56288ce8a3c8297efab22ea164
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:50 +0800

    adding "s00d/ai_s00d_05_09.py" on  2/Sep/2026

--- s00d/ai_s00d_05_09.py
+++ s00d/ai_s00d_05_09.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:22:25 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays
-a = numpy.array ([1.0, 2.0])
-b = numpy.array ([3.0, 4.0])
+# main function
+def main ():
+    # making Numpy arrays
+    a = numpy.array ([1.0, 2.0])
+    b = numpy.array ([3.0, 4.0])
 
-# printing a and b
-print (f'a                  = {a}')
-print (f'b                  = {b}')
+    # printing a and b
+    print (f'a                  = {a}')
+    print (f'b                  = {b}')
 
-# dot product of two vectors
-dot = numpy.dot (a, b)
+    # dot product of two vectors
+    dot = numpy.dot (a, b)
 
-# printing dot product
-print (f'numpy.dot (a, b)   = {dot}')
+    # printing dot product
+    print (f'numpy.dot (a, b)   = {dot}')
 
-# inner product of two vectors
-inner = numpy.inner (a, b)
+    # inner product of two vectors
+    inner = numpy.inner (a, b)
 
-# printing inner product
-print (f'numpy.inner (a, b) = {inner}')
+    # printing inner product
+    print (f'numpy.inner (a, b) = {inner}')
 
-# confirmation
-print (f'a0 x b0 + a1 x b1  = {a[0]*b[0]+a[1]*b[1]}')
+    # confirmation
+    print (f'a0 x b0 + a1 x b1  = {a[0]*b[0]+a[1]*b[1]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
