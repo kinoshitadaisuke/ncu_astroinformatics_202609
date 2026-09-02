73baf47b56c215d70db33733e44959100dc5ca75
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:07:15 +0800

    adding "s00e/ai_s00e_16_00.py" on  2/Sep/2026

--- s00e/ai_s00e_16_00.py
+++ s00e/ai_s00e_16_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/13 13:55:15 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 15:00:32 (UT+08:00) daisuke>
 #
 
 # importing PIL module
@@ -14,18 +14,24 @@ import numpy
 import matplotlib.backends.backend_agg
 import matplotlib.figure
 
-# JPEG file
-file_jpeg = 'campus_map.jpg'
+# main function
+def main ():
+    # JPEG file
+    file_jpeg = 'campus_map.jpg'
 
-# opening and reading an image from a file
-image = PIL.Image.open (file_jpeg)
+    # opening and reading an image from a file
+    image = PIL.Image.open (file_jpeg)
 
-# converting PIL Image object into Numpy array
-array_image = numpy.asarray (image)
+    # converting PIL Image object into Numpy array
+    array_image = numpy.asarray (image)
 
-# printing basic information about Numpy array
-print (f'ndim     = {array_image.ndim}')
-print (f'shape    = {array_image.shape}')
-print (f'size     = {array_image.size}')
-print (f'dtype    = {array_image.dtype}')
-print (f'itemsize = {array_image.itemsize}')
+    # printing basic information about Numpy array
+    print (f'ndim     = {array_image.ndim}')
+    print (f'shape    = {array_image.shape}')
+    print (f'size     = {array_image.size}')
+    print (f'dtype    = {array_image.dtype}')
+    print (f'itemsize = {array_image.itemsize}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
