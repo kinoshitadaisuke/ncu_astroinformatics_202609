fe3fa5f3b4f1fc242f07208d1437899b7a16619a
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:48 +0800

    adding "s00d/ai_s00d_03_07.py" on  2/Sep/2026

--- s00d/ai_s00d_03_07.py
+++ s00d/ai_s00d_03_07.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:21:35 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:51:36 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) using numpy.linspace ()
-array_r = numpy.linspace (1000, 2000, 21)
+# main function
+def main ():
+    # making a Numpy array (ndarray) using numpy.linspace ()
+    array_r = numpy.linspace (1000, 2000, 21)
 
-# printing Numpy array
-print (f'array_r:\n{array_r}')
+    # printing Numpy array
+    print (f'array_r:\n{array_r}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_r.ndim}')
-print (f'  size     = {array_r.size}')
-print (f'  shape    = {array_r.shape}')
-print (f'  dtype    = {array_r.dtype}')
-print (f'  itemsize = {array_r.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_r.ndim}')
+    print (f'  size     = {array_r.size}')
+    print (f'  shape    = {array_r.shape}')
+    print (f'  dtype    = {array_r.dtype}')
+    print (f'  itemsize = {array_r.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
