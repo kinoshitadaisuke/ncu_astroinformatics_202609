690bfc3d59dc5e1f8a5f5e077d5be7c213d164f1
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:36 +0800

    adding "s00d/ai_s00d_07_00.py" on  2/Sep/2026

--- s00d/ai_s00d_07_00.py
+++ s00d/ai_s00d_07_00.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:25:39 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:25:46 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.linspace (0.0, 10.0, 11)
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.linspace (0.0, 10.0, 11)
 
-# printing "a"
-print (f'a:\n{a}')
+    # printing "a"
+    print (f'a:\n{a}')
 
-# trying b = a
-b = a
+    # trying b = a
+    b = a
 
-# printing "b"
-print (f'b:\n{b}')
+    # printing "b"
+    print (f'b:\n{b}')
 
-# IDs of "a" and "b"
-print (f'id (a) = {id (a)}')
-print (f'id (b) = {id (b)}')
+    # IDs of "a" and "b"
+    print (f'id (a) = {id (a)}')
+    print (f'id (b) = {id (b)}')
 
-# changing "a[5]"
-a[5] += 10
+    # changing "a[5]"
+    a[5] += 10
 
-# printing "a"
-print (f'a:\n{a}')
+    # printing "a"
+    print (f'a:\n{a}')
 
-# printing "b"
-print (f'b:\n{b}')
+    # printing "b"
+    print (f'b:\n{b}')
 
-# IDs of "a" and "b"
-print (f'id (a) = {id (a)}')
-print (f'id (b) = {id (b)}')
+    # IDs of "a" and "b"
+    print (f'id (a) = {id (a)}')
+    print (f'id (b) = {id (b)}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
