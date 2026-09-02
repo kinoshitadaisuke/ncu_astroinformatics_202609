750b40b97960d43b5c1568a923d67db52d55007e
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:05:51 +0800

    adding "s00e/ai_s00e_09_02.py" on  2/Sep/2026

--- s00e/ai_s00e_09_02.py
+++ s00e/ai_s00e_09_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:22:00 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:52:36 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -13,37 +13,43 @@ import astropy.coordinates
 import astropy.time
 import astropy.units
 
-# constructing a parser object
-descr  = 'obtaining positions of the Sun and planets'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'obtaining positions of the Sun and planets'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-t', '--time', default='2000-01-01T12:00:00', \
-                     help='date/time in YYYY-MM-DDThh:mm:ss format')
+    # adding arguments
+    parser.add_argument ('-t', '--time', default='2000-01-01T12:00:00', \
+                         help='date/time in YYYY-MM-DDThh:mm:ss format')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-datetime = args.time
+    # parameters
+    datetime = args.time
 
-# setting for solar system ephemeris
-astropy.coordinates.solar_system_ephemeris.set ('jpl')
+    # setting for solar system ephemeris
+    astropy.coordinates.solar_system_ephemeris.set ('jpl')
 
-# time t in UTC
-t = astropy.time.Time (datetime, format='isot', scale='utc')
+    # time t in UTC
+    t = astropy.time.Time (datetime, format='isot', scale='utc')
 
-# getting positions of Sun, Mercury, Venus, Earth, and Mars
-sun     = astropy.coordinates.get_body_barycentric ('sun', t)
-mercury = astropy.coordinates.get_body_barycentric ('mercury', t)
-venus   = astropy.coordinates.get_body_barycentric ('venus', t)
-earth   = astropy.coordinates.get_body_barycentric ('earth', t)
-mars    = astropy.coordinates.get_body_barycentric ('mars', t)
+    # getting positions of Sun, Mercury, Venus, Earth, and Mars
+    sun     = astropy.coordinates.get_body_barycentric ('sun', t)
+    mercury = astropy.coordinates.get_body_barycentric ('mercury', t)
+    venus   = astropy.coordinates.get_body_barycentric ('venus', t)
+    earth   = astropy.coordinates.get_body_barycentric ('earth', t)
+    mars    = astropy.coordinates.get_body_barycentric ('mars', t)
 
-# printing positions of the Sun and planets
-print (f'Positions of the Sun and the planets at t = {t}')
-print (f'  Sun     : {sun}')
-print (f'  Mercury : {mercury}')
-print (f'  Venus   : {venus}')
-print (f'  Earth   : {earth}')
-print (f'  Mars    : {mars}')
+    # printing positions of the Sun and planets
+    print (f'Positions of the Sun and the planets at t = {t}')
+    print (f'  Sun     : {sun}')
+    print (f'  Mercury : {mercury}')
+    print (f'  Venus   : {venus}')
+    print (f'  Earth   : {earth}')
+    print (f'  Mars    : {mars}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
