24b14cbf7f4f4e04e93fcb1c7e079960b8ecddac
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:52:53 +0800

    adding "s00d/ai_s00d_12_03.py" on  2/Sep/2026

--- s00d/ai_s00d_12_03.py
+++ s00d/ai_s00d_12_03.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:30:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:39:33 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_00.data'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_00.data'
 
-# opening and reading file and storing data in a Numpy array
-data = numpy.loadtxt (file_input)
+    # opening and reading file and storing data in a Numpy array
+    data = numpy.loadtxt (file_input)
 
-# printing data
-print (f'{data}')
+    # printing data
+    print (f'{data}')
 
-# object type of the object "data"
-type_data = type (data)
+    # object type of the object "data"
+    type_data = type (data)
 
-# printing object type
-print (f'object type = {type_data}')
+    # printing object type
+    print (f'object type = {type_data}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
