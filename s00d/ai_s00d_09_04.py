c6398f2f3c276f74c5ed4e15a31f39a0f72dfff2
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:30 +0800

    adding "s00d/ai_s00d_09_04.py" on  2/Sep/2026

--- s00d/ai_s00d_09_04.py
+++ s00d/ai_s00d_09_04.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:27:33 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:30:02 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# SFC64 random number generator
-rng = numpy.random.Generator (numpy.random.SFC64 ())
+# main function
+def main ():
+    # SFC64 random number generator
+    rng = numpy.random.Generator (numpy.random.SFC64 ())
 
-# generating 10 random numbers of uniform distribution between 100 and 200
-array_x = rng.uniform (1000.0, 2000.0, 30)
+    # generating 10 random numbers of uniform distribution between 100 and 200
+    array_x = rng.uniform (1000.0, 2000.0, 30)
 
-# printing generated random numbers
-print (f'{array_x}')
+    # printing generated random numbers
+    print (f'{array_x}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
