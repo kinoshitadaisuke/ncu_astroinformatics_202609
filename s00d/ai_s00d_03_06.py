2d0642e8035463ed17d8c6c25e82e884392eec94
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:44 +0800

    adding "s00d/ai_s00d_03_06.py" on  2/Sep/2026

--- s00d/ai_s00d_03_06.py
+++ s00d/ai_s00d_03_06.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:21:27 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:51:20 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) using numpy.arange ()
-array_q = numpy.arange (0, 30, 2)
+# main function
+def main ():
+    # making a Numpy array (ndarray) using numpy.arange ()
+    array_q = numpy.arange (0, 30, 2)
 
-# printing Numpy array
-print (f'array_q:\n{array_q}')
+    # printing Numpy array
+    print (f'array_q:\n{array_q}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_q.ndim}')
-print (f'  size     = {array_q.size}')
-print (f'  shape    = {array_q.shape}')
-print (f'  dtype    = {array_q.dtype}')
-print (f'  itemsize = {array_q.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_q.ndim}')
+    print (f'  size     = {array_q.size}')
+    print (f'  shape    = {array_q.shape}')
+    print (f'  dtype    = {array_q.dtype}')
+    print (f'  itemsize = {array_q.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
