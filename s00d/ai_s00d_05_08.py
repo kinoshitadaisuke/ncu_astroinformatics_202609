1efbb05d9f59762188bfa33c460627e8f32ad0b5
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:46 +0800

    adding "s00d/ai_s00d_05_08.py" on  2/Sep/2026

--- s00d/ai_s00d_05_08.py
+++ s00d/ai_s00d_05_08.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:23:54 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:22:09 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays
-a = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
-b = numpy.array ([ [5.0, 6.0], [7.0, 8.0] ])
+# main function
+def main ():
+    # making Numpy arrays
+    a = numpy.array ([ [1.0, 2.0], [3.0, 4.0] ])
+    b = numpy.array ([ [5.0, 6.0], [7.0, 8.0] ])
 
-# printing a and b
-print (f'a:\n{a}')
-print (f'b:\n{b}')
+    # printing a and b
+    print (f'a:\n{a}')
+    print (f'b:\n{b}')
 
-# calculation
-# no need of using "for"
-c = a + b
+    # calculation
+    # no need of using "for"
+    c = a + b
 
-# printing c
-print (f'c = a + b:\n{c}')
+    # printing c
+    print (f'c = a + b:\n{c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
