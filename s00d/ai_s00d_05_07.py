f547c8bf9ab93cf2d2acf932a75ca932b6f08336
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:43 +0800

    adding "s00d/ai_s00d_05_07.py" on  2/Sep/2026

--- s00d/ai_s00d_05_07.py
+++ s00d/ai_s00d_05_07.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:23:47 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:21:53 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays using numpy.linspace ()
-a_deg = numpy.linspace (0.0, 180.0, 7)
+# main function
+def main ():
+    # making Numpy arrays using numpy.linspace ()
+    a_deg = numpy.linspace (0.0, 180.0, 7)
 
-# printing a_deg
-print (f'a_deg = {a_deg}')
+    # printing a_deg
+    print (f'a_deg = {a_deg}')
 
-# angle in radian
-a_rad = a_deg / 180.0 * numpy.pi
+    # angle in radian
+    a_rad = a_deg / 180.0 * numpy.pi
 
-# printing a_rad
-print (f'a_rad = {a_rad}')
+    # printing a_rad
+    print (f'a_rad = {a_rad}')
 
-# calculation
-# no need of using "for"
-sin_a = numpy.sin (a_rad)
-cos_a = numpy.cos (a_rad)
+    # calculation
+    # no need of using "for"
+    sin_a = numpy.sin (a_rad)
+    cos_a = numpy.cos (a_rad)
 
-# printing b
-print (f'sin (a) = {sin_a}')
-print (f'cos (a) = {cos_a}')
+    # printing b
+    print (f'sin (a) = {sin_a}')
+    print (f'cos (a) = {cos_a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
