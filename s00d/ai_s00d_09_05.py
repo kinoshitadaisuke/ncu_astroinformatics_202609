e49c1eb7fc13f3d87ed6e2c41c0420e36c1907d0
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:34 +0800

    adding "s00d/ai_s00d_09_05.py" on  2/Sep/2026

--- s00d/ai_s00d_09_05.py
+++ s00d/ai_s00d_09_05.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:27:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:30:20 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# random number generator
-rng = numpy.random.Generator (numpy.random.Philox ())
+# main function
+def main ():
+    # random number generator
+    rng = numpy.random.Generator (numpy.random.Philox ())
 
-# generating 100 random numbers of Gaussian distribution
-# of mean of 100.0 and standard deviation of 10.0
-array_x = rng.normal (100.0, 10.0, 100)
+    # generating 100 random numbers of Gaussian distribution
+    # of mean of 100.0 and standard deviation of 10.0
+    array_x = rng.normal (100.0, 10.0, 100)
 
-# printing generated random numbers
-print (f'{array_x}')
+    # printing generated random numbers
+    print (f'{array_x}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
