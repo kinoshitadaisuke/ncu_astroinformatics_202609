1e5b7a026c46e92c4c2de5d38561b6e808a53559
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:03:26 +0800

    adding "s00e/ai_s00e_00_00.py" on  2/Sep/2026

--- s00e/ai_s00e_00_00.py
+++ s00e/ai_s00e_00_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:15:49 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:38:32 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -19,53 +19,59 @@ import numpy
 # importing matplotlib module
 import matplotlib.pyplot
 
-# constructing a parser object
-descr  = 'A sample Matplotlib code using implicit pyplot interface'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'A sample Matplotlib code using implicit pyplot interface'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-o', '--output', default='sample.png', \
-                     help='output file name (default: sample.png)')
+    # adding arguments
+    parser.add_argument ('-o', '--output', default='sample.png', \
+                         help='output file name (default: sample.png)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-file_output = args.output
+    # parameters
+    file_output = args.output
 
-# making a pathlib object for output file
-path_output = pathlib.Path (file_output)
+    # making a pathlib object for output file
+    path_output = pathlib.Path (file_output)
 
-# check of existence of output file
-if (path_output.exists ()):
-    # printing a message
-    print (f'ERROR: output file "{file_output}" exists!')
-    # stopping the script
-    sys.exit (0)
+    # check of existence of output file
+    if (path_output.exists ()):
+        # printing a message
+        print (f'ERROR: output file "{file_output}" exists!')
+        # stopping the script
+        sys.exit (0)
 
-# check of extension of output file
-if not ( (path_output.suffix == '.eps') \
-         or (path_output.suffix == '.pdf') \
-         or (path_output.suffix == '.png') \
-         or (path_output.suffix == '.ps') ):
-    # printing a message
-    print (f'ERROR: output file must be either EPS or PDF or PNG or PS file.')
-    # stopping the script
-    sys.exit (0)
+    # check of extension of output file
+    if not ( (path_output.suffix == '.eps') \
+             or (path_output.suffix == '.pdf') \
+             or (path_output.suffix == '.png') \
+             or (path_output.suffix == '.ps') ):
+        # printing a message
+        print (f'ERROR: output file must be either EPS or PDF or PNG or PS file.')
+        # stopping the script
+        sys.exit (0)
 
-# data to be plotted
-data_x = numpy.linspace (0.0, +5.0, 10000)
-data_y = numpy.sin (data_x**data_x) / (data_x**data_x)
+    # data to be plotted
+    data_x = numpy.linspace (0.0, +5.0, 10000)
+    data_y = numpy.sin (data_x**data_x) / (data_x**data_x)
 
-#
-# for making a plot using implicit pyplot interface, we call some functions
-#
+    #
+    # for making a plot using implicit pyplot interface, we call some functions
+    #
 
-# plotting data using procedural pyplot interface
-matplotlib.pyplot.plot (data_x, data_y, label="Sample data")
+    # plotting data using procedural pyplot interface
+    matplotlib.pyplot.plot (data_x, data_y, label="Sample data")
 
-# adding legend to the plot
-matplotlib.pyplot.legend ()
+    # adding legend to the plot
+    matplotlib.pyplot.legend ()
 
-# saving a plot as a file
-matplotlib.pyplot.savefig (file_output)
+    # saving a plot as a file
+    matplotlib.pyplot.savefig (file_output)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
