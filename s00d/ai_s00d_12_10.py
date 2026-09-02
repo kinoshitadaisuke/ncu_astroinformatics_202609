6aca49eeda0f7bfbc6f0a68f3db72aab5f060086
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:18 +0800

    adding "s00d/ai_s00d_12_10.py" on  2/Sep/2026

--- s00d/ai_s00d_12_10.py
+++ s00d/ai_s00d_12_10.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:31:16 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:41:29 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_03.npy'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_03.npy'
 
-# loading a Numpy array from npy file
-array_b = numpy.load (file_input)
+    # loading a Numpy array from npy file
+    array_b = numpy.load (file_input)
 
-# printing Numpy array
-print (f'{array_b}')
+    # printing Numpy array
+    print (f'{array_b}')
 
-# object type of the object "array_b"
-type_array_b = type (array_b)
+    # object type of the object "array_b"
+    type_array_b = type (array_b)
 
-# printing object type
-print (f'object type = {type_array_b}')
+    # printing object type
+    print (f'object type = {type_array_b}')
 
-# printing information of Numpy array
-print (f'information:')
-print (f'  ndim     = {array_b.ndim}')
-print (f'  size     = {array_b.size}')
-print (f'  shape    = {array_b.shape}')
-print (f'  dtype    = {array_b.dtype}')
-print (f'  itemsize = {array_b.itemsize} byte')
+    # printing information of Numpy array
+    print (f'information:')
+    print (f'  ndim     = {array_b.ndim}')
+    print (f'  size     = {array_b.size}')
+    print (f'  shape    = {array_b.shape}')
+    print (f'  dtype    = {array_b.dtype}')
+    print (f'  itemsize = {array_b.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
