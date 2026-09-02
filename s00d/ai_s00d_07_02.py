a93c9c5a46f6cf6be609613a80efc0e6c29e0b63
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:43 +0800

    adding "s00d/ai_s00d_07_02.py" on  2/Sep/2026

--- s00d/ai_s00d_07_02.py
+++ s00d/ai_s00d_07_02.py
@@ -1,50 +1,56 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:25:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:26:18 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.linspace (0.0, 10.0, 11)
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.linspace (0.0, 10.0, 11)
 
-# printing values of "a", "b", and "c"
-print (f'a = {a}')
+    # printing values of "a", "b", and "c"
+    print (f'a = {a}')
 
-# trying b = a
-print (f'making array "b" by "b = a"...')
-b = a
+    # trying b = a
+    print (f'making array "b" by "b = a"...')
+    b = a
 
-# trying a.copy ()
-print (f'making array "c" by "c = a.copy ()"...')
-c = a.copy ()
+    # trying a.copy ()
+    print (f'making array "c" by "c = a.copy ()"...')
+    c = a.copy ()
 
-# printing values of "a", "b", and "c"
-print (f'a = {a}')
-print (f'b = {b}')
-print (f'c = {c}')
+    # printing values of "a", "b", and "c"
+    print (f'a = {a}')
+    print (f'b = {b}')
+    print (f'c = {c}')
 
-# IDs of "a", "b", and "c"
-print (f'id (a) = {id (a)}')
-print (f'id (b) = {id (b)}')
-print (f'id (c) = {id (c)}')
+    # IDs of "a", "b", and "c"
+    print (f'id (a) = {id (a)}')
+    print (f'id (b) = {id (b)}')
+    print (f'id (c) = {id (c)}')
 
-# modifying array "a"
-print (f'carrying out "a[3] += 5.0"...')
-a[3] += 5.0
+    # modifying array "a"
+    print (f'carrying out "a[3] += 5.0"...')
+    a[3] += 5.0
 
-# printing values of "a", "b", and "c"
-print (f'a = {a}')
-print (f'b = {b}')
-print (f'c = {c}')
+    # printing values of "a", "b", and "c"
+    print (f'a = {a}')
+    print (f'b = {b}')
+    print (f'c = {c}')
 
-# modifying array "c"
-print (f'carrying out "c[7] -= 10.0"...')
-c[7] -= 10.0
+    # modifying array "c"
+    print (f'carrying out "c[7] -= 10.0"...')
+    c[7] -= 10.0
 
-# printing values of "a", "b", and "c"
-print (f'a = {a}')
-print (f'b = {b}')
-print (f'c = {c}')
+    # printing values of "a", "b", and "c"
+    print (f'a = {a}')
+    print (f'b = {b}')
+    print (f'c = {c}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
