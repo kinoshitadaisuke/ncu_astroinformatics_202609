5ee83536cb63625cf3e338774878a260d21b0b8d
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:41 +0800

    adding "s00d/ai_s00d_03_05.py" on  2/Sep/2026

--- s00d/ai_s00d_03_05.py
+++ s00d/ai_s00d_03_05.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:21:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:51:04 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) of 3x3x3 shape
-# with all the elements initialised by one.
-array_p = numpy.ones ( (3, 3, 3) )
+# main function
+def main ():
+    # making a Numpy array (ndarray) of 3x3x3 shape
+    # with all the elements initialised by one.
+    array_p = numpy.ones ( (3, 3, 3) )
 
-# printing Numpy array
-print (f'array_p:\n{array_p}')
+    # printing Numpy array
+    print (f'array_p:\n{array_p}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_p.ndim}')
-print (f'  size     = {array_p.size}')
-print (f'  shape    = {array_p.shape}')
-print (f'  dtype    = {array_p.dtype}')
-print (f'  itemsize = {array_p.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_p.ndim}')
+    print (f'  size     = {array_p.size}')
+    print (f'  shape    = {array_p.shape}')
+    print (f'  dtype    = {array_p.dtype}')
+    print (f'  itemsize = {array_p.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
