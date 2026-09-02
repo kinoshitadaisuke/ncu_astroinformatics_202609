a03fa910b3b319bf11484e5088362b435b0a9b04
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:59 +0800

    adding "s00d/ai_s00d_04_01.py" on  2/Sep/2026

--- s00d/ai_s00d_04_01.py
+++ s00d/ai_s00d_04_01.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:22:06 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:52:29 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays using numpy.linspace ()
-array_u = numpy.linspace (0, 10, 11)
-array_v = numpy.linspace (11, 15, 9)
+# main function
+def main ():
+    # making Numpy arrays using numpy.linspace ()
+    array_u = numpy.linspace (0, 10, 11)
+    array_v = numpy.linspace (11, 15, 9)
 
-# printing Numpy array
-print (f'array_u:\n{array_u}')
+    # printing Numpy array
+    print (f'array_u:\n{array_u}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_u.ndim}')
-print (f'  size     = {array_u.size}')
-print (f'  shape    = {array_u.shape}')
-print (f'  dtype    = {array_u.dtype}')
-print (f'  itemsize = {array_u.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_u.ndim}')
+    print (f'  size     = {array_u.size}')
+    print (f'  shape    = {array_u.shape}')
+    print (f'  dtype    = {array_u.dtype}')
+    print (f'  itemsize = {array_u.itemsize} byte')
 
-# printing Numpy array
-print (f'array_v:\n{array_v}')
+    # printing Numpy array
+    print (f'array_v:\n{array_v}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_v.ndim}')
-print (f'  size     = {array_v.size}')
-print (f'  shape    = {array_v.shape}')
-print (f'  dtype    = {array_v.dtype}')
-print (f'  itemsize = {array_v.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_v.ndim}')
+    print (f'  size     = {array_v.size}')
+    print (f'  shape    = {array_v.shape}')
+    print (f'  dtype    = {array_v.dtype}')
+    print (f'  itemsize = {array_v.itemsize} byte')
 
-# concatenating array_u and array_v, and creating array_w
-array_w = numpy.concatenate ([array_u, array_v])
+    # concatenating array_u and array_v, and creating array_w
+    array_w = numpy.concatenate ([array_u, array_v])
 
-# printing Numpy array
-print (f'array_w:\n{array_w}')
+    # printing Numpy array
+    print (f'array_w:\n{array_w}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_w.ndim}')
-print (f'  size     = {array_w.size}')
-print (f'  shape    = {array_w.shape}')
-print (f'  dtype    = {array_w.dtype}')
-print (f'  itemsize = {array_w.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_w.ndim}')
+    print (f'  size     = {array_w.size}')
+    print (f'  shape    = {array_w.shape}')
+    print (f'  dtype    = {array_w.dtype}')
+    print (f'  itemsize = {array_w.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
