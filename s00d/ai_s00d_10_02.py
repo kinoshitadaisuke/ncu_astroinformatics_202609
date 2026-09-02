d7ce09dce29d1d0b4652c63f4bc07712a7800454
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:44 +0800

    adding "s00d/ai_s00d_10_02.py" on  2/Sep/2026

--- s00d/ai_s00d_10_02.py
+++ s00d/ai_s00d_10_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:28:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:34:08 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -10,36 +10,42 @@ import numpy
 # importing statistics module
 import statistics
 
-# random number generator
-rng = numpy.random.Generator (numpy.random.PCG64DXSM ())
+# main function
+def main ():
+    # random number generator
+    rng = numpy.random.Generator (numpy.random.PCG64DXSM ())
 
-# generating 10^8 random numbers of Gaussian distribution 
-# of mean = 1000.0 and stddev = 25.0
-array_x = rng.normal (1000.0, 25.0, 10**8)
+    # generating 10^8 random numbers of Gaussian distribution 
+    # of mean = 1000.0 and stddev = 25.0
+    array_x = rng.normal (1000.0, 25.0, 10**8)
 
-# printing generated random numbers
-print (f'{array_x}')
+    # printing generated random numbers
+    print (f'{array_x}')
 
-# printing number of data
-print (f'number of data = {len (array_x):g}')
+    # printing number of data
+    print (f'number of data = {len (array_x):g}')
 
-# statistical values calculated by numpy module
-mean_n     = numpy.mean (array_x)
-median_n   = numpy.median (array_x)
-variance_n = numpy.var (array_x)
-stddev_n   = numpy.std (array_x)
+    # statistical values calculated by numpy module
+    mean_n     = numpy.mean (array_x)
+    median_n   = numpy.median (array_x)
+    variance_n = numpy.var (array_x)
+    stddev_n   = numpy.std (array_x)
 
-# printing statistical values
-print (f'statistical values by Numpy:')
-print (f'  mean     = {mean_n:10.3f}')
-print (f'  median   = {median_n:10.3f}')
-print (f'  variance = {variance_n:10.3f}')
-print (f'  stddev   = {stddev_n:10.3f}')
+    # printing statistical values
+    print (f'statistical values by Numpy:')
+    print (f'  mean     = {mean_n:10.3f}')
+    print (f'  median   = {median_n:10.3f}')
+    print (f'  variance = {variance_n:10.3f}')
+    print (f'  stddev   = {stddev_n:10.3f}')
 
-# constructing a histogram
-hist = numpy.histogram (array_x, bins=40, range=(900.0, 1100.0))
+    # constructing a histogram
+    hist = numpy.histogram (array_x, bins=40, range=(900.0, 1100.0))
 
-# printing histogram
-print (f'histogram:')
-for i in range (40):
-    print (f'  {hist[1][i]:6.1f}-{hist[1][i]+10:6.1f} : {hist[0][i]:8d}')
+    # printing histogram
+    print (f'histogram:')
+    for i in range (40):
+        print (f'  {hist[1][i]:6.1f}-{hist[1][i]+10:6.1f} : {hist[0][i]:8d}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
