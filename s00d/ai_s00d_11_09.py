caa2e9ce7921b6869e017f14ff041b7ea5264119
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:52:24 +0800

    adding "s00d/ai_s00d_11_09.py" on  2/Sep/2026

--- s00d/ai_s00d_11_09.py
+++ s00d/ai_s00d_11_09.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:29:25 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:37:06 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 import numpy.ma
 
-# data
-data = numpy.array ([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, \
-                     5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5])
+# main function
+def main ():
+    # data
+    data = numpy.array ([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, \
+                         5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5])
 
-# printing data
-print ("data:")
-print (data)
+    # printing data
+    print ("data:")
+    print (data)
 
-# making a mask using an operator ">="
-mask = data >= 7.0
+    # making a mask using an operator ">="
+    mask = data >= 7.0
 
-# printing mask
-print ("mask:")
-print (mask)
+    # printing mask
+    print ("mask:")
+    print (mask)
 
-# making a masked array
-masked_data = numpy.ma.array (data, mask=mask)
+    # making a masked array
+    masked_data = numpy.ma.array (data, mask=mask)
 
-# printing a masked array
-print ("masked_data:")
-print (masked_data)
+    # printing a masked array
+    print ("masked_data:")
+    print (masked_data)
 
-# making a mask using an operator "<="
-mask2 = data <= 3.0
+    # making a mask using an operator "<="
+    mask2 = data <= 3.0
 
-# printing mask
-print ("mask2:")
-print (mask2)
+    # printing mask
+    print ("mask2:")
+    print (mask2)
 
-# making a masked array
-masked_data2 = numpy.ma.array (data, mask=mask2)
+    # making a masked array
+    masked_data2 = numpy.ma.array (data, mask=mask2)
 
-# printing a masked array
-print ("masked_data:")
-print (masked_data2)
+    # printing a masked array
+    print ("masked_data:")
+    print (masked_data2)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
