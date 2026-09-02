d6adcf41d6694b434c19df8a2d89ed431b0cbd36
Daisuke <mhdf23888@gmail.com>
2026-09-02 14:33:50 +0800

    adding "s00e/ai_s00e_11_01.py" on  2/Sep/2026

---
+++ s00e/ai_s00e_11_01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/10 20:23:04 (UT+08:00) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# frame rate
+framerate = 30
+
+# number of threads
+n_thread = 1
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -i solsys_3d1/%06d.png -start_number 0' \
+    + f' -framerate {framerate} -an -vcodec libx264 -pix_fmt yuv420p' \
+    + f' -threads {n_thread}'
+
+# output file name
+file_output = 'solsys_3d1.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
