65e25901fa8d0f1be2577997a2222ffecb9fe406
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:21 +0800

    adding "s00d/ai_s00d_06_00.py" on  2/Sep/2026

--- s00d/ai_s00d_06_00.py
+++ s00d/ai_s00d_06_00.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:25:07 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:24:39 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.linspace (0.0, 10.0, 11)
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.linspace (0.0, 10.0, 11)
 
-# printing A
-print (f'a:\n{a}')
+    # printing A
+    print (f'a:\n{a}')
 
-# accessing to an element by indexing
-print (f'a[0]  = {a[0]}')
-print (f'a[1]  = {a[1]}')
-print (f'a[5]  = {a[5]}')
-print (f'a[-1] = {a[-1]}')
-print (f'a[-3] = {a[-3]}')
+    # accessing to an element by indexing
+    print (f'a[0]  = {a[0]}')
+    print (f'a[1]  = {a[1]}')
+    print (f'a[5]  = {a[5]}')
+    print (f'a[-1] = {a[-1]}')
+    print (f'a[-3] = {a[-3]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
