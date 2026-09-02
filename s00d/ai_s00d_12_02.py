557f593b632515834f7f2ade6921a0b132d61138
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:52:49 +0800

    adding "s00d/ai_s00d_12_02.py" on  2/Sep/2026

--- s00d/ai_s00d_12_02.py
+++ s00d/ai_s00d_12_02.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:30:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:39:17 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_00.data'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_00.data'
 
-# opening and reading file and storing data in a Numpy array
-data = numpy.loadtxt (file_input)
+    # opening and reading file and storing data in a Numpy array
+    data = numpy.loadtxt (file_input)
 
-# printing data
-print (f'{data}')
+    # printing data
+    print (f'{data}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
