1b0f8503d18d2dfc848007bdf10e5b6f7b7124c1
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:50:28 +0800

    adding "s00d/ai_s00d_06_02.py" on  2/Sep/2026

--- s00d/ai_s00d_06_02.py
+++ s00d/ai_s00d_06_02.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:25:20 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:25:11 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.linspace (0.0, 10.0, 11)
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.linspace (0.0, 10.0, 11)
 
-# printing A
-print (f'a:\n{a}')
+    # printing A
+    print (f'a:\n{a}')
 
-# accessing to an element by indexing
-print (f'a[2:5] = {a[2:5]}')
-print (f'a[6:]  = {a[6:]}')
-print (f'a[:3]  = {a[:3]}')
-print (f'a[:]   = {a[:]}')
-print (f'a[-3:] = {a[-3:]}')
+    # accessing to an element by indexing
+    print (f'a[2:5] = {a[2:5]}')
+    print (f'a[6:]  = {a[6:]}')
+    print (f'a[:3]  = {a[:3]}')
+    print (f'a[:]   = {a[:]}')
+    print (f'a[-3:] = {a[-3:]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
