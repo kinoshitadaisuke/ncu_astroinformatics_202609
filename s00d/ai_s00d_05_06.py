923d5a3afe8da4e6d0dae85e46cda2b809d92c0f
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:40 +0800

    adding "s00d/ai_s00d_05_06.py" on  2/Sep/2026

--- s00d/ai_s00d_05_06.py
+++ s00d/ai_s00d_05_06.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:23:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:21:37 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays using numpy.logspace ()
-a = numpy.logspace (-5.0, 5.0, 11)
+# main function
+def main ():
+    # making Numpy arrays using numpy.logspace ()
+    a = numpy.logspace (-5.0, 5.0, 11)
 
-# printing a
-print (f'a = {a}')
+    # printing a
+    print (f'a = {a}')
 
-# calculation
-# no need of using "for"
-b = numpy.log10 (a)
+    # calculation
+    # no need of using "for"
+    b = numpy.log10 (a)
 
-# printing b
-print (f'b = log10 (a) = {b}')
+    # printing b
+    print (f'b = log10 (a) = {b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
