fcee31ade9b82b814f29d3ded0743bc663c43fd9
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:23 +0800

    adding "s00d/ai_s00d_03_00.py" on  2/Sep/2026

--- s00d/ai_s00d_03_00.py
+++ s00d/ai_s00d_03_00.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:20:40 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:49:38 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) with 10 elements all equal to zeros
-array_k = numpy.zeros ( (10,) )
+# main function
+def main ():
+    # making a Numpy array (ndarray) with 10 elements all equal to zeros
+    array_k = numpy.zeros ( (10,) )
 
-# printing Numpy array
-print (f'array_k:\n{array_k}')
+    # printing Numpy array
+    print (f'array_k:\n{array_k}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_k.ndim}')
-print (f'  size     = {array_k.size}')
-print (f'  shape    = {array_k.shape}')
-print (f'  dtype    = {array_k.dtype}')
-print (f'  itemsize = {array_k.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_k.ndim}')
+    print (f'  size     = {array_k.size}')
+    print (f'  shape    = {array_k.shape}')
+    print (f'  dtype    = {array_k.dtype}')
+    print (f'  itemsize = {array_k.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
