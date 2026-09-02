13ed683e7e4505fd3bc2ea43f20a4a8bc73b441d
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:59 +0800

    adding "s00d/ai_s00d_11_02.py" on  2/Sep/2026

--- s00d/ai_s00d_11_02.py
+++ s00d/ai_s00d_11_02.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:28:41 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:35:18 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# sample Numpy array
-a = numpy.array ([10.0, 10.1, 9.9, 10.2, 9.8, 10.3, 9.7, \
-                  300.0, 10.0, 10.0, 9.9, 9.9, 10.1, 10.1, 10.0])
+# main function
+def main ():
+    # sample Numpy array
+    a = numpy.array ([10.0, 10.1, 9.9, 10.2, 9.8, 10.3, 9.7, \
+                      300.0, 10.0, 10.0, 9.9, 9.9, 10.1, 10.1, 10.0])
 
-# making a mask
-mask = numpy.array ([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+    # making a mask
+    mask = numpy.array ([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
 
-# making a masked array
-a_masked = numpy.ma.array (a, mask=mask)
+    # making a masked array
+    a_masked = numpy.ma.array (a, mask=mask)
 
-# printing array "a"
-print (f'a:')
-print (f'{a}')
+    # printing array "a"
+    print (f'a:')
+    print (f'{a}')
 
-# printing mask "mask"
-print (f'mask:')
-print (f'{mask}')
+    # printing mask "mask"
+    print (f'mask:')
+    print (f'{mask}')
 
-# printing masked array "a_masked"
-print (f'a_masked:')
-print (f'{a_masked}')
+    # printing masked array "a_masked"
+    print (f'a_masked:')
+    print (f'{a_masked}')
 
-# calculation of mean of "a"
-mean_a = numpy.mean (a)
+    # calculation of mean of "a"
+    mean_a = numpy.mean (a)
 
-# calculation of mean of "a_masked"
-mean_a_masked = numpy.ma.mean (a_masked)
+    # calculation of mean of "a_masked"
+    mean_a_masked = numpy.ma.mean (a_masked)
 
-# printing mean values
-print (f'numpy.mean (a)           = {mean_a}')
-print (f'numpy.ma.mean (a_masked) = {mean_a_masked}')
+    # printing mean values
+    print (f'numpy.mean (a)           = {mean_a}')
+    print (f'numpy.ma.mean (a_masked) = {mean_a_masked}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
