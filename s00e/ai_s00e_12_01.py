7dc85a3dd5f1d2c181326412629c9d3afc94bbd3
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:06:34 +0800

    adding "s00e/ai_s00e_12_01.py" on  2/Sep/2026

--- s00e/ai_s00e_12_01.py
+++ s00e/ai_s00e_12_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:23:59 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:56:23 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -26,108 +26,114 @@ import astropy.time
 # importing astroquery module
 import astroquery.jplhorizons
 
-# constructing a parser object
-descr  = f'Position of the Moon on Az-El plot'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = f'Position of the Moon on Az-El plot'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-c', '--obscode', default='D35', \
-                     help='observatory code (default: D35)')
-parser.add_argument ('-o', '--output', default='', \
-                     help='output file name')
-parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
-                     help='resolution of plot in DPI (default: 225.0)')
-parser.add_argument ('-t', '--time', default='2000-01-01T12:00:00', \
-                     help='start date/time in YYYY-MM-DDThh:mm:ss format')
+    # adding arguments
+    parser.add_argument ('-c', '--obscode', default='D35', \
+                         help='observatory code (default: D35)')
+    parser.add_argument ('-o', '--output', default='', \
+                         help='output file name')
+    parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
+                         help='resolution of plot in DPI (default: 225.0)')
+    parser.add_argument ('-t', '--time', default='2000-01-01T12:00:00', \
+                         help='start date/time in YYYY-MM-DDThh:mm:ss format')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-obscode        = args.obscode
-file_output    = args.output
-resolution_dpi = args.resolution
-datetime_start = args.time
+    # parameters
+    obscode        = args.obscode
+    file_output    = args.output
+    resolution_dpi = args.resolution
+    datetime_start = args.time
 
-# if "file_output" is empty, then stop the script
-if (file_output == ''):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: need to specify output file name!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # if "file_output" is empty, then stop the script
+    if (file_output == ''):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: need to specify output file name!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# making pathlib object
-path_output = pathlib.Path (file_output)
+    # making pathlib object
+    path_output = pathlib.Path (file_output)
 
-# existence check of output file
-if (path_output.exists ()):
-    # printing message
-    print (f'ERROR:')
-    print (f'ERROR: output file "{file_output}" already exists!')
-    print (f'ERROR:')
-    # stopping the script
-    sys.exit (0)
+    # existence check of output file
+    if (path_output.exists ()):
+        # printing message
+        print (f'ERROR:')
+        print (f'ERROR: output file "{file_output}" already exists!')
+        print (f'ERROR:')
+        # stopping the script
+        sys.exit (0)
 
-# time
-t_start = astropy.time.Time (datetime_start, format='isot', scale='utc')
+    # time
+    t_start = astropy.time.Time (datetime_start, format='isot', scale='utc')
 
-# setting a query
-query_moon = astroquery.jplhorizons.Horizons (id_type=None, id='301', \
-                                              location=f'{obscode}', \
-                                              epochs=t_start.jd)
+    # setting a query
+    query_moon = astroquery.jplhorizons.Horizons (id_type=None, id='301', \
+                                                  location=f'{obscode}', \
+                                                  epochs=t_start.jd)
 
-# getting the position of the Moon
-eph_moon = query_moon.ephemerides ()
+    # getting the position of the Moon
+    eph_moon = query_moon.ephemerides ()
 
-# azimuth and elevation of the Moon
-moon_az_deg = eph_moon["AZ"][0]
-moon_el_deg = eph_moon["EL"][0]
+    # azimuth and elevation of the Moon
+    moon_az_deg = eph_moon["AZ"][0]
+    moon_el_deg = eph_moon["EL"][0]
 
-# printing the position of the Moon
-print (f'Date/Time: {t_start.iso}')
-print (f'  Moon:')
-print (f'    AZ = {moon_az_deg} [deg]')
-print (f'    EL = {moon_el_deg} [deg]')
+    # printing the position of the Moon
+    print (f'Date/Time: {t_start.iso}')
+    print (f'  Moon:')
+    print (f'    AZ = {moon_az_deg} [deg]')
+    print (f'    EL = {moon_el_deg} [deg]')
 
-# making a fig object
-fig = matplotlib.figure.Figure ()
+    # making a fig object
+    fig = matplotlib.figure.Figure ()
 
-# making a canvas object
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    # making a canvas object
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
 
-# making an axes object
-ax = fig.add_subplot (111, projection='polar')
+    # making an axes object
+    ax = fig.add_subplot (111, projection='polar')
 
-# coordinate settings
-ax.set_rlim (0.0, 90.0)
-ax.grid (True)
-ax.set_rgrids ( numpy.linspace (0.0, 90.0, 10),
-                labels=('') * 10,
-                fontsize=12)
-ax.set_thetagrids ( numpy.linspace (0.0, 315.0, 8),
-                    labels=('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'),
-                    fontsize=16)
-ax.set_theta_zero_location ('N')
+    # coordinate settings
+    ax.set_rlim (0.0, 90.0)
+    ax.grid (True)
+    ax.set_rgrids ( numpy.linspace (0.0, 90.0, 10),
+                    labels=('') * 10,
+                    fontsize=12)
+    ax.set_thetagrids ( numpy.linspace (0.0, 315.0, 8),
+                        labels=('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'),
+                        fontsize=16)
+    ax.set_theta_zero_location ('N')
 
-# r and theta
-moon_r     = 90.0 - moon_el_deg
-moon_theta = numpy.deg2rad (moon_az_deg)
+    # r and theta
+    moon_r     = 90.0 - moon_el_deg
+    moon_theta = numpy.deg2rad (moon_az_deg)
 
-# plotting data
-ax.plot (moon_theta, moon_r, \
-         linestyle='None', marker='o', markersize=15, color='yellow', \
-         label='Moon')
+    # plotting data
+    ax.plot (moon_theta, moon_r, \
+             linestyle='None', marker='o', markersize=15, color='yellow', \
+             label='Moon')
 
-# title
-ax.set_title (f'Sky on {t_start.iso} at {obscode}')
+    # title
+    ax.set_title (f'Sky on {t_start.iso} at {obscode}')
 
-# legend
-a = 0.0
-legend_x = 0.5 * numpy.cos (a) + 0.50
-legend_y = 0.5 * numpy.sin (a) + 0.45
-ax.legend (loc='upper left', bbox_to_anchor=(legend_x, legend_y))
+    # legend
+    a = 0.0
+    legend_x = 0.5 * numpy.cos (a) + 0.50
+    legend_y = 0.5 * numpy.sin (a) + 0.45
+    ax.legend (loc='upper left', bbox_to_anchor=(legend_x, legend_y))
 
-# saving the figure to a file
-fig.savefig (file_output, dpi=resolution_dpi)
+    # saving the figure to a file
+    fig.savefig (file_output, dpi=resolution_dpi)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
