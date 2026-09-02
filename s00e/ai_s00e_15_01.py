271cfd74a73a656ca1264dd045a8c5b7fc0fc9cf
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:06:58 +0800

    adding "s00e/ai_s00e_15_01.py" on  2/Sep/2026

--- s00e/ai_s00e_15_01.py
+++ s00e/ai_s00e_15_01.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:25:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:58:53 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.fits
 import astropy.wcs
 
-# input FITS file name
-file_fits = 'hltau_alma.fits'
+# main function
+def main ():
+    # input FITS file name
+    file_fits = 'hltau_alma.fits'
 
-# opening FITS file
-with astropy.io.fits.open (file_fits) as hdu_list:
-    # printing basic information of FITS file
-    print (hdu_list.info ())
-    # reading FITS header, WCS information, and image data
-    image  = hdu_list[0].data
-    # printing image data
-    print (f'shape of image data')
-    print (f'  ndim  = {image.ndim}')
-    print (f'  shape = {image.shape}')
-    print (f'image data:')
-    print (image)
+    # opening FITS file
+    with astropy.io.fits.open (file_fits) as hdu_list:
+        # printing basic information of FITS file
+        print (hdu_list.info ())
+        # reading FITS header, WCS information, and image data
+        image  = hdu_list[0].data
+        # printing image data
+        print (f'shape of image data')
+        print (f'  ndim  = {image.ndim}')
+        print (f'  shape = {image.shape}')
+        print (f'image data:')
+        print (image)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
