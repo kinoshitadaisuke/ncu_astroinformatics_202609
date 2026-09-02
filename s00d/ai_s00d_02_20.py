7b0e4a5ddeb1725246a86f984d39988879c09076
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:48:15 +0800

    adding "s00d/ai_s00d_02_20.py" on  2/Sep/2026

--- s00d/ai_s00d_02_20.py
+++ s00d/ai_s00d_02_20.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:20:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:48:59 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) with a specified data type
-# numpy.dtype ('U10') : 10-character Unicode string
-array_j = numpy.array (['Ceres', 'Pallas', 'Juno', 'Vesta', 'Astraea', \
-                        'Hebe', 'Iris', 'Flora', 'Metis', 'Hygiea'], \
-                       dtype=numpy.dtype ('U10') )
+# main function
+def main ():
+    # making a Numpy array (ndarray) with a specified data type
+    # numpy.dtype ('U10') : 10-character Unicode string
+    array_j = numpy.array (['Ceres', 'Pallas', 'Juno', 'Vesta', 'Astraea', \
+                            'Hebe', 'Iris', 'Flora', 'Metis', 'Hygiea'], \
+                           dtype=numpy.dtype ('U10') )
 
-# printing Numpy array
-print (f'array_j:\n{array_j}')
+    # printing Numpy array
+    print (f'array_j:\n{array_j}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_j.ndim}')
-print (f'  size     = {array_j.size}')
-print (f'  shape    = {array_j.shape}')
-print (f'  dtype    = {array_j.dtype}')
-print (f'  itemsize = {array_j.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_j.ndim}')
+    print (f'  size     = {array_j.size}')
+    print (f'  shape    = {array_j.shape}')
+    print (f'  dtype    = {array_j.dtype}')
+    print (f'  itemsize = {array_j.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
