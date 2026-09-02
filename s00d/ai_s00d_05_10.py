81971958cfe55f12a85f7bbb198974b7f5f2aebf
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:49:53 +0800

    adding "s00d/ai_s00d_05_10.py" on  2/Sep/2026

--- s00d/ai_s00d_05_10.py
+++ s00d/ai_s00d_05_10.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:24:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:22:41 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy arrays
-a = numpy.array ([1.0, 1.0, 0.0])
-b = numpy.array ([-1.0, 3.0, 0.0])
+# main function
+def main ():
+    # making Numpy arrays
+    a = numpy.array ([1.0, 1.0, 0.0])
+    b = numpy.array ([-1.0, 3.0, 0.0])
 
-# printing a and b
-print (f'a     = {a}')
-print (f'b     = {b}')
+    # printing a and b
+    print (f'a     = {a}')
+    print (f'b     = {b}')
 
-# dot product of two vectors
-dot = numpy.dot (a, b)
+    # dot product of two vectors
+    dot = numpy.dot (a, b)
 
-# printing dot product
-print (f'dot   = {dot}')
+    # printing dot product
+    print (f'dot   = {dot}')
 
-# inner product of two vectors
-inner = numpy.inner (a, b)
+    # inner product of two vectors
+    inner = numpy.inner (a, b)
 
-# printing inner product
-print (f'inner = {inner}')
+    # printing inner product
+    print (f'inner = {inner}')
 
-# cross product of two vectors
-cross = numpy.cross (a, b)
+    # cross product of two vectors
+    cross = numpy.cross (a, b)
 
-# printing cross product
-print (f'cross = {cross}')
+    # printing cross product
+    print (f'cross = {cross}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
