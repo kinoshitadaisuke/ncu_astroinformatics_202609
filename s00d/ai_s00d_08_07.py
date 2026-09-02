8d9a1643ad3b46cd06900f848b18488db39a670f
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:51:12 +0800

    adding "s00d/ai_s00d_08_07.py" on  2/Sep/2026

--- s00d/ai_s00d_08_07.py
+++ s00d/ai_s00d_08_07.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:26:53 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:28:31 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making Numpy array
-a = numpy.array ([5.0, 3.0, 7.0, 4.0, 9.0, 8.0, 1.0, 6.0, 2.0, 0.0])
+# main function
+def main ():
+    # making Numpy array
+    a = numpy.array ([5.0, 3.0, 7.0, 4.0, 9.0, 8.0, 1.0, 6.0, 2.0, 0.0])
 
-# printing "a"
-print (f'a:\n{a}')
+    # printing "a"
+    print (f'a:\n{a}')
 
-# in-place sorting by timsort in descending order using ".sort ()" method
-a[::-1].sort (kind='mergesort')
+    # in-place sorting by timsort in descending order using ".sort ()" method
+    a[::-1].sort (kind='mergesort')
 
-# printing "a"
-print (f'a:\n{a}')
+    # printing "a"
+    print (f'a:\n{a}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
