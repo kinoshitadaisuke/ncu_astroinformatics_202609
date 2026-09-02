68ea93dec1bd7e32c17af8fcf686ac9677b4312f
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:05:04 +0800

    adding "s00e/ai_s00e_07_01.py" on  2/Sep/2026

--- s00e/ai_s00e_07_01.py
+++ s00e/ai_s00e_07_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:20:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:48:43 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -16,46 +16,52 @@ import sys
 # importing numpy module
 import numpy
 
-# constructing a parser object
-descr  = 'opening a file and reading data'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'opening a file and reading data'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('file', default='', help='input data file name')
+    # adding arguments
+    parser.add_argument ('file', default='', help='input data file name')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-file_input = args.file
+    # parameters
+    file_input = args.file
 
-# making a pathlib object for input file
-path_input = pathlib.Path (file_input)
+    # making a pathlib object for input file
+    path_input = pathlib.Path (file_input)
 
-# check of existence of input file
-if not (path_input.exists ()):
-    # printing a message
-    print (f'ERROR: input file "{file_input}" does not exist!')
-    # stopping the script
-    sys.exit (0)
+    # check of existence of input file
+    if not (path_input.exists ()):
+        # printing a message
+        print (f'ERROR: input file "{file_input}" does not exist!')
+        # stopping the script
+        sys.exit (0)
 
-# opening file
-with open (file_input, 'r') as fh:
-    # reading file line-by-line
-    for line in fh:
-        # if line starts with '#', then skip
-        if (line[0] == '#'):
-            continue
-        # reading data
-        data_str = line
-        # conversion from string into float
-        try:
+    # opening file
+    with open (file_input, 'r') as fh:
+        # reading file line-by-line
+        for line in fh:
+            # if line starts with '#', then skip
+            if (line[0] == '#'):
+                continue
+            # reading data
+            data_str = line
             # conversion from string into float
-            data = float (data_str)
-        except:
-            # printing a message
-            print (f'ERROR: conversion from string into float failed!')
-            # stopping this script
-            sys.exit (0)
-        # printing data
-        print (f'{data:15.6f}')
+            try:
+                # conversion from string into float
+                data = float (data_str)
+            except:
+                # printing a message
+                print (f'ERROR: conversion from string into float failed!')
+                # stopping this script
+                sys.exit (0)
+            # printing data
+            print (f'{data:15.6f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
