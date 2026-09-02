292bac6a8ba8a53ee489e61ce77d8abaad600002
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:40 +0800

    adding "s00d/ai_s00d_12_16.py" on  2/Sep/2026

--- s00d/ai_s00d_12_16.py
+++ s00d/ai_s00d_12_16.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:32:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:43:09 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_05.npz'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_05.npz'
 
-# loading Numpy arrays from npy file
-arrays = numpy.load (file_input)
+    # loading Numpy arrays from npy file
+    arrays = numpy.load (file_input)
 
-# printing "arrays"
-print (f'{arrays}')
+    # printing "arrays"
+    print (f'{arrays}')
 
-# printing object type of "arrays"
-print (f'{type (arrays)}')
+    # printing object type of "arrays"
+    print (f'{type (arrays)}')
 
-# first Numpy array in "arrays"
-array_e = arrays['array_e']
+    # first Numpy array in "arrays"
+    array_e = arrays['array_e']
 
-# second Numpy array in "arrays"
-array_f = arrays['array_f']
+    # second Numpy array in "arrays"
+    array_f = arrays['array_f']
 
-# third Numpy array in "arrays"
-array_g = arrays['array_g']
+    # third Numpy array in "arrays"
+    array_g = arrays['array_g']
 
-# printing array_c and array_d
-print (f'array_e:')
-print (array_e)
-print (f'array_f:')
-print (array_f)
-print (f'array_g:')
-print (array_g)
+    # printing array_c and array_d
+    print (f'array_e:')
+    print (array_e)
+    print (f'array_f:')
+    print (array_f)
+    print (f'array_g:')
+    print (array_g)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
