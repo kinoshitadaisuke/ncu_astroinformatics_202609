816d9bd1c1171e596b821ecf033221cfb7e624bc
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:47:58 +0800

    adding "s00d/ai_s00d_02_16.py" on  2/Sep/2026

--- s00d/ai_s00d_02_16.py
+++ s00d/ai_s00d_02_16.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:19:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/01 14:47:52 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# making a Numpy array (ndarray) with a specified data type
-# numpy.dtype ('c8') : 64-bit complex number
-array_i = numpy.array ([1.0 + 2.0j, 3.0j, 4.0, 5.0 - 6.0j, -7.0 + 8.0j, \
-                        -9.0 - 10.0j, -11.0j, -12.0, 13.0 + 14.0j, 15.0j], \
-                       dtype=numpy.dtype ('c8'))
+# main function
+def main ():
+    # making a Numpy array (ndarray) with a specified data type
+    # numpy.dtype ('c8') : 64-bit complex number
+    array_i = numpy.array ([1.0 + 2.0j, 3.0j, 4.0, 5.0 - 6.0j, -7.0 + 8.0j, \
+                            -9.0 - 10.0j, -11.0j, -12.0, 13.0 + 14.0j, 15.0j], \
+                           dtype=numpy.dtype ('c8'))
 
-# printing Numpy array
-print (f'array_i:\n{array_i}')
+    # printing Numpy array
+    print (f'array_i:\n{array_i}')
 
-# printing information
-print (f'information:')
-print (f'  ndim     = {array_i.ndim}')
-print (f'  size     = {array_i.size}')
-print (f'  shape    = {array_i.shape}')
-print (f'  dtype    = {array_i.dtype}')
-print (f'  itemsize = {array_i.itemsize} byte')
+    # printing information
+    print (f'information:')
+    print (f'  ndim     = {array_i.ndim}')
+    print (f'  size     = {array_i.size}')
+    print (f'  shape    = {array_i.shape}')
+    print (f'  dtype    = {array_i.dtype}')
+    print (f'  itemsize = {array_i.itemsize} byte')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
