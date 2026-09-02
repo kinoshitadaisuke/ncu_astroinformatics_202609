0eed437305568881ff00bf94aee6f18c5e2f5927
Daisuke <mhdf23888@gmail.com>
2026-09-02 09:53:36 +0800

    adding "s00d/ai_s00d_12_15.py" on  2/Sep/2026

--- s00d/ai_s00d_12_15.py
+++ s00d/ai_s00d_12_15.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/04 16:31:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 09:42:52 (UT+08:00) daisuke>
 #
 
 # importing Numpy module
 import numpy
 
-# input data file
-file_input = 'numpy_05.npz'
+# main function
+def main ():
+    # input data file
+    file_input = 'numpy_05.npz'
 
-# loading Numpy arrays from npy file
-arrays = numpy.load (file_input)
+    # loading Numpy arrays from npy file
+    arrays = numpy.load (file_input)
 
-# printing "arrays"
-print (f'{arrays}')
+    # printing "arrays"
+    print (f'{arrays}')
 
-# printing object type of "arrays"
-print (f'{type (arrays)}')
+    # printing object type of "arrays"
+    print (f'{type (arrays)}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
