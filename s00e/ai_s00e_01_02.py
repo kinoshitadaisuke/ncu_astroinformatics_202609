d0b963cc3323dae52d4977585b1701eabc275af6
Daisuke <mhdf23888@gmail.com>
2026-09-02 15:03:46 +0800

    adding "s00e/ai_s00e_01_02.py" on  2/Sep/2026

--- s00e/ai_s00e_01_02.py
+++ s00e/ai_s00e_01_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/10 20:16:46 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/02 14:41:48 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -20,76 +20,82 @@ import numpy
 import matplotlib.backends.backend_agg
 import matplotlib.figure
 
-# constructing a parser object
-parser = argparse.ArgumentParser (description='plotting a sine curve')
+# main function
+def main ():
+    # constructing a parser object
+    parser = argparse.ArgumentParser (description='plotting a sine curve')
 
-# adding arguments
-parser.add_argument ('-o', '--output', default='output.png', \
-                     help='output file name (default: output.png)')
-parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
-                     help='resolution of plot in DPI (default: 225.0)')
+    # adding arguments
+    parser.add_argument ('-o', '--output', default='output.png', \
+                         help='output file name (default: output.png)')
+    parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
+                         help='resolution of plot in DPI (default: 225.0)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# parameters
-file_output    = args.output
-resolution_dpi = args.resolution
+    # parameters
+    file_output    = args.output
+    resolution_dpi = args.resolution
 
-# making a pathlib object for output file
-path_output = pathlib.Path (file_output)
+    # making a pathlib object for output file
+    path_output = pathlib.Path (file_output)
 
-# check of existence of output file
-if (path_output.exists ()):
-    # printing a message
-    print (f'ERROR: output file "{file_output}" exists!')
-    # stopping the script
-    sys.exit (0)
+    # check of existence of output file
+    if (path_output.exists ()):
+        # printing a message
+        print (f'ERROR: output file "{file_output}" exists!')
+        # stopping the script
+        sys.exit (0)
 
-# check of extension of output file
-if not ( (path_output.suffix == '.eps') \
-         or (path_output.suffix == '.pdf') \
-         or (path_output.suffix == '.png') \
-         or (path_output.suffix == '.ps') ):
-    # printing a message
-    print (f'ERROR: output file must be either EPS or PDF or PNG or PS file.')
-    # stopping the script
-    sys.exit (0)
+    # check of extension of output file
+    if not ( (path_output.suffix == '.eps') \
+             or (path_output.suffix == '.pdf') \
+             or (path_output.suffix == '.png') \
+             or (path_output.suffix == '.ps') ):
+        # printing a message
+        print (f'ERROR: output file must be either EPS or PDF or PNG or PS file.')
+        # stopping the script
+        sys.exit (0)
 
-# data to be plotted
-# y = sin (x)
-data_x = numpy.linspace (0.0, 720.0, 7201)
-data_y = numpy.sin ( numpy.deg2rad (data_x) )
+    # data to be plotted
+    # y = sin (x)
+    data_x = numpy.linspace (0.0, 720.0, 7201)
+    data_y = numpy.sin ( numpy.deg2rad (data_x) )
 
-# making a fig object
-fig = matplotlib.figure.Figure ()
+    # making a fig object
+    fig = matplotlib.figure.Figure ()
 
-# making a canvas object
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    # making a canvas object
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
 
-# making an axes object
-ax = fig.add_subplot (111)
+    # making an axes object
+    ax = fig.add_subplot (111)
 
-# making a plot using object-oriented interface
-ax.plot (data_x, data_y, label=r'$f(x) = \sin (x)$')
+    # making a plot using object-oriented interface
+    ax.plot (data_x, data_y, label=r'$f(x) = \sin (x)$')
 
-# setting ranges of x-axis and y-axis
-ax.set_xlim (0.0, +720.0)
-ax.set_ylim (-1.2, +1.2)
+    # setting ranges of x-axis and y-axis
+    ax.set_xlim (0.0, +720.0)
+    ax.set_ylim (-1.2, +1.2)
 
-# setting labels for x-axis and y-axis
-ax.set_xlabel ('$x$ [deg]')
-ax.set_ylabel ('$y$')
+    # setting labels for x-axis and y-axis
+    ax.set_xlabel ('$x$ [deg]')
+    ax.set_ylabel ('$y$')
 
-# setting ticks
-ax.set_xticks (numpy.linspace (0.0, 720.0, 9))
-ax.set_yticks (numpy.linspace (-1.0, +1.0, 11))
+    # setting ticks
+    ax.set_xticks (numpy.linspace (0.0, 720.0, 9))
+    ax.set_yticks (numpy.linspace (-1.0, +1.0, 11))
 
-# showing grid
-ax.grid ()
+    # showing grid
+    ax.grid ()
 
-# adding legend to the plot
-ax.legend ()
+    # adding legend to the plot
+    ax.legend ()
 
-# saving a plot as a file
-fig.savefig (file_output, dpi=resolution_dpi)
+    # saving a plot as a file
+    fig.savefig (file_output, dpi=resolution_dpi)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
