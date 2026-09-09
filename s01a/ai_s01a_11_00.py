942ae99b01e86eee6853d8e392b147e34afa0ad1
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:07:02 +0800

    adding "s01a/ai_s01a_11_00.py" on  9/Sep/2026

--- s01a/ai_s01a_11_00.py
+++ s01a/ai_s01a_11_00.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/17 09:36:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:46:41 (UT+08:00) daisuke>
 #
 
 # importing numpy module
 import numpy
 
-# output data file
-file_output = 'appy_s05_11_00.data'
+# main function
+def main ():
+    # output data file
+    file_output = 'ai_s01a_11_00.data'
 
-# pi
-pi = numpy.pi
+    # pi
+    pi = numpy.pi
 
-# amplitude
-A = 2.0
+    # amplitude
+    A = 2.0
 
-# frequency in Hz
-f = 11.0
+    # frequency in Hz
+    f = 11.0
 
-# initial phase
-delta = 0.5 * pi
+    # initial phase
+    delta = 0.5 * pi
 
-# baseline
-b = 3.0
+    # baseline
+    b = 3.0
 
-# number of data points
-n = 8192
+    # number of data points
+    n = 8192
 
-# data acquisition interval in sec
-dt = 0.005
+    # data acquisition interval in sec
+    dt = 0.005
 
-# time
-t = numpy.linspace (0.0, n * dt, n, endpoint=False)
+    # time
+    t = numpy.linspace (0.0, n * dt, n, endpoint=False)
 
-# synthetic data for FFT
-y = A * numpy.sin (2.0 * pi * f * t - delta) + b
+    # synthetic data for FFT
+    y = A * numpy.sin (2.0 * pi * f * t - delta) + b
 
-# opening file for writing
-with open (file_output, 'w') as fh:
-    # for all the data
-    for i in range (n):
-        # writing data into file
-        fh.write (f'{t[i]:12.6f}  {y[i]:12.9f}\n')
+    # opening file for writing
+    with open (file_output, 'w') as fh:
+        # for all the data
+        for i in range (n):
+            # writing data into file
+            fh.write (f'{t[i]:12.6f}  {y[i]:12.9f}\n')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
