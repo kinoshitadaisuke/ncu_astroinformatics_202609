01db62e034f4d147c186862b4d1875e03a1c783e
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:09:14 +0800

    adding "s01a/ai_s01a_09_01.py" on  9/Sep/2026

---
+++ s01a/ai_s01a_09_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/14 21:26:12 (UT+08:00) daisuke>
+#
+
+# importing scipy module
+import scipy.special
+
+# calculation of Gamma (6.5)
+result = scipy.special.gamma (6.5)
+
+# printing result of calculation
+print (f'Gamma (6.5) = {result}')
