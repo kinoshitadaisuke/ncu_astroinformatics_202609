b2d16e2bfd1f9d19baaedeb587ace85525cf9548
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:59:01 +0800

    adding "s01a/ai_s01a_00_07.py" on  9/Sep/2026

--- s01a/ai_s01a_00_07.py
+++ s01a/ai_s01a_00_07.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:14:49 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:16:34 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# some units of time
-day  = scipy.constants.day
-year = scipy.constants.year
+# main function
+def main ():
+    # some units of time
+    day  = scipy.constants.day
+    year = scipy.constants.year
 
-# printing units of time
-print (f'1 day  = {day:g} [sec]')
-print (f'1 year = {year:g} [sec]')
+    # printing units of time
+    print (f'1 day  = {day:g} [sec]')
+    print (f'1 year = {year:g} [sec]')
 
-# some units of energy
-eV  = scipy.constants.eV
-erg = scipy.constants.erg
+    # some units of energy
+    eV  = scipy.constants.eV
+    erg = scipy.constants.erg
 
-# printing units of energy
-print (f'1 eV   = {eV:g} [J]')
-print (f'1 erg  = {erg:g} [J]')
+    # printing units of energy
+    print (f'1 eV   = {eV:g} [J]')
+    print (f'1 erg  = {erg:g} [J]')
 
-# some units of pressure
-atm  = scipy.constants.atm
-bar  = scipy.constants.bar
-torr = scipy.constants.torr
-mmHg = scipy.constants.mmHg
+    # some units of pressure
+    atm  = scipy.constants.atm
+    bar  = scipy.constants.bar
+    torr = scipy.constants.torr
+    mmHg = scipy.constants.mmHg
 
-# printing units of pressure
-print (f'1 atm  = {atm:g} [Pa]')
-print (f'1 bar  = {bar:g} [Pa]')
-print (f'1 torr = {torr:g} [Pa]')
-print (f'1 mmHg = {mmHg:g} [Pa]')
+    # printing units of pressure
+    print (f'1 atm  = {atm:g} [Pa]')
+    print (f'1 bar  = {bar:g} [Pa]')
+    print (f'1 torr = {torr:g} [Pa]')
+    print (f'1 mmHg = {mmHg:g} [Pa]')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
