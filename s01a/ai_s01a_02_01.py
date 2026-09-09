ab88fb8b85f2136501e06496d4cb4afffaa1383d
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:59:52 +0800

    adding "s01a/ai_s01a_02_01.py" on  9/Sep/2026

--- s01a/ai_s01a_02_01.py
+++ s01a/ai_s01a_02_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:16:59 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:20:33 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -10,52 +10,58 @@ import argparse
 # importing scipy module
 import scipy.stats
 
-# constructing a parser object
-descr  = 'finding mean, variance, and standard deviation'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'finding mean, variance, and standard deviation'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-n', '--number', type=int, default=1, \
-                     help='number of random numbers (default: 1)')
-parser.add_argument ('-m', '--mean', type=float, default=0.0, \
-                     help='mean value of distribution (default: 0.0)')
-parser.add_argument ('-s', '--stddev', type=float, default=1.0, \
-                     help='standard deviation of distribution (default: 1.0)')
+    # adding arguments
+    parser.add_argument ('-n', '--number', type=int, default=1, \
+                         help='number of random numbers (default: 1)')
+    parser.add_argument ('-m', '--mean', type=float, default=0.0, \
+                         help='mean value of distribution (default: 0.0)')
+    parser.add_argument ('-s', '--stddev', type=float, default=1.0, \
+                         help='standard deviation of distribution (default: 1.0)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# input parameters
-n_rg      = args.number
-mean_rg   = args.mean
-stddev_rg = args.stddev
+    # input parameters
+    n_rg      = args.number
+    mean_rg   = args.mean
+    stddev_rg = args.stddev
 
-# generating a set of random numbers of Gaussian distribution
-rg = scipy.stats.norm.rvs (loc=mean_rg, scale=stddev_rg, size=n_rg)
+    # generating a set of random numbers of Gaussian distribution
+    rg = scipy.stats.norm.rvs (loc=mean_rg, scale=stddev_rg, size=n_rg)
 
-# printing generated random numbers
-print (f'generated random numbers:')
-print (f'{rg}')
+    # printing generated random numbers
+    print (f'generated random numbers:')
+    print (f'{rg}')
 
-# finding minimum value
-tmin = scipy.stats.tmin (rg)
+    # finding minimum value
+    tmin = scipy.stats.tmin (rg)
 
-# finding maximum value
-tmax = scipy.stats.tmax (rg)
+    # finding maximum value
+    tmax = scipy.stats.tmax (rg)
 
-# calculation of arithmetic mean of distribution
-mean = scipy.stats.tmean (rg)
+    # calculation of arithmetic mean of distribution
+    mean = scipy.stats.tmean (rg)
 
-# calculation of variance of distribution
-var = scipy.stats.tvar (rg)
+    # calculation of variance of distribution
+    var = scipy.stats.tvar (rg)
 
-# calculation of standard deviation of distribution
-stddev = scipy.stats.tstd (rg)
+    # calculation of standard deviation of distribution
+    stddev = scipy.stats.tstd (rg)
 
-# printing minimum and maximum values
-print (f'statistical values:')
-print (f'  tmin   = {tmin:15.6f}')
-print (f'  tmax   = {tmax:15.6f}')
-print (f'  mean   = {mean:15.6f}')
-print (f'  var    = {var:15.6f}')
-print (f'  stddev = {stddev:15.6f}')
+    # printing minimum and maximum values
+    print (f'statistical values:')
+    print (f'  tmin   = {tmin:15.6f}')
+    print (f'  tmax   = {tmax:15.6f}')
+    print (f'  mean   = {mean:15.6f}')
+    print (f'  var    = {var:15.6f}')
+    print (f'  stddev = {stddev:15.6f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
