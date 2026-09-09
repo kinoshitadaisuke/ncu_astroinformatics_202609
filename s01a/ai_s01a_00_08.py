e1ca8fb5b42fe1f175d77d60736fbeebebee6af7
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:59:05 +0800

    adding "s01a/ai_s01a_00_08.py" on  9/Sep/2026

--- s01a/ai_s01a_00_08.py
+++ s01a/ai_s01a_00_08.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:15:02 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:17:00 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# nano
-nano = scipy.constants.nano
+# main function
+def main ():
+    # nano
+    nano = scipy.constants.nano
 
-# wavelength of electromagnetic radiation
-wavelength = 500.0 * nano
+    # wavelength of electromagnetic radiation
+    wavelength = 500.0 * nano
 
-# frequency of electromagnetic radiation
-frequency = scipy.constants.lambda2nu (wavelength)
+    # frequency of electromagnetic radiation
+    frequency = scipy.constants.lambda2nu (wavelength)
 
-# printing the result of conversion
-print (f'{wavelength:g} [m] ==> {frequency:g} [Hz]')
+    # printing the result of conversion
+    print (f'{wavelength:g} [m] ==> {frequency:g} [Hz]')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
