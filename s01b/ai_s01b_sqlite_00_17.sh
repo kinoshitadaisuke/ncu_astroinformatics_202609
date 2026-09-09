b5be4c2f7d46aea98b240bf10e60f395938de6e7
Daisuke <mhdf23888@gmail.com>
2026-09-10 07:22:19 +0800

    adding "s01b/ai_s01b_sqlite_00_17.sh" on 10/Sep/2026

---
+++ s01b/ai_s01b_sqlite_00_17.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#
+# Time-stamp: <2026/03/19 10:33:44 (UT+08:00) daisuke>
+#
+
+# SQL query
+sqlite3 -header -column planet0.db \
+	"select name,mass,diameter,satellite, ring,magnetic_field from planet;"
