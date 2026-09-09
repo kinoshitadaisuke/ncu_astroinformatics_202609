20c15d36b6278a6ec1f3f42fe9c5bee46166150f
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:58:45 +0800

    adding "s01a/ai_s01a_00_03.py" on  9/Sep/2026

--- s01a/ai_s01a_00_03.py
+++ s01a/ai_s01a_00_03.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:14:09 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:15:09 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# searching constants
-search_result = scipy.constants.find ('light')
+# main function
+def main ():
+    # searching constants
+    search_result = scipy.constants.find ('light')
 
-# printing search result
-for constant in search_result:
-    print (f'{constant}')
+    # printing search result
+    for constant in search_result:
+        print (f'{constant}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
