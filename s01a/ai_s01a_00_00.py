8ea154637f8b6aa77d4c63bb892551f573cdef3b
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:03:54 +0800

    adding "s01a/ai_s01a_00_00.py" on  9/Sep/2026

---
+++ s01a/ai_s01a_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/14 21:13:38 (UT+08:00) daisuke>
+#
+
+# importing scipy module
+import scipy.constants
+
+# value of pi
+pi = scipy.constants.pi
+
+# printing value of pi
+print (f'pi = {pi}')
