efe5ac49927de5bbccad6ef1b7e36fae61d6e942
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:59:10 +0800

    adding "s01a/ai_s01a_00_09.py" on  9/Sep/2026

--- s01a/ai_s01a_00_09.py
+++ s01a/ai_s01a_00_09.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:15:12 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:17:18 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# giga
-giga = scipy.constants.giga
+# main function
+def main ():
+    # giga
+    giga = scipy.constants.giga
 
-# frequency of electromagnetic radiation
-frequency = 115.0 * giga
+    # frequency of electromagnetic radiation
+    frequency = 115.0 * giga
 
-# wavelength of electromagnetic radiation
-wavelength = scipy.constants.nu2lambda (frequency)
+    # wavelength of electromagnetic radiation
+    wavelength = scipy.constants.nu2lambda (frequency)
 
-# printing the result of conversion
-print (f'{frequency:g} [Hz] ==> {wavelength:g} [m]')
+    # printing the result of conversion
+    print (f'{frequency:g} [Hz] ==> {wavelength:g} [m]')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
