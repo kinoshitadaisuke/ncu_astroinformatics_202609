7c258bf7ee7eebc65e443efc90f58c06996835ef
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:59:23 +0800

    adding "s01a/ai_s01a_01_02.py" on  9/Sep/2026

--- s01a/ai_s01a_01_02.py
+++ s01a/ai_s01a_01_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:15:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:18:21 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -10,35 +10,41 @@ import argparse
 # importing scipy module
 import scipy.stats
 
-# constructing a parser object
-descr  = 'generating random numbers of uniform distribution between 0 and 1'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'generating random numbers of uniform distribution between 0 and 1'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-n', '--number', type=int, default=1, \
-                     help='number of random numbers (default: 1)')
+    # adding arguments
+    parser.add_argument ('-n', '--number', type=int, default=1, \
+                         help='number of random numbers (default: 1)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# input parameters
-n = args.number
+    # input parameters
+    n = args.number
 
-# generating a set of random numbers of uniform distribution between 0.0 and 1.0
-ru = scipy.stats.uniform.rvs (size=n)
+    # generating a set of random numbers of uniform distribution between 0.0 and 1.0
+    ru = scipy.stats.uniform.rvs (size=n)
 
-# printing generated random numbers
-print (f'generated random numbers:')
-print (f'{ru}')
+    # printing generated random numbers
+    print (f'generated random numbers:')
+    print (f'{ru}')
 
-# finding data type of object "ru"
-type_ru = type (ru)
+    # finding data type of object "ru"
+    type_ru = type (ru)
 
-# printing type of object "ru"
-print (f'type of object "ru" = {type_ru}')
+    # printing type of object "ru"
+    print (f'type of object "ru" = {type_ru}')
 
-# getting number of elements
-n_elements = ru.size
+    # getting number of elements
+    n_elements = ru.size
 
-# printing number of elements
-print (f'number of elements in the array = {n_elements}')
+    # printing number of elements
+    print (f'number of elements in the array = {n_elements}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
