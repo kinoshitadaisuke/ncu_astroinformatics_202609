ac13c8e16ee37013b4f9b3a3e59549c637993ce5
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:04:46 +0800

    adding "s01a/ai_s01a_06_04.py" on  9/Sep/2026

--- s01a/ai_s01a_06_04.py
+++ s01a/ai_s01a_06_04.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:22:07 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:34:43 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
 import subprocess
 
-# ffmpeg command
-ffmpeg = 'ffmpeg'
+# main function
+def main ():
+    # ffmpeg command
+    ffmpeg = 'ffmpeg'
 
-# frame rate
-framerate = 30
+    # frame rate
+    framerate = 30
 
-# number of threads
-n_thread = 1
+    # number of threads
+    n_thread = 1
 
-# ffmpeg options
-options_ffmpeg = f'-f image2 -i p2/p2_%08d.png -start_number 0' \
-    + f' -framerate {framerate} -an -vcodec libx264 -pix_fmt yuv420p' \
-    + f' -threads {n_thread}'
+    # ffmpeg options
+    options_ffmpeg = f'-f image2 -i p2/p2_%08d.png -start_number 0' \
+        + f' -framerate {framerate} -an -vcodec libx264 -pix_fmt yuv420p' \
+        + f' -threads {n_thread}'
 
-# output file name
-file_output = 'p2.mp4'
+    # output file name
+    file_output = 'p2.mp4'
 
-# command to be executed
-command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+    # command to be executed
+    command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
 
-# execution of command
-subprocess.run (command_ffmpeg, shell=True)
+    # execution of command
+    subprocess.run (command_ffmpeg, shell=True)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
