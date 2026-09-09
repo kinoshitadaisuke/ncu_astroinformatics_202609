167128e89ea7d8d7688e4f8714c437e00c720fc2
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:58:49 +0800

    adding "s01a/ai_s01a_00_04.py" on  9/Sep/2026

--- s01a/ai_s01a_00_04.py
+++ s01a/ai_s01a_00_04.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:14:18 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:15:28 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# searching constants
-search_result = scipy.constants.find ('light')
+# main function
+def main ():
+    # searching constants
+    search_result = scipy.constants.find ('light')
 
-# printing search result
-for constant in search_result:
-    print (f'{constant}:')
-    print (f'  value = {scipy.constants.physical_constants[constant][0]}')
-    print (f'  error = {scipy.constants.physical_constants[constant][2]}')
-    print (f'  unit  = {scipy.constants.physical_constants[constant][1]}')
+    # printing search result
+    for constant in search_result:
+        print (f'{constant}:')
+        print (f'  value = {scipy.constants.physical_constants[constant][0]}')
+        print (f'  error = {scipy.constants.physical_constants[constant][2]}')
+        print (f'  unit  = {scipy.constants.physical_constants[constant][1]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
