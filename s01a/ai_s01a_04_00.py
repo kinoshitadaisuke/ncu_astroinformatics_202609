615f723261ff8cbf45aee213712f4c05478c9548
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:00:51 +0800

    adding "s01a/ai_s01a_04_00.py" on  9/Sep/2026

--- s01a/ai_s01a_04_00.py
+++ s01a/ai_s01a_04_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:18:55 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:24:49 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -14,43 +14,49 @@ import numpy
 import scipy
 import scipy.interpolate
 
-# constructing a parser object
-descr  = 'carrying out linear interpolation'
-parser = argparse.ArgumentParser (description=descr)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'carrying out linear interpolation'
+    parser = argparse.ArgumentParser (description=descr)
 
-# adding arguments
-parser.add_argument ('-a', '--gradient', type=float, default=1.0, \
-                     help='gradient of a line (default: 1.0)')
-parser.add_argument ('-b', '--yintercept', type=float, default=0.0, \
-                     help='Y-axis intercept of a line (default: 0.0)')
-parser.add_argument ('-x', '--xvalue', type=float, default=0.0, \
-                     help='X-value of interpolation (default: 0.0)')
+    # adding arguments
+    parser.add_argument ('-a', '--gradient', type=float, default=1.0, \
+                         help='gradient of a line (default: 1.0)')
+    parser.add_argument ('-b', '--yintercept', type=float, default=0.0, \
+                         help='Y-axis intercept of a line (default: 0.0)')
+    parser.add_argument ('-x', '--xvalue', type=float, default=0.0, \
+                         help='X-value of interpolation (default: 0.0)')
 
-# parsing arguments
-args = parser.parse_args ()
+    # parsing arguments
+    args = parser.parse_args ()
 
-# input parameters
-a              = args.gradient
-b              = args.yintercept
-xi             = args.xvalue
+    # input parameters
+    a              = args.gradient
+    b              = args.yintercept
+    xi             = args.xvalue
 
-# generating data for interpolation
-data_x = numpy.linspace (-10.0, 10.0, 11)
-data_y = a * data_x + b
+    # generating data for interpolation
+    data_x = numpy.linspace (-10.0, 10.0, 11)
+    data_y = a * data_x + b
 
-# printing data_x and data_y
-print (f'line: y = {a} x + {b}')
-print (f'  data_x = {data_x}')
-print (f'  data_y = {data_y}')
+    # printing data_x and data_y
+    print (f'line: y = {a} x + {b}')
+    print (f'  data_x = {data_x}')
+    print (f'  data_y = {data_y}')
 
-# making a function for linear interpolation
-func_interp = scipy.interpolate.interp1d (data_x, data_y, kind='linear')
+    # making a function for linear interpolation
+    func_interp = scipy.interpolate.interp1d (data_x, data_y, kind='linear')
 
-# getting Y-value for X-value at X-value of "xi"
-yi = func_interp (xi)
+    # getting Y-value for X-value at X-value of "xi"
+    yi = func_interp (xi)
 
-# printing result
-print (f'result of interpolation:')
-print (f'  func_interp ({xi}) = {yi}')
-print (f'what we expect:')
-print (f'  x={xi} --> y = {a} * {xi} + {b} = {a*xi+b}')
+    # printing result
+    print (f'result of interpolation:')
+    print (f'  func_interp ({xi}) = {yi}')
+    print (f'what we expect:')
+    print (f'  x={xi} --> y = {a} * {xi} + {b} = {a*xi+b}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
