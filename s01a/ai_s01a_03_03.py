e7d59e0c62ed565f4df2b1e1e1c954c4d8b1e688
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:00:38 +0800

    adding "s01a/ai_s01a_03_03.py" on  9/Sep/2026

--- s01a/ai_s01a_03_03.py
+++ s01a/ai_s01a_03_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:18:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:23:52 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -11,15 +11,21 @@ import numpy
 import scipy
 import scipy.linalg
 
-# matrix A
-A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
+# main function
+def main ():
+    # matrix A
+    A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
 
-# printing matrix A
-print (f'matrix A:\n{A}')
+    # printing matrix A
+    print (f'matrix A:\n{A}')
 
-# eigenvalues and eigenvectors of matrix A
-eigenvalvec = scipy.linalg.eig (A)
+    # eigenvalues and eigenvectors of matrix A
+    eigenvalvec = scipy.linalg.eig (A)
 
-# printing eigenvalues and eigenvectors of matrix A
-print (f'eigenvalues of matrix A:\n{eigenvalvec[0]}')
-print (f'eigenvectors of matrix A:\n{eigenvalvec[1]}')
+    # printing eigenvalues and eigenvectors of matrix A
+    print (f'eigenvalues of matrix A:\n{eigenvalvec[0]}')
+    print (f'eigenvectors of matrix A:\n{eigenvalvec[1]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
