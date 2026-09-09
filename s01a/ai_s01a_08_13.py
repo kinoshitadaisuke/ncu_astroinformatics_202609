20baca3beb441fd71e5e717fa06b50d6843bb9c1
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:06:13 +0800

    adding "s01a/ai_s01a_08_13.py" on  9/Sep/2026

--- s01a/ai_s01a_08_13.py
+++ s01a/ai_s01a_08_13.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:24:53 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:57:13 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -19,54 +19,6 @@ import numpy
 # importing scipy module
 import scipy.stats
 
-# constructing a parser object
-descr  = 'Synthetic data generation'
-parser = argparse.ArgumentParser (description=descr)
-
-# adding arguments
-parser.add_argument ('-a', type=float, default=1.0, \
-                     help='a of y=a(x-b)^2+c (default: 1.0)')
-parser.add_argument ('-b', type=float, default=0.0, \
-                     help='b of y=a(x-b)^2+c (default: 1.0)')
-parser.add_argument ('-c', type=float, default=0.0, \
-                     help='c of y=a(x-b)^2+c (default: 1.0)')
-parser.add_argument ('-e', type=float, default=1.0, \
-                     help='std. dev. of error (default: 1)')
-parser.add_argument ('-n', type=int, default=1, \
-                     help='number of data to generate (default: 1)')
-parser.add_argument ('-min', type=float, default=0.0, \
-                     help='minimum value of x for data generation (default: 0)')
-parser.add_argument ('-max', type=float, default=1.0, \
-                     help='maximum value of x for data generation (default: 1)')
-parser.add_argument ('-o', default='output.data', \
-                     help='output data file (default: output.data)')
-
-# parsing arguments
-args = parser.parse_args ()
-
-# input parameters
-a           = args.a
-b           = args.b
-c           = args.c
-stddev      = args.e
-n           = args.n
-range_min   = args.min
-range_max   = args.max
-file_output = args.o
-
-# making a pathlib object for output file
-path_output = pathlib.Path (file_output)
-
-# check of existence of output file
-if (path_output.exists ()):
-    # printing a message
-    print (f'ERROR: output file "{file_output}" exists!')
-    # stopping the script
-    sys.exit (0)
-
-# generating random numbers
-err = scipy.stats.norm.rvs (loc=0.0, scale=stddev, size=n)
-
 # function for a line
 def curve (x):
     # line
@@ -74,13 +26,67 @@ def curve (x):
     # returning y
     return y
 
-# synthetic data for least-squares method
-data_x   = numpy.linspace (range_min, range_max, n)
-data_y   = curve (data_x) + err
-data_err = numpy.absolute (err)
+# main function
+def main ():
+    # constructing a parser object
+    descr  = 'Synthetic data generation'
+    parser = argparse.ArgumentParser (description=descr)
 
-# opening file for writing
-with open (file_output, 'w') as fh:
-    # writing generated synthetic data into file
-    for i in range (len (data_x)):
-        fh.write (f'{data_x[i]:8.3f}  {data_y[i]:8.3f}  {data_err[i]:8.3f}\n')
+    # adding arguments
+    parser.add_argument ('-a', type=float, default=1.0, \
+                         help='a of y=a(x-b)^2+c (default: 1.0)')
+    parser.add_argument ('-b', type=float, default=0.0, \
+                         help='b of y=a(x-b)^2+c (default: 1.0)')
+    parser.add_argument ('-c', type=float, default=0.0, \
+                         help='c of y=a(x-b)^2+c (default: 1.0)')
+    parser.add_argument ('-e', type=float, default=1.0, \
+                         help='std. dev. of error (default: 1)')
+    parser.add_argument ('-n', type=int, default=1, \
+                         help='number of data to generate (default: 1)')
+    parser.add_argument ('-min', type=float, default=0.0, \
+                         help='minimum value of x for data generation (default: 0)')
+    parser.add_argument ('-max', type=float, default=1.0, \
+                         help='maximum value of x for data generation (default: 1)')
+    parser.add_argument ('-o', default='output.data', \
+                         help='output data file (default: output.data)')
+
+    # parsing arguments
+    args = parser.parse_args ()
+
+    # input parameters
+    a           = args.a
+    b           = args.b
+    c           = args.c
+    stddev      = args.e
+    n           = args.n
+    range_min   = args.min
+    range_max   = args.max
+    file_output = args.o
+
+    # making a pathlib object for output file
+    path_output = pathlib.Path (file_output)
+
+    # check of existence of output file
+    if (path_output.exists ()):
+        # printing a message
+        print (f'ERROR: output file "{file_output}" exists!')
+        # stopping the script
+        sys.exit (0)
+
+    # generating random numbers
+    err = scipy.stats.norm.rvs (loc=0.0, scale=stddev, size=n)
+
+    # synthetic data for least-squares method
+    data_x   = numpy.linspace (range_min, range_max, n)
+    data_y   = curve (data_x) + err
+    data_err = numpy.absolute (err)
+
+    # opening file for writing
+    with open (file_output, 'w') as fh:
+        # writing generated synthetic data into file
+        for i in range (len (data_x)):
+            fh.write (f'{data_x[i]:8.3f}  {data_y[i]:8.3f}  {data_err[i]:8.3f}\n')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
