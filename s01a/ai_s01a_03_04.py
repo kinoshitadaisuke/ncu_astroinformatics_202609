7d73046a043ff51bb1f8cc0de910feca80048c9f
Daisuke <mhdf23888@gmail.com>
2026-09-09 14:00:42 +0800

    adding "s01a/ai_s01a_03_04.py" on  9/Sep/2026

--- s01a/ai_s01a_03_04.py
+++ s01a/ai_s01a_03_04.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:18:35 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:24:09 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -11,14 +11,20 @@ import numpy
 import scipy
 import scipy.linalg
 
-# matrix A
-A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
+# main function
+def main ():
+    # matrix A
+    A = numpy.array ( [ [3.0, 1.0], [2.0, 2.0] ] )
 
-# printing matrix A
-print (f'matrix A:\n{A}')
+    # printing matrix A
+    print (f'matrix A:\n{A}')
 
-# the other way to get eigenvalues of matrix A
-eigenvalues = scipy.linalg.eigvals (A)
+    # the other way to get eigenvalues of matrix A
+    eigenvalues = scipy.linalg.eigvals (A)
 
-# printing eigenvalues of matrix A
-print (f'eigenvalues of matrix A:\n{eigenvalues}')
+    # printing eigenvalues of matrix A
+    print (f'eigenvalues of matrix A:\n{eigenvalues}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
