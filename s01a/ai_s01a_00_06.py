dc25444d6071d62f4c1f0b3401b6bbfc1b2ccb45
Daisuke <mhdf23888@gmail.com>
2026-09-09 13:58:57 +0800

    adding "s01a/ai_s01a_00_06.py" on  9/Sep/2026

--- s01a/ai_s01a_00_06.py
+++ s01a/ai_s01a_00_06.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/14 21:14:38 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/09 13:16:11 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-# some units for length
-angstrom = scipy.constants.angstrom
-au       = scipy.constants.au
-ly       = scipy.constants.light_year
-parsec   = scipy.constants.parsec
+# main function
+def main ():
+    # some units for length
+    angstrom = scipy.constants.angstrom
+    au       = scipy.constants.au
+    ly       = scipy.constants.light_year
+    parsec   = scipy.constants.parsec
 
-# printing units for length
-print (f'1 angstrom = {angstrom:g} [m]')
-print (f'1 au       = {au:g} [m]')
-print (f'1 ly       = {ly:g} [m]')
-print (f'1 parsec   = {parsec:g} [m]')
+    # printing units for length
+    print (f'1 angstrom = {angstrom:g} [m]')
+    print (f'1 au       = {au:g} [m]')
+    print (f'1 ly       = {ly:g} [m]')
+    print (f'1 parsec   = {parsec:g} [m]')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
