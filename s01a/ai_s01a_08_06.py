26ee8b40f942d29e22b436f4e1470c5b37ca21a0
Daisuke <mhdf23888@gmail.com>
2026-09-10 14:30:37 +0800

    adding "s01a/ai_s01a_08_06.py" on 10/Sep/2026

--- s01a/ai_s01a_08_06.py
+++ s01a/ai_s01a_08_06.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/09/09 13:55:28 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 13:49:58 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -23,20 +23,6 @@ import scipy.stats
 # importing matplotlib module
 import matplotlib.figure
 import matplotlib.backends.backend_agg
-
-# a function for straight line
-def line (x, a, b):
-    # line
-    y = a * x + b
-    # returning y
-    return y
-
-# a function to calculate residuals
-def residual (param, x, y, y_err):
-    # calculation of residual
-    residue = (param[0] * x + param[1] - y) / y_err
-    # returning residual
-    return residue
 
 # main function
 def main ():
@@ -141,6 +127,13 @@ def main ():
         print (f'(x_{i:03d}, y_{i:03d})', \
                f' = ({data_x[i]:15.6f}, {data_y[i]:15.6f} +/- {data_err[i]:15.6f})')
 
+    # a function to calculate residuals
+    def residual (param, x, y, y_err):
+        # calculation of residual
+        residue = (param[0] * x + param[1] - y) / y_err
+        # returning residual
+        return residue
+
     # initial guess of coefficients
     param0 = [a, b]
 
@@ -182,6 +175,13 @@ def main ():
     # range of data
     x_min = scipy.stats.tmin (data_x)
     x_max = scipy.stats.tmax (data_x)
+
+    # a function for straight line
+    def line (x, a, b):
+        # line
+        y = a * x + b
+        # returning y
+        return y
 
     # fitted line
     fitted_x = numpy.linspace (x_min, x_max, 1000)
