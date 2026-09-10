a9480a5114cf89ace86500980e13f0f8ff6381b5
Daisuke <mhdf23888@gmail.com>
2026-09-10 16:26:34 +0800

    adding "s01b/ai_s01b_05_05.py" on 10/Sep/2026

--- s01b/ai_s01b_05_05.py
+++ s01b/ai_s01b_05_05.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/19 10:41:12 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:16:56 (UT+08:00) daisuke>
 #
 
 # importing sqlite module
@@ -10,26 +10,32 @@ import sqlite3
 # importing contextlib module
 import contextlib
 
-# database file name
-file_db = 'ngc2000.db'
+# main function
+def main ():
+    # database file name
+    file_db = 'ngc2000.db'
 
-# connecting to database
-with contextlib.closing (sqlite3.connect (file_db)) as conn:
-    # constructing a cursor object
-    cursor = conn.cursor ()
+    # connecting to database
+    with contextlib.closing (sqlite3.connect (file_db)) as conn:
+        # constructing a cursor object
+        cursor = conn.cursor ()
 
-    # SQL command for a query
-    sql_query = 'select id,type,constellation,size,mag from ngc2000 ' \
-        + f'where type is "galaxy" and size > 30 order by mag;'
+        # SQL command for a query
+        sql_query = 'select id,type,constellation,size,mag from ngc2000 ' \
+            + f'where type is "galaxy" and size > 30 order by mag;'
 
-    # executing a SQL query
-    cursor.execute (sql_query)
+        # executing a SQL query
+        cursor.execute (sql_query)
 
-    # fetching results of query
-    results = cursor.fetchall ()
+        # fetching results of query
+        results = cursor.fetchall ()
 
-    # printing results of query
-    print (f'# ID    Constellation Size (arcmin)    Mag  Object Type')
-    for result in results:
-        print (f'{result[0]:7s} {result[2]:>13s} {result[3]:13.1f}', \
-               f' {result[4]:5.1f}  {result[1]}')
+        # printing results of query
+        print (f'# ID    Constellation Size (arcmin)    Mag  Object Type')
+        for result in results:
+            print (f'{result[0]:7s} {result[2]:>13s} {result[3]:13.1f}', \
+                   f' {result[4]:5.1f}  {result[1]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
