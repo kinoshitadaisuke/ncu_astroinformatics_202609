f52967bcc5b41b58690faa19ed5b55712eff9621
Daisuke <mhdf23888@gmail.com>
2026-09-10 17:31:15 +0800

    adding "s01b/ai_s01b_sqlite_00_13.sh" on 10/Sep/2026

--- s01b/ai_s01b_sqlite_00_13.sh
+++ s01b/ai_s01b_sqlite_00_13.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #
-# Time-stamp: <2026/03/19 10:33:18 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:37:41 (UT+08:00) daisuke>
 #
 
 # SQL query
-sqlite3 planet0.db "select * from planet;"
+sqlite3 -list planet0.db "select * from planet;"
