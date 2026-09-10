84f37831f05b47c1c5be5eff9331241a7dacdd85
Daisuke <mhdf23888@gmail.com>
2026-09-10 16:25:40 +0800

    adding "s01b/ai_s01b_03_05.py" on 10/Sep/2026

--- s01b/ai_s01b_03_05.py
+++ s01b/ai_s01b_03_05.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/19 10:39:36 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:13:08 (UT+08:00) daisuke>
 #
 
 # importing sqlite module
@@ -10,26 +10,32 @@ import sqlite3
 # importing contextlib module
 import contextlib
 
-# database file name
-file_db = 'hip.db'
+# main function
+def main ():
+    # database file name
+    file_db = 'hip.db'
 
-# connecting to database
-with contextlib.closing (sqlite3.connect (file_db)) as conn:
-    # constructing a cursor object
-    cursor = conn.cursor ()
+    # connecting to database
+    with contextlib.closing (sqlite3.connect (file_db)) as conn:
+        # constructing a cursor object
+        cursor = conn.cursor ()
 
-    # SQL command for a query
-    sql_query = 'select hip, ra_hms, dec_dms, vmag, bv, parallax, sptype ' \
-        + f'from hip where parallax > 300 order by parallax desc;'
+        # SQL command for a query
+        sql_query = 'select hip, ra_hms, dec_dms, vmag, bv, parallax, sptype ' \
+            + f'from hip where parallax > 300 order by parallax desc;'
 
-    # executing a SQL query
-    cursor.execute (sql_query)
+        # executing a SQL query
+        cursor.execute (sql_query)
 
-    # fetching results of query
-    results = cursor.fetchall ()
+        # fetching results of query
+        results = cursor.fetchall ()
 
-    # printing results of query
-    print (f'# HIP   RA           Dec          Vmag   B-V      p      sptype')
-    for result in results:
-        print (f'{result[0]:06d}  {result[1]}  {result[2]}  {result[3]:5.2f}', \
-               f' {result[4]:7.2f}  {result[5]:5.1f}  {result[6]}')
+        # printing results of query
+        print (f'# HIP   RA           Dec          Vmag   B-V      p      sptype')
+        for result in results:
+            print (f'{result[0]:06d}  {result[1]}  {result[2]}  {result[3]:5.2f}', \
+                   f' {result[4]:7.2f}  {result[5]:5.1f}  {result[6]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
