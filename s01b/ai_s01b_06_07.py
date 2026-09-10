7070814cba4b65dc4eb86edf09ad93e1c5b9694d
Daisuke <mhdf23888@gmail.com>
2026-09-10 16:27:12 +0800

    adding "s01b/ai_s01b_06_07.py" on 10/Sep/2026

--- s01b/ai_s01b_06_07.py
+++ s01b/ai_s01b_06_07.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/21 16:42:18 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:23:04 (UT+08:00) daisuke>
 #
 
 # importing duckdb module
 import duckdb
 
-# database file
-file_db = 'landolt_2009.db'
+# main function
+def main ():
+    # database file
+    file_db = 'landolt_2009.db'
 
-# SQL command to carry out a query
-sql_query = "SELECT name,ra,dec,mag_v,colour_bv FROM landolt WHERE mag_v < 9.0 ORDER BY mag_v;"
+    # SQL command to carry out a query
+    sql_query = "SELECT name,ra,dec,mag_v,colour_bv FROM landolt" \
+        + " WHERE mag_v < 9.0 ORDER BY mag_v;"
 
-# connecting to a new database
-with duckdb.connect (file_db) as connection:
-    # executing a query
-    query_result = connection.sql (sql_query).df ()
-    # printing result of query
-    print (query_result)
+    # connecting to a new database
+    with duckdb.connect (file_db) as connection:
+        # executing a query
+        query_result = connection.sql (sql_query).df ()
+        # printing result of query
+        print (query_result)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
