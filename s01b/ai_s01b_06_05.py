89370b201d1d19b405359f4c48912b52c2badb7b
Daisuke <mhdf23888@gmail.com>
2026-09-10 16:27:03 +0800

    adding "s01b/ai_s01b_06_05.py" on 10/Sep/2026

--- s01b/ai_s01b_06_05.py
+++ s01b/ai_s01b_06_05.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/21 16:46:40 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:18:53 (UT+08:00) daisuke>
 #
 
 # importing duckdb module
 import duckdb
 
-# database file
-file_db = 'landolt_2009.db'
+# main function
+def main ():
+    # database file
+    file_db = 'landolt_2009.db'
 
-# connecting to a new database
-with duckdb.connect (file_db) as connection:
-    # getting table information
-    table_info = connection.sql ('DESCRIBE landolt').df ()
-    # printing table information
-    print (table_info)
+    # connecting to a new database
+    with duckdb.connect (file_db) as connection:
+        # getting table information
+        table_info = connection.sql ('DESCRIBE landolt').df ()
+        # printing table information
+        print (table_info)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
