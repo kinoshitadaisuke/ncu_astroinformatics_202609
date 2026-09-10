442dacc4eef17557cea49d1b82faaa3ede5d544d
Daisuke <mhdf23888@gmail.com>
2026-09-10 16:26:02 +0800

    adding "s01b/ai_s01b_04_03.py" on 10/Sep/2026

--- s01b/ai_s01b_04_03.py
+++ s01b/ai_s01b_04_03.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/19 10:40:17 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/10 16:14:42 (UT+08:00) daisuke>
 #
 
 # importing sqlite module
@@ -10,28 +10,34 @@ import sqlite3
 # importing contextlib module
 import contextlib
 
-# database file name
-file_db = 'mpcorb.db'
+# main function
+def main ():
+    # database file name
+    file_db = 'mpcorb.db'
 
-# connecting to database
-with contextlib.closing (sqlite3.connect (file_db)) as conn:
-    # constructing a cursor object
-    cursor = conn.cursor ()
+    # connecting to database
+    with contextlib.closing (sqlite3.connect (file_db)) as conn:
+        # constructing a cursor object
+        cursor = conn.cursor ()
 
-    # SQL command for a query
-    sql_query = 'select name, a, e, i, node, peri, M, nobs, residual, ' \
-        + f'flag, lastobs, absmag from mpcorb ' \
-        + f'where (a >= 1000.0) order by a desc;'
+        # SQL command for a query
+        sql_query = 'select name, a, e, i, node, peri, M, nobs, residual, ' \
+            + f'flag, lastobs, absmag from mpcorb ' \
+            + f'where (a >= 1000.0) order by a desc;'
 
-    # executing a SQL query
-    cursor.execute (sql_query)
+        # executing a SQL query
+        cursor.execute (sql_query)
 
-    # fetching results of query
-    results = cursor.fetchall ()
+        # fetching results of query
+        results = cursor.fetchall ()
 
-    # printing results of query
-    print (f'# name, a, e, i, node, peri, M, absmag')
-    for result in results:
-        print (f'{result[0]:24s}  {result[1]:8.2f}  {result[2]:5.3f} ', \
-               f'{result[3]:6.2f} {result[4]:6.2f} {result[5]:6.2f} ', \
-               f'{result[6]:6.2f}  {result[11]:7.2f}')
+        # printing results of query
+        print (f'# name, a, e, i, node, peri, M, absmag')
+        for result in results:
+            print (f'{result[0]:24s}  {result[1]:8.2f}  {result[2]:5.3f} ', \
+                   f'{result[3]:6.2f} {result[4]:6.2f} {result[5]:6.2f} ', \
+                   f'{result[6]:6.2f}  {result[11]:7.2f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
