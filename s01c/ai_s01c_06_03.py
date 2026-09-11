9b7ff1258714a75bd4a6cfe3c12ebb577d04f2a1
Daisuke <mhdf23888@gmail.com>
2026-09-11 13:55:51 +0800

    adding "s01c/ai_s01c_06_03.py" on 11/Sep/2026

---
+++ s01c/ai_s01c_06_03.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/04/20 08:42:06 (UT+08:00) daisuke>
+#
+
+# importing pathlib module
+import pathlib
+
+# importing json module
+import json
+
+# list of data files
+files = pathlib.Path ('.').glob ('osc_0000_1989/*.json')
+
+# key names
+key_type = 'claimedtype'
+
+# processing each file
+for file in sorted (files):
+    # opening file
+    with open (file, 'r') as fh:
+        # reading JSON data from file
+        data = json.load (fh)
+
+    # checking all the objects in JSON file
+    for obj in data:
+        # skip if type is not known
+        if not (key_type in data[obj]):
+            continue
+        # skip if supernova is not type-Ia
+        if not (data[obj][key_type][0]['value'] == 'Ia'):
+            continue
+        # printing data
+        print ("%s ==> %s" % (obj, data[obj][key_type][0]['value']) )
