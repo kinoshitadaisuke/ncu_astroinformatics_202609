aa65f08405f7393757fdd97bb973eba1a2863d48
Daisuke <mhdf23888@gmail.com>
2026-09-11 13:54:49 +0800

    adding "s01c/ai_s01c_03_00.py" on 11/Sep/2026

---
+++ s01c/ai_s01c_03_00.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/04/21 11:45:05 (UT+08:00) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://www.sao.ru/lv/lvgdb/tables/lvg_table4.dat'
+
+# output file name
+file_output = 'lvg_v.data'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
