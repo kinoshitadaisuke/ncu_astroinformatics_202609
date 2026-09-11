e6d82bac1e9d4971f5dd19303471db64bb63fde3
Daisuke <mhdf23888@gmail.com>
2026-09-11 13:56:19 +0800

    adding "s01c/ai_s01c_06_11.py" on 11/Sep/2026

---
+++ s01c/ai_s01c_06_11.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/04/20 08:43:12 (UT+08:00) daisuke>
+#
+
+# importing git module
+import git
+
+# URL of repository
+url_repo = 'https://github.com/astrocatalogs/sne-2005-2009.git'
+
+# directory name of downloaded repository
+dir_repo = 'osc_2005_2009'
+
+# downloading repository
+repo = git.Repo.clone_from (url_repo, dir_repo)
