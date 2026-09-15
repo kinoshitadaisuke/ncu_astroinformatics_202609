1b9b5bce2d011301317dcaf8edb46a4c666cd470
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:05 +0800

    adding "s01c/ai_s01c_02_01.py" on 15/Sep/2026

--- s01c/ai_s01c_02_01.py
+++ s01c/ai_s01c_02_01.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:38:46 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:36:24 (UT+08:00) daisuke>
 #
 
 # importing json module
 import json
 
-# JSON file name
-file_json = 'exoplanets/data/exoplanet.json'
+# main function
+def main ():
+    # JSON file name
+    file_json = 'exoplanets/data/exoplanet.json'
 
-# opening file
-with open (file_json, 'r') as fh:
-    # reading JSON file
-    data = json.load (fh)
+    # opening file
+    with open (file_json, 'r') as fh:
+        # reading JSON file
+        data = json.load (fh)
 
-# printing type of data
-print (f'type (data)    = {type (data)}')
+    # printing type of data
+    print (f'type (data)    = {type (data)}')
 
-# printing type of data[0]
-print (f'type (data[0]) = {type (data[0])}')
+    # printing type of data[0]
+    print (f'type (data[0]) = {type (data[0])}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
