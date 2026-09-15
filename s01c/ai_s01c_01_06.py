138e36ad1cebaad8c2ffdc9292d8760aaab284a1
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:50:56 +0800

    adding "s01c/ai_s01c_01_06.py" on 15/Sep/2026

--- s01c/ai_s01c_01_06.py
+++ s01c/ai_s01c_01_06.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:38:20 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:35:29 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
-# CSV file name
-file_csv = 'hyg/hyg/v3/hyg_v38.csv.gz'
+# main function
+def main ():
+    # CSV file name
+    file_csv = 'hyg/hyg/v3/hyg_v38.csv.gz'
 
-# reading a CSV file and storing data in an astropy table
-table = astropy.io.ascii.read (file_csv, format='csv')
+    # reading a CSV file and storing data in an astropy table
+    table = astropy.io.ascii.read (file_csv, format='csv')
 
-# printing astropy table
-print (table)
+    # printing astropy table
+    print (table)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
