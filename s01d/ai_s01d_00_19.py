d9a8e5a40de7cee2271a1867dfe4508fc8fe6456
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:26:14 +0800

    adding "s01d/ai_s01d_00_19.py" on 15/Sep/2026

--- s01d/ai_s01d_00_19.py
+++ s01d/ai_s01d_00_19.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/09/15 12:09:30 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:21:23 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -69,7 +69,7 @@ def wi_nu (frequency, T):
 # main function
 def main ():
     # output file name
-    file_output = 'appy_s08_00_19.png'
+    file_output = 'ai_s01d_00_19.png'
 
     # resolution in DPI
     resolution_dpi = 150
