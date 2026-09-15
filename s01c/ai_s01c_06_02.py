1894108834ac8c83705daa5e36f4b095ceb589c6
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:52:28 +0800

    adding "s01c/ai_s01c_06_02.py" on 15/Sep/2026

--- s01c/ai_s01c_06_02.py
+++ s01c/ai_s01c_06_02.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:41:59 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:43:23 (UT+08:00) daisuke>
 #
 
 # importing json module
 import json
 
-# file name
-file_json = 'osc_0000_1989/SN1989V.json'
+# main function
+def main ():
+    # file name
+    file_json = 'osc_0000_1989/SN1989V.json'
 
-# opening file
-with open (file_json, 'r') as fh:
-    # reading JSON data from file
-    data = json.load (fh)
+    # opening file
+    with open (file_json, 'r') as fh:
+        # reading JSON data from file
+        data = json.load (fh)
 
-# printing data
-for obj in data:
-    print ("obj =", obj)
-    for key in data[obj]:
-        print ("  %-16s ==> %s" % (key, data[obj][key]) )
+    # printing data
+    for obj in data:
+        print ("obj =", obj)
+        for key in data[obj]:
+            print ("  %-16s ==> %s" % (key, data[obj][key]) )
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
