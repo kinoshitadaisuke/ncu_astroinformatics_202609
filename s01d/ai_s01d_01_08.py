7513ac35c5b2288536a6e57332937de8e4b4bb34
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:26:51 +0800

    adding "s01d/ai_s01d_01_08.py" on 15/Sep/2026

--- s01d/ai_s01d_01_08.py
+++ s01d/ai_s01d_01_08.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/09/15 12:12:33 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:22:27 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -18,7 +18,7 @@ import matplotlib.backends.backend_agg
 # main function
 def main ():
     # output file name
-    file_output = 'appy_s08_01_08.png'
+    file_output = 'ai_s01d_01_08.png'
 
     # resolution in DPI
     resolution_dpi = 150
