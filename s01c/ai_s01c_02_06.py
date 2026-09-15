d6b146dff1458c4e7ac973c72f70377a435cc44d
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:22 +0800

    adding "s01c/ai_s01c_02_06.py" on 15/Sep/2026

--- s01c/ai_s01c_02_06.py
+++ s01c/ai_s01c_02_06.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:39:23 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:37:49 (UT+08:00) daisuke>
 #
 
 # importing json module
 import json
 
-# JSON file name
-file_json = 'bsc/bsc5-all.json'
+# main function
+def main ():
+    # JSON file name
+    file_json = 'bsc/bsc5-all.json'
 
-# opening file
-with open (file_json, 'r') as fh:
-    # reading JSON file
-    data = json.load (fh)
+    # opening file
+    with open (file_json, 'r') as fh:
+        # reading JSON file
+        data = json.load (fh)
 
-# printing keys of the data
-for key in data[0].keys ():
-    print (f"{key}")
+    # printing keys of the data
+    for key in data[0].keys ():
+        print (f"{key}")
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
