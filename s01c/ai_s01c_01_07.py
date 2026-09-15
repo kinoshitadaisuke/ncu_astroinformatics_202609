f72431fce40db4e297de6b4f0bd3a57030e1aed9
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:50:59 +0800

    adding "s01c/ai_s01c_01_07.py" on 15/Sep/2026

--- s01c/ai_s01c_01_07.py
+++ s01c/ai_s01c_01_07.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:38:27 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:35:46 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
-# CSV file name
-file_csv = 'hyg/hyg/v3/hyg_v38.csv.gz'
+# main function
+def main ():
+    # CSV file name
+    file_csv = 'hyg/hyg/v3/hyg_v38.csv.gz'
 
-# reading a CSV file and storing data in an astropy table
-table = astropy.io.ascii.read (file_csv, format='csv')
+    # reading a CSV file and storing data in an astropy table
+    table = astropy.io.ascii.read (file_csv, format='csv')
 
-# making a mask for Vega
-obj  = 'Vega'
-mask = (table['proper'] == obj)
+    # making a mask for Vega
+    obj  = 'Vega'
+    mask = (table['proper'] == obj)
 
-# printing information of Vega
-print (f"object name = {obj}")
-print (table[mask]['proper', 'con', 'ra', 'dec', \
-                   'mag', 'dist', 'absmag', 'spect'])
+    # printing information of Vega
+    print (f"object name = {obj}")
+    print (table[mask]['proper', 'con', 'ra', 'dec', \
+                       'mag', 'dist', 'absmag', 'spect'])
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
