bae4d9cbf3f0d4da1b7c26b7988616171f09a367
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:53:25 +0800

    adding "s01c/ai_s01c_07_02.py" on 15/Sep/2026

--- s01c/ai_s01c_07_02.py
+++ s01c/ai_s01c_07_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:44:19 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:48:25 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -13,52 +13,58 @@ import pathlib
 # import tarfile module
 import tarfile
 
-# choices
-choices_compression = ['none', 'gz', 'bz2', 'xz']
+# main function
+def main ():
+    # choices
+    choices_compression = ['none', 'gz', 'bz2', 'xz']
 
-# command-line argument analysis
-desc   = 'extracting files in gzipped tar files'
-parser = argparse.ArgumentParser (description=desc)
-parser.add_argument ('-c', '--compression', default='', \
-                     choices=choices_compression, \
-                     help='compression type (default: none)')
-parser.add_argument ('files', nargs='+', help='files')
+    # command-line argument analysis
+    desc   = 'extracting files in gzipped tar files'
+    parser = argparse.ArgumentParser (description=desc)
+    parser.add_argument ('-c', '--compression', default='', \
+                         choices=choices_compression, \
+                         help='compression type (default: none)')
+    parser.add_argument ('files', nargs='+', help='files')
 
-# command-line arguments analysis
-args = parser.parse_args ()
+    # command-line arguments analysis
+    args = parser.parse_args ()
 
-# input parameters
-compression = args.compression
-list_files  = args.files
+    # input parameters
+    compression = args.compression
+    list_files  = args.files
 
-# mode for opening tar file
-if (compression == 'gz'):
-    mode = 'r:gz'
-elif (compression == 'bz2'):
-    mode = 'r:bz2'
-elif (compression == 'xz'):
-    mode = 'r:xz'
-else:
-    mode = 'r:'
+    # mode for opening tar file
+    if (compression == 'gz'):
+        mode = 'r:gz'
+    elif (compression == 'bz2'):
+        mode = 'r:bz2'
+    elif (compression == 'xz'):
+        mode = 'r:xz'
+    else:
+        mode = 'r:'
 
-# processing each file
-for file_tar in list_files:
-    # making pathlib object
-    path_tar = pathlib.Path (file_tar)
-    # existence check
-    if not (path_tar.exists ()):
-        # printing message
-        print (f'ERROR:')
-        print (f'ERROR: file "{file_tar}" does not exists!')
-        print (f'ERROR:')
-        # skipping
-        continue
-    # opening tar file
-    with tarfile.open (name=file_tar, mode=mode) as tar:
-        # getting a list of files in tar file
-        list_members = tar.getmembers ()
-        # printing a list of files in tar file
-        for member in list_members:
-            print (f'{member.name}    ({member.size} byte)')
-        # extracting files in tar file
-        tar.extractall ()
+    # processing each file
+    for file_tar in list_files:
+        # making pathlib object
+        path_tar = pathlib.Path (file_tar)
+        # existence check
+        if not (path_tar.exists ()):
+            # printing message
+            print (f'ERROR:')
+            print (f'ERROR: file "{file_tar}" does not exists!')
+            print (f'ERROR:')
+            # skipping
+            continue
+        # opening tar file
+        with tarfile.open (name=file_tar, mode=mode) as tar:
+            # getting a list of files in tar file
+            list_members = tar.getmembers ()
+            # printing a list of files in tar file
+            for member in list_members:
+                print (f'{member.name}    ({member.size} byte)')
+            # extracting files in tar file
+            tar.extractall ()
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
