cb418c7bd802063a9b46c6b42c4fdc25cb58ab6a
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:20:32 +0800

    adding "s01d/ai_s01d_00_19.py" on 15/Sep/2026

--- s01d/ai_s01d_00_19.py
+++ s01d/ai_s01d_00_19.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/10 10:29:02 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:09:30 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -13,12 +13,6 @@ import scipy.constants
 # importing matplotlib module
 import matplotlib.figure
 import matplotlib.backends.backend_agg
-
-# output file name
-file_output = 'appy_s08_00_19.png'
-
-# resolution in DPI
-resolution_dpi = 150
 
 #
 # function to calculate blackbody curve
@@ -72,94 +66,106 @@ def wi_nu (frequency, T):
     # returning blackbody curve by Wien's law
     return (blackbody)
 
-# temperature of blackbody
-T = 5800.0
+# main function
+def main ():
+    # output file name
+    file_output = 'appy_s08_00_19.png'
 
-# printing temperature of blackbody
-print (f'Temperature:')
-print (f'  T = {T} K')
+    # resolution in DPI
+    resolution_dpi = 150
 
-# range of frequency (from 10**2 Hz to 10**16 Hz)
-frequency_min = 2.0
-frequency_max = 16.0
+    # temperature of blackbody
+    T = 5800.0
 
-# frequency in Hz
-frequency = numpy.logspace (frequency_min, frequency_max, num=14001)
+    # printing temperature of blackbody
+    print (f'Temperature:')
+    print (f'  T = {T} K')
 
-# T = 5800 K blackbody spectrum
-bb_5800 = bb_nu (frequency, T)
-rj_5800 = rj_nu (frequency, T)
-wi_5800 = wi_nu (frequency, T)
+    # range of frequency (from 10**2 Hz to 10**16 Hz)
+    frequency_min = 2.0
+    frequency_max = 16.0
 
-# printing Planck function
-print (f'Frequency:')
-print (f'{frequency}')
-print (f'Planck function:')
-print (f'{bb_5800}')
-print (f'Rayleigh-Jeans law:')
-print (f'{rj_5800}')
-print (f'Wien\'s law:')
-print (f'{wi_5800}')
+    # frequency in Hz
+    frequency = numpy.logspace (frequency_min, frequency_max, num=14001)
 
-# making objects "fig", "canvas", and "ax"
-fig    = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax     = fig.add_subplot (111)
+    # T = 5800 K blackbody spectrum
+    bb_5800 = bb_nu (frequency, T)
+    rj_5800 = rj_nu (frequency, T)
+    wi_5800 = wi_nu (frequency, T)
 
-# labels
-ax.set_xlabel (r'Frequency [Hz]')
-ax.set_ylabel (r'Specific Intensity [W sr$^{-1}$ m$^{-2}$ Hz$^{-1}$]')
+    # printing Planck function
+    print (f'Frequency:')
+    print (f'{frequency}')
+    print (f'Planck function:')
+    print (f'{bb_5800}')
+    print (f'Rayleigh-Jeans law:')
+    print (f'{rj_5800}')
+    print (f'Wien\'s law:')
+    print (f'{wi_5800}')
 
-# axes
-ax.set_xscale ('log')
-ax.set_yscale ('log')
-ax.set_xlim (10**2, 10**17)
-ax.set_ylim (10**-30, 10**-3)
+    # making objects "fig", "canvas", and "ax"
+    fig    = matplotlib.figure.Figure ()
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    ax     = fig.add_subplot (111)
 
-# make secondary X-axis
-c   = scipy.constants.physical_constants['speed of light in vacuum'][0]
-ax2 = ax.secondary_xaxis (location='top', \
-                          functions=(lambda x: c/x, lambda x: c/x) )
-ax2.set_xlabel (r'Wavelength [m]')
+    # labels
+    ax.set_xlabel (r'Frequency [Hz]')
+    ax.set_ylabel (r'Specific Intensity [W sr$^{-1}$ m$^{-2}$ Hz$^{-1}$]')
 
-# showing X-ray region
-ax.fill_between ([3*10**16, 10**18], 10**-30, 10**-3, \
-                 color='cyan', alpha=0.1)
-ax.text (x=4*10**16, y=10**-29, s='X-ray')
+    # axes
+    ax.set_xscale ('log')
+    ax.set_yscale ('log')
+    ax.set_xlim (10**2, 10**17)
+    ax.set_ylim (10**-30, 10**-3)
 
-# showing UV region
-ax.fill_between ([10**15, 3*10**16], 10**-30, 10**-3, \
-                 color='violet', alpha=0.1)
-ax.text (x=1.1*10**15, y=10**-29, s='UV')
+    # make secondary X-axis
+    c   = scipy.constants.physical_constants['speed of light in vacuum'][0]
+    ax2 = ax.secondary_xaxis (location='top', \
+                              functions=(lambda x: c/x, lambda x: c/x) )
+    ax2.set_xlabel (r'Wavelength [m]')
 
-# showing visible region
-ax.fill_between ([3*10**14, 10**15], 10**-30, 10**-3, \
-                 color='green', alpha=0.1)
-ax.text (x=10**14, y=3*10**-28, s='Visible')
+    # showing X-ray region
+    ax.fill_between ([3*10**16, 10**18], 10**-30, 10**-3, \
+                     color='cyan', alpha=0.1)
+    ax.text (x=4*10**16, y=10**-29, s='X-ray')
 
-# showing IR region
-ax.fill_between ([10**12, 3*10**14], 10**-30, 10**-3, \
-                 color='red', alpha=0.1)
-ax.text (x=10**13, y=10**-29, s='IR')
+    # showing UV region
+    ax.fill_between ([10**15, 3*10**16], 10**-30, 10**-3, \
+                     color='violet', alpha=0.1)
+    ax.text (x=1.1*10**15, y=10**-29, s='UV')
 
-# showing radio region
-ax.fill_between ([10**0, 10**12], 10**-30, 10**-3, color='yellow', alpha=0.1)
-ax.text (x=10**8, y=10**-29, s='Radio')
+    # showing visible region
+    ax.fill_between ([3*10**14, 10**15], 10**-30, 10**-3, \
+                     color='green', alpha=0.1)
+    ax.text (x=10**14, y=3*10**-28, s='Visible')
 
-# plotting data
-ax.plot (frequency, bb_5800, \
-         linestyle='-', linewidth=5, color='red', \
-         zorder=0.1, \
-         label='Blackbody of T = 5800 K')
-ax.plot (frequency, rj_5800, \
-         linestyle='--', linewidth=3, color='blue', \
-         zorder=0.2, \
-         label='Rayleigh-Jeans law for T = 5800 K')
-ax.plot (frequency, wi_5800, \
-         linestyle=':', linewidth=3, color='green', \
-         zorder=0.3, \
-         label='Wien\'s law for T = 5800 K')
-ax.legend ()
+    # showing IR region
+    ax.fill_between ([10**12, 3*10**14], 10**-30, 10**-3, \
+                     color='red', alpha=0.1)
+    ax.text (x=10**13, y=10**-29, s='IR')
 
-# saving the plot into a file
-fig.savefig (file_output, dpi=resolution_dpi)
+    # showing radio region
+    ax.fill_between ([10**0, 10**12], 10**-30, 10**-3, color='yellow', alpha=0.1)
+    ax.text (x=10**8, y=10**-29, s='Radio')
+
+    # plotting data
+    ax.plot (frequency, bb_5800, \
+             linestyle='-', linewidth=5, color='red', \
+             zorder=0.1, \
+             label='Blackbody of T = 5800 K')
+    ax.plot (frequency, rj_5800, \
+             linestyle='--', linewidth=3, color='blue', \
+             zorder=0.2, \
+             label='Rayleigh-Jeans law for T = 5800 K')
+    ax.plot (frequency, wi_5800, \
+             linestyle=':', linewidth=3, color='green', \
+             zorder=0.3, \
+             label='Wien\'s law for T = 5800 K')
+    ax.legend ()
+
+    # saving the plot into a file
+    fig.savefig (file_output, dpi=resolution_dpi)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
