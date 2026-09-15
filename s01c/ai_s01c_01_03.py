e4b1f90ea2a723ce33ed4076a02b594d6068852c
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:50:46 +0800

    adding "s01c/ai_s01c_01_03.py" on 15/Sep/2026

--- s01c/ai_s01c_01_03.py
+++ s01c/ai_s01c_01_03.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:37:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:34:40 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
-# CSV file name
-file_csv = 'honey-badger/examples/planets/planets.csv'
+# main function
+def main ():
+    # CSV file name
+    file_csv = 'honey-badger/examples/planets/planets.csv'
 
-# reading a CSV file and storing data in an astropy table
-table = astropy.io.ascii.read (file_csv, format='csv')
+    # reading a CSV file and storing data in an astropy table
+    table = astropy.io.ascii.read (file_csv, format='csv')
 
-# printing the column for mean temperature
-print (f'{table["Planet", "Mean Temperature (C)"]}')
+    # printing the column for mean temperature
+    print (f'{table["Planet", "Mean Temperature (C)"]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
