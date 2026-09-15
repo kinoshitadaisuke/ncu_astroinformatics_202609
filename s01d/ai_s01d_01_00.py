645e808b439c631ff7a855aa01eb98dbe9ed099c
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:20:40 +0800

    adding "s01d/ai_s01d_01_00.py" on 15/Sep/2026

--- s01d/ai_s01d_01_00.py
+++ s01d/ai_s01d_01_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/30 22:22:50 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:10:30 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -11,33 +11,39 @@ import numpy
 import astropy.modeling.models
 import astropy.units
 
-# units
-unit_K  = astropy.units.K
-unit_Hz = astropy.units.Hz
+# main function
+def main ():
+    # units
+    unit_K  = astropy.units.K
+    unit_Hz = astropy.units.Hz
 
-# temperature of blackbody
-T = 8000.0 * unit_K
+    # temperature of blackbody
+    T = 8000.0 * unit_K
 
-# printing temperature of blackbody
-print (f'Temperature:')
-print (f'  T = {T}')
+    # printing temperature of blackbody
+    print (f'Temperature:')
+    print (f'  T = {T}')
 
-# range of frequency (from 10**9 to 10**16)
-frequency_min =  9.0
-frequency_max = 16.0
+    # range of frequency (from 10**9 to 10**16)
+    frequency_min =  9.0
+    frequency_max = 16.0
 
-# frequency in Hz
-frequency_Hz = numpy.logspace (frequency_min, frequency_max, \
-                               num=7001, dtype=numpy.longdouble) * unit_Hz
+    # frequency in Hz
+    frequency_Hz = numpy.logspace (frequency_min, frequency_max, \
+                                   num=7001, dtype=numpy.longdouble) * unit_Hz
 
-# a function to calculate blackbody radiation for T=8000 K
-bb_model = astropy.modeling.models.BlackBody (temperature=T)
+    # a function to calculate blackbody radiation for T=8000 K
+    bb_model = astropy.modeling.models.BlackBody (temperature=T)
 
-# calculation of blackbody radiation
-bb_data = bb_model (frequency_Hz)
+    # calculation of blackbody radiation
+    bb_data = bb_model (frequency_Hz)
 
-# printing blackbody radiation
-print (f'Frequency:')
-print (f'{frequency_Hz}')
-print (f'Blackbody radiation:')
-print (f'{bb_data}')
+    # printing blackbody radiation
+    print (f'Frequency:')
+    print (f'{frequency_Hz}')
+    print (f'Blackbody radiation:')
+    print (f'{bb_data}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
