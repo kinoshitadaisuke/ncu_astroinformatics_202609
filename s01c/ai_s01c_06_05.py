cdf67a5e08af67a59e8d887e04d5ef7d1c9580ce
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:52:38 +0800

    adding "s01c/ai_s01c_06_05.py" on 15/Sep/2026

--- s01c/ai_s01c_06_05.py
+++ s01c/ai_s01c_06_05.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:42:21 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:44:16 (UT+08:00) daisuke>
 #
 
 # importing argparse module
@@ -14,66 +14,72 @@ import numpy
 import matplotlib.figure
 import matplotlib.backends.backend_agg
 
-# command-line argument analysis
-desc   = 'making Hubble diagram using SNIa from Open Supernova Catalog'
-parser = argparse.ArgumentParser (description=desc)
-parser.add_argument ('-i', '--input', help='input data file name')
-parser.add_argument ('-o', '--output', help='output figure file name')
-parser.add_argument ('-r', '--resolution', type=float, default=150.0, \
-                     help='resolution in DPI (default: 150)')
+# main function
+def main ():
+    # command-line argument analysis
+    desc   = 'making Hubble diagram using SNIa from Open Supernova Catalog'
+    parser = argparse.ArgumentParser (description=desc)
+    parser.add_argument ('-i', '--input', help='input data file name')
+    parser.add_argument ('-o', '--output', help='output figure file name')
+    parser.add_argument ('-r', '--resolution', type=float, default=150.0, \
+                         help='resolution in DPI (default: 150)')
 
-# command-line arguments analysis
-args = parser.parse_args ()
+    # command-line arguments analysis
+    args = parser.parse_args ()
 
-# input parameters
-file_data      = args.input
-file_fig       = args.output
-resolution_dpi = args.resolution
+    # input parameters
+    file_data      = args.input
+    file_fig       = args.output
+    resolution_dpi = args.resolution
 
-# numpy arrays to store data
-data_d = numpy.array ([])
-data_v = numpy.array ([])
-data_z = numpy.array ([])
+    # numpy arrays to store data
+    data_d = numpy.array ([])
+    data_v = numpy.array ([])
+    data_z = numpy.array ([])
 
-# opening file
-with open (file_data, 'r') as fh:
-    # reading data
-    for line in fh:
-        # skip the line, if it starts with '#'
-        if (line[0] == '#'):
-            continue
-        # splitting the line
-        data  = line.split ()
-        # extracting data
-        d1 = float (data[0])
-        d2 = float (data[1])
-        z  = float (data[2])
-        v  = float (data[3])
-        # checking distance
-        diff = numpy.absolute (d1 - d2)
-        diff_rel = diff / d2
-        if (diff_rel > 0.1):
-            continue
-        # appending data to numpy arrays
-        data_d = numpy.append (data_d, d2)
-        data_v = numpy.append (data_v, v)
-        data_z = numpy.append (data_z, z)
+    # opening file
+    with open (file_data, 'r') as fh:
+        # reading data
+        for line in fh:
+            # skip the line, if it starts with '#'
+            if (line[0] == '#'):
+                continue
+            # splitting the line
+            data  = line.split ()
+            # extracting data
+            d1 = float (data[0])
+            d2 = float (data[1])
+            z  = float (data[2])
+            v  = float (data[3])
+            # checking distance
+            diff = numpy.absolute (d1 - d2)
+            diff_rel = diff / d2
+            if (diff_rel > 0.1):
+                continue
+            # appending data to numpy arrays
+            data_d = numpy.append (data_d, d2)
+            data_v = numpy.append (data_v, v)
+            data_z = numpy.append (data_z, z)
 
-# making objects "fig", "canvas", and "ax"
-fig    = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax     = fig.add_subplot (111)
+    # making objects "fig", "canvas", and "ax"
+    fig    = matplotlib.figure.Figure ()
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    ax     = fig.add_subplot (111)
 
-# axes
-ax.set_xlabel ('Distance [Mpc]')
-ax.set_ylabel ('Velocity [km/s]')
-ax.grid ()
+    # axes
+    ax.set_xlabel ('Distance [Mpc]')
+    ax.set_ylabel ('Velocity [km/s]')
+    ax.grid ()
 
-# making a Hubble diagram
-ax.plot (data_d, data_v, \
-         linestyle='None', marker='o', markersize=2, color='blue', \
-         label='type-Ia supernovae fro Open Supernova Catalogue')
-ax.legend ()
+    # making a Hubble diagram
+    ax.plot (data_d, data_v, \
+             linestyle='None', marker='o', markersize=2, color='blue', \
+             label='type-Ia supernovae fro Open Supernova Catalogue')
+    ax.legend ()
 
-# saving the plot into a file
-fig.savefig (file_fig, dpi=resolution_dpi, bbox_inches="tight")
+    # saving the plot into a file
+    fig.savefig (file_fig, dpi=resolution_dpi, bbox_inches="tight")
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
