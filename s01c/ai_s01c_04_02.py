2f9b4e810f21cf5030fa3193458d101154d0fc2b
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:48 +0800

    adding "s01c/ai_s01c_04_02.py" on 15/Sep/2026

--- s01c/ai_s01c_04_02.py
+++ s01c/ai_s01c_04_02.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:40:33 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:40:07 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
-# file
-file_data = 'ned1d_new.csv'
+# main function
+def main ():
+    # file
+    file_data = 'ned1d_new.csv'
 
-# reading CSV data
-rawdata = astropy.io.ascii.read (file_data, format='csv')
+    # reading CSV data
+    rawdata = astropy.io.ascii.read (file_data, format='csv')
 
-# printing astropy table summary information
-print (rawdata.info ())
+    # printing astropy table summary information
+    print (rawdata.info ())
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
