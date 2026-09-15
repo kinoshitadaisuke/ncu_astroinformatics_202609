06401c64fce9ff39a8c08085b957748cc8d7cefc
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:26:06 +0800

    adding "s01d/ai_s01d_00_17.py" on 15/Sep/2026

--- s01d/ai_s01d_00_17.py
+++ s01d/ai_s01d_00_17.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/09/15 12:08:34 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:21:09 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -51,7 +51,7 @@ def rj_nu (frequency, T):
 # main function
 def main ():
     # output file name
-    file_output = 'appy_s08_00_17.png'
+    file_output = 'ai_s01d_00_17.png'
 
     # resolution in DPI
     resolution_dpi = 150
