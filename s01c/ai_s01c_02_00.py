fa1f48a5fb14d3b53efd55bb73f641b12ca40d14
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:02 +0800

    adding "s01c/ai_s01c_02_00.py" on 15/Sep/2026

--- s01c/ai_s01c_02_00.py
+++ s01c/ai_s01c_02_00.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:38:39 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:36:08 (UT+08:00) daisuke>
 #
 
 # importing git module
 import git
 
-# URL of repository
-url_repo = 'https://github.com/paulfitz/exoplanets.git'
+# main function
+def main ():
+    # URL of repository
+    url_repo = 'https://github.com/paulfitz/exoplanets.git'
 
-# directory name of downloaded repository
-dir_repo = 'exoplanets'
+    # directory name of downloaded repository
+    dir_repo = 'exoplanets'
 
-# downloading repository
-repo = git.Repo.clone_from (url_repo, dir_repo)
+    # downloading repository
+    repo = git.Repo.clone_from (url_repo, dir_repo)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
