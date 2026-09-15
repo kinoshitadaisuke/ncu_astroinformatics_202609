bf7fc890da777584c913a26077eb300cea2c96a6
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:45 +0800

    adding "s01c/ai_s01c_04_01.py" on 15/Sep/2026

--- s01c/ai_s01c_04_01.py
+++ s01c/ai_s01c_04_01.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:40:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:39:52 (UT+08:00) daisuke>
 #
 
-# original CSV file
-file_original = 'ned1d.csv'
+# main function
+def main ():
+    # original CSV file
+    file_original = 'ned1d.csv'
 
-# output new CSV file
-file_new = 'ned1d_new.csv'
+    # output new CSV file
+    file_new = 'ned1d_new.csv'
 
-# header for new CSV file
-header = f'"Exclusion Code","D","G","Galaxy ID","m-M","err","D (Mpc)",' \
-    + f'"Method","REFCODE","","","Hubble const.","Adopted LMC modulus",' \
-    + f'"Date (Yr. - 1980)","NOTES"\n'
+    # header for new CSV file
+    header = f'"Exclusion Code","D","G","Galaxy ID","m-M","err","D (Mpc)",' \
+        + f'"Method","REFCODE","","","Hubble const.","Adopted LMC modulus",' \
+        + f'"Date (Yr. - 1980)","NOTES"\n'
 
-# opening new CSV file for writing
-with open (file_new, 'w') as fh_w:
-    # writing a new header
-    fh_w.write (header)
-    # opening original CSV file for reading
-    with open (file_original, 'r') as fh_r:
-        # reading the file line-by-line
-        for line in fh_r:
-            # if finding "FRN" at the beginning of the line, then skip
-            if (line[:5] == '"FRN"'):
-                continue
-            # writing data into new file
-            fh_w.write (line)
+    # opening new CSV file for writing
+    with open (file_new, 'w') as fh_w:
+        # writing a new header
+        fh_w.write (header)
+        # opening original CSV file for reading
+        with open (file_original, 'r') as fh_r:
+            # reading the file line-by-line
+            for line in fh_r:
+                # if finding "FRN" at the beginning of the line, then skip
+                if (line[:5] == '"FRN"'):
+                    continue
+                # writing data into new file
+                fh_w.write (line)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
