8ae346e66bc4a0c3ad4ac99fe0e05d177d59117f
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:19:13 +0800

    adding "s01d/ai_s01d_00_00.py" on 15/Sep/2026

--- s01d/ai_s01d_00_00.py
+++ s01d/ai_s01d_00_00.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/30 22:20:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:02:04 (UT+08:00) daisuke>
 #
 
 # importing scipy module
 import scipy.constants
 
-#
-# constants
-#
+# main function
+def main ():
+    #
+    # constants
+    #
 
-# speed of light in vacuum
-c = scipy.constants.physical_constants['speed of light in vacuum']
+    # speed of light in vacuum
+    c = scipy.constants.physical_constants['speed of light in vacuum']
 
-# Planck constant
-h = scipy.constants.physical_constants['Planck constant']
+    # Planck constant
+    h = scipy.constants.physical_constants['Planck constant']
 
-# Boltzmann constant
-k = scipy.constants.physical_constants['Boltzmann constant']
+    # Boltzmann constant
+    k = scipy.constants.physical_constants['Boltzmann constant']
 
-# printing values and units of constants
-print (f'Constants:')
-print (f'  c = {c[0]:g} [{c[1]}]')
-print (f'  h = {h[0]:g} [{h[1]}]')
-print (f'  k = {k[0]:g} [{k[1]}]')
+    # printing values and units of constants
+    print (f'Constants:')
+    print (f'  c = {c[0]:g} [{c[1]}]')
+    print (f'  h = {h[0]:g} [{h[1]}]')
+    print (f'  k = {k[0]:g} [{k[1]}]')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
