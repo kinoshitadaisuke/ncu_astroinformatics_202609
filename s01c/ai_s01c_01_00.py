a6a464900bea69e7358ac0ff2f80037a09caad5a
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:50:36 +0800

    adding "s01c/ai_s01c_01_00.py" on 15/Sep/2026

--- s01c/ai_s01c_01_00.py
+++ s01c/ai_s01c_01_00.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:37:23 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:33:43 (UT+08:00) daisuke>
 #
 
 # importing subprocess module
 import subprocess
 
-# URL of GitHub repository
-url_repo = 'https://github.com/architecture-building-systems/honey-badger.git'
+# main function
+def main ():
+    # URL of GitHub repository
+    url_repo = 'https://github.com/architecture-building-systems/honey-badger.git'
 
-# command for downloading GitHub repository
-command_git = f'git clone {url_repo}'
+    # command for downloading GitHub repository
+    command_git = f'git clone {url_repo}'
 
-# downloading GitHub repository
-subprocess.run (command_git, shell=True)
+    # downloading GitHub repository
+    subprocess.run (command_git, shell=True)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
