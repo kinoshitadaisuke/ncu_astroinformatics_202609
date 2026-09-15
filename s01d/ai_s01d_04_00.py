3596eb4ab0aa48688b37eb742d83c8b21f5f565d
Daisuke <mhdf23888@gmail.com>
2026-09-15 12:22:32 +0800

    adding "s01d/ai_s01d_04_00.py" on 15/Sep/2026

--- s01d/ai_s01d_04_00.py
+++ s01d/ai_s01d_04_00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/30 22:26:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 12:17:39 (UT+08:00) daisuke>
 #
 
 # importing urllib module
@@ -10,37 +10,43 @@ import urllib.request
 # importing ssl module
 import ssl
 
-# allow insecure downloading
-ssl._create_default_https_context = ssl._create_unverified_context
+# main function
+def main ():
+    # allow insecure downloading
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# URL of data file
-url_data = 'https://lambda.gsfc.nasa.gov/data/cobe/firas/monopole_spec/' \
-    + 'firas_monopole_spec_v1.txt'
+    # URL of data file
+    url_data = 'https://lambda.gsfc.nasa.gov/data/cobe/firas/monopole_spec/' \
+        + 'firas_monopole_spec_v1.txt'
 
-# output file name
-file_output = 'cmb_cobe.data'
+    # output file name
+    file_output = 'cmb_cobe.data'
 
-# printing status
-print (f'Fetching {url_data}...')
+    # printing status
+    print (f'Fetching {url_data}...')
 
-# opening URL
-with urllib.request.urlopen (url_data) as fh_read:
-    # reading data
-    data_byte = fh_read.read ()
+    # opening URL
+    with urllib.request.urlopen (url_data) as fh_read:
+        # reading data
+        data_byte = fh_read.read ()
 
-# printing status
-print (f'Fetched {url_data}!')
+    # printing status
+    print (f'Fetched {url_data}!')
 
-# converting raw byte data into string
-data_str = data_byte.decode ('utf-8')
+    # converting raw byte data into string
+    data_str = data_byte.decode ('utf-8')
 
-# printing status
-print (f'Now, writing data into file "{file_output}"...')
+    # printing status
+    print (f'Now, writing data into file "{file_output}"...')
 
-# opening file for writing
-with open (file_output, 'w') as fh_write:
-    # writing data
-    fh_write.write (data_str)
+    # opening file for writing
+    with open (file_output, 'w') as fh_write:
+        # writing data
+        fh_write.write (data_str)
 
-# printing status
-print (f'Finished writing data into file "{file_output}"!')
+    # printing status
+    print (f'Finished writing data into file "{file_output}"!')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
