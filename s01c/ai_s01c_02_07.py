96a83a1bcc84e468d956e1c750ea1b40e1318116
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:51:25 +0800

    adding "s01c/ai_s01c_02_07.py" on 15/Sep/2026

--- s01c/ai_s01c_02_07.py
+++ s01c/ai_s01c_02_07.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:39:35 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:38:07 (UT+08:00) daisuke>
 #
 
 # importing json module
 import json
 
-# JSON file name
-file_json = 'bsc/bsc5-all.json'
+# main function
+def main ():
+    # JSON file name
+    file_json = 'bsc/bsc5-all.json'
 
-# opening file
-with open (file_json, 'r') as fh:
-    # reading JSON file
-    data = json.load (fh)
+    # opening file
+    with open (file_json, 'r') as fh:
+        # reading JSON file
+        data = json.load (fh)
 
-# printing information of stars brighter than 1.5 mag
-print (f'# HR number, RA, Dec, Vmag, B-V, sptype')
-for i in range (len (data)):
-    if (float (data[i]['Vmag']) < 1.5):
-        RA = f'{data[i]["RAh"]}h{data[i]["RAm"]}m{data[i]["RAs"]}s'
-        Dec = f'{data[i]["DE-"]}{data[i]["DEd"]}d{data[i]["DEm"]}m{data[i]["DEs"]}s'
-        print (f'{data[i]["HR"]:>5s}  {RA}  {Dec}', \
-               f' {float (data[i]["Vmag"]):+5.2f}', \
-               f' {float (data[i]["B-V"]):+5.2f}', \
-               f' {data[i]["SpType"]}')
+    # printing information of stars brighter than 1.5 mag
+    print (f'# HR number, RA, Dec, Vmag, B-V, sptype')
+    for i in range (len (data)):
+        if (float (data[i]['Vmag']) < 1.5):
+            RA = f'{data[i]["RAh"]}h{data[i]["RAm"]}m{data[i]["RAs"]}s'
+            Dec = f'{data[i]["DE-"]}{data[i]["DEd"]}d{data[i]["DEm"]}m{data[i]["DEs"]}s'
+            print (f'{data[i]["HR"]:>5s}  {RA}  {Dec}', \
+                   f' {float (data[i]["Vmag"]):+5.2f}', \
+                   f' {float (data[i]["B-V"]):+5.2f}', \
+                   f' {data[i]["SpType"]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
