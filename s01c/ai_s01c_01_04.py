ab9019e09cc7af5e08b9ac483d1d18e442f06233
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:50:49 +0800

    adding "s01c/ai_s01c_01_04.py" on 15/Sep/2026

--- s01c/ai_s01c_01_04.py
+++ s01c/ai_s01c_01_04.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:38:07 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:34:56 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.ascii
 
-# CSV file name
-file_csv = 'honey-badger/examples/planets/planets.csv'
+# main function
+def main ():
+    # CSV file name
+    file_csv = 'honey-badger/examples/planets/planets.csv'
 
-# reading a CSV file and storing data in an astropy table
-table = astropy.io.ascii.read (file_csv, format='csv')
+    # reading a CSV file and storing data in an astropy table
+    table = astropy.io.ascii.read (file_csv, format='csv')
 
-# printing the information about Jupiter
-mask = (table['Planet'] == 'JUPITER')
-print (f'{table[mask]["Planet", "Number of Moons"]}')
+    # printing the information about Jupiter
+    mask = (table['Planet'] == 'JUPITER')
+    print (f'{table[mask]["Planet", "Number of Moons"]}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
