226540f36c72588beb55819044e7f177b130d0a9
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:53:04 +0800

    adding "s01c/ai_s01c_06_13.py" on 15/Sep/2026

--- s01c/ai_s01c_06_13.py
+++ s01c/ai_s01c_06_13.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:43:26 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:46:39 (UT+08:00) daisuke>
 #
 
 # importing git module
 import git
 
-# URL of repository
-url_repo = 'https://github.com/astrocatalogs/sne-2010-2014.git'
+# main function
+def main ():
+    # URL of repository
+    url_repo = 'https://github.com/astrocatalogs/sne-2010-2014.git'
 
-# directory name of downloaded repository
-dir_repo = 'osc_2010_2014'
+    # directory name of downloaded repository
+    dir_repo = 'osc_2010_2014'
 
-# downloading repository
-repo = git.Repo.clone_from (url_repo, dir_repo)
+    # downloading repository
+    repo = git.Repo.clone_from (url_repo, dir_repo)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
