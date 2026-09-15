9bc86b0c79d479ee555d40dfd5ec603c1cc8661d
Daisuke <mhdf23888@gmail.com>
2026-09-15 08:52:24 +0800

    adding "s01c/ai_s01c_06_01.py" on 15/Sep/2026

--- s01c/ai_s01c_06_01.py
+++ s01c/ai_s01c_06_01.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/04/20 08:41:52 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/15 08:43:04 (UT+08:00) daisuke>
 #
 
 # importing pathlib module
 import pathlib
 
-# list of data files
-files = pathlib.Path ('.').glob ('osc_0000_1989/*.json')
+# main function
+def main ():
+    # list of data files
+    files = pathlib.Path ('.').glob ('osc_0000_1989/*.json')
 
-# printing file names
-for file in sorted (files):
-    print (file)
+    # printing file names
+    for file in sorted (files):
+        print (file)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
