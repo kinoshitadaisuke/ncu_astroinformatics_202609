6fafbff79efbf3b55f44b54a1fd6a0bae06496d3
Daisuke <mhdf23888@gmail.com>
2026-09-21 22:43:33 +0800

    adding "s02a/ai_s02a_01_07.py" on 21/Sep/2026

---
+++ s02a/ai_s02a_01_07.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/22 12:52:47 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+
+# units
+u_micron   = astropy.units.micron
+u_Hz       = astropy.units.Hz
+u_GHz      = astropy.units.GHz
+u_spectral = astropy.units.spectral ()
+
+# wavelength
+wl = 850 * u_micron
+
+# frequency corresponding to EM wave of wavelength 850 micron
+freq     = wl.to (u_Hz, equivalencies=u_spectral)
+freq_GHz = wl.to (u_GHz, equivalencies=u_spectral)
+
+# printing result
+print (f'wavelength = {wl:g}  ==>  frequency = {freq:g} = {freq_GHz:g}')
