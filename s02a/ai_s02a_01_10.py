71e9a656718d91c97b739ab0694646bdbf9bf81e
Daisuke <mhdf23888@gmail.com>
2026-09-21 22:43:43 +0800

    adding "s02a/ai_s02a_01_10.py" on 21/Sep/2026

---
+++ s02a/ai_s02a_01_10.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/22 12:53:09 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+
+# units
+u_nm         = astropy.units.nm
+u_km         = astropy.units.km
+u_sec        = astropy.units.s
+u_km_per_sec = u_km / u_sec
+u_spectral   = astropy.units.spectral ()
+
+# wavelength of H-alpha at rest frame
+wl_rest = 656.28 * u_nm
+
+# observed wavelength of H-alpha
+wl_obs = 656.12 * u_nm
+
+# calculation of velocity
+H_alpha  = astropy.units.doppler_optical (wl_rest)
+velocity = wl_obs.to (u_km_per_sec, equivalencies=H_alpha)
+
+# printing result
+print (f'rest frame wavelength  = {wl_rest}')
+print (f'observed wavelength    = {wl_obs}')
+print (f'line-of-sight velocity = {velocity}')
