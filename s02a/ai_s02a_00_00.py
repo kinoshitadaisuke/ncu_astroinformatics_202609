33669f9cbc74f2b6fd8fa37c48dcc1d8a7176743
Daisuke <mhdf23888@gmail.com>
2026-09-21 22:42:35 +0800

    adding "s02a/ai_s02a_00_00.py" on 21/Sep/2026

---
+++ s02a/ai_s02a_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/22 12:50:39 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.constants
+
+# speeed of light in vacuum
+c = astropy.constants.c
+
+# printing c
+print (c)
