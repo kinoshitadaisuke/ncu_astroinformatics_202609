0ea647462b27e32281971db0af43ea13a32ca388
Daisuke <mhdf23888@gmail.com>
2026-09-21 22:43:29 +0800

    adding "s02a/ai_s02a_01_06.py" on 21/Sep/2026

---
+++ s02a/ai_s02a_01_06.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/22 12:52:41 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+
+# units
+u_cm        = astropy.units.cm
+u_g         = astropy.units.g
+u_g_per_cm3 = u_g / u_cm**3
+
+# density in g/cm^3
+rho_cgi = 3.0 * u_g_per_cm3
+
+# density in kg/m^3
+rho_si = rho_cgi.si
+
+# printing density in SI and in CGS
+print (f'rho = {rho_cgi}')
+print (f'    = {rho_si}')
