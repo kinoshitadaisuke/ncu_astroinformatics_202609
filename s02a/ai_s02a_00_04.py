c8b280c44184c8f8adc68b86e714aef92e78791b
Daisuke <mhdf23888@gmail.com>
2026-09-21 22:42:49 +0800

    adding "s02a/ai_s02a_00_04.py" on 21/Sep/2026

---
+++ s02a/ai_s02a_00_04.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+#
+# Time-stamp: <2026/03/22 12:51:16 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.constants
+
+# gravitational constant
+G = astropy.constants.G
+
+# printing G
+print (G, "\n")
+
+# Boltzmann constant
+k = astropy.constants.k_B
+
+# printing k
+print (k, "\n")
+
+# Stafan-Boltzmann constant
+sigma = astropy.constants.sigma_sb
+
+# printing sigma
+print (sigma, "\n")
