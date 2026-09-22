14f0f51730e76be72153a6bf745fdf5221f6682c
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:52 +0800

    adding "s02a/ai_s02a_01_04.py" on 22/Sep/2026

--- s02a/ai_s02a_01_04.py
+++ s02a/ai_s02a_01_04.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:52:24 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:41:09 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_sec = astropy.units.s
-u_min = astropy.units.min
-u_hr  = astropy.units.h
-u_m   = astropy.units.m
-u_km  = astropy.units.km
+# main function
+def main ():
+    # units
+    u_sec = astropy.units.s
+    u_min = astropy.units.min
+    u_hr  = astropy.units.h
+    u_m   = astropy.units.m
+    u_km  = astropy.units.km
 
-# distance travelled
-d = 180.0 * u_km
+    # distance travelled
+    d = 180.0 * u_km
 
-# time elapsed
-t = 2.0 * u_hr
+    # time elapsed
+    t = 2.0 * u_hr
 
-# velocity
-v = d / t
+    # velocity
+    v = d / t
 
-# unit of metre per sec
-u_m_per_sec = u_m / u_sec
+    # unit of metre per sec
+    u_m_per_sec = u_m / u_sec
 
-# printing distance, time, and velocity
-print (f'distance travelled = {d}')
-print (f'time elapsed       = {t}')
-print (f'average velocity   = {v} = {v.to (u_m_per_sec)}')
+    # printing distance, time, and velocity
+    print (f'distance travelled = {d}')
+    print (f'time elapsed       = {t}')
+    print (f'average velocity   = {v} = {v.to (u_m_per_sec)}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
