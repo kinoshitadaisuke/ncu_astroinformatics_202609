51bda93baa992731aa97e1639e94ac53c5ead9d8
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:08:24 +0800

    adding "s02a/ai_s02a_06_09.py" on 22/Sep/2026

--- s02a/ai_s02a_06_09.py
+++ s02a/ai_s02a_06_09.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/28 20:45:51 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:59:25 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -18,108 +18,114 @@ import astroquery.skyview
 import matplotlib.figure
 import matplotlib.backends.backend_agg
 
-# object name
-object_name = 'M66'
+# main function
+def main ():
+    # object name
+    object_name = 'M66'
 
-# survey name
-survey = 'DSS2 Red'
+    # survey name
+    survey = 'DSS2 Red'
 
-# field-of-view
-fov_arcmin = 15.0
-fov_arcsec = fov_arcmin * 60.0
-npixel     = int (fov_arcsec)
+    # field-of-view
+    fov_arcmin = 15.0
+    fov_arcsec = fov_arcmin * 60.0
+    npixel     = int (fov_arcsec)
 
-# FITS file name
-file_fits = 'm66.fits'
+    # FITS file name
+    file_fits = 'm66.fits'
 
-# PNG file name
-file_png = 'm66.png'
+    # PNG file name
+    file_png = 'm66.png'
 
-# colour map
-cmap = 'bone'
+    # colour map
+    cmap = 'bone'
 
-# resolution in DPI
-resolution_dpi = 225
+    # resolution in DPI
+    resolution_dpi = 225
 
-# units
-u_ha  = astropy.units.hourangle
-u_deg = astropy.units.deg
+    # units
+    u_ha  = astropy.units.hourangle
+    u_deg = astropy.units.deg
 
-# name resolver
-query_result = astroquery.simbad.Simbad.query_object (object_name)
+    # name resolver
+    query_result = astroquery.simbad.Simbad.query_object (object_name)
 
-# coordinate from Simbad
-ra_deg  = query_result['ra'][0]
-dec_deg = query_result['dec'][0]
+    # coordinate from Simbad
+    ra_deg  = query_result['ra'][0]
+    dec_deg = query_result['dec'][0]
 
-# making SkyCoord object of astropy
-coord = astropy.coordinates.SkyCoord (ra_deg, dec_deg, frame='icrs', unit=u_deg)
+    # making SkyCoord object of astropy
+    coord = astropy.coordinates.SkyCoord (ra_deg, dec_deg, frame='icrs', unit=u_deg)
 
-# RA and Dec of coordinate in sexagesimal format
-(ra, dec) = coord.to_string (style='hmsdms').split ()
+    # RA and Dec of coordinate in sexagesimal format
+    (ra, dec) = coord.to_string (style='hmsdms').split ()
 
-# printing result
-print (f'Target name: "{object_name}"')
-print (f'  RA  = {ra:20s} = {coord.ra.deg:10.6f} [deg]')
-print (f'  Dec = {dec:20s} = {coord.dec.deg:+10.6f} [deg]')
+    # printing result
+    print (f'Target name: "{object_name}"')
+    print (f'  RA  = {ra:20s} = {coord.ra.deg:10.6f} [deg]')
+    print (f'  Dec = {dec:20s} = {coord.dec.deg:+10.6f} [deg]')
 
-# clearing astroquery skyview cache
-astroquery.skyview.SkyView.clear_cache ()
+    # clearing astroquery skyview cache
+    astroquery.skyview.SkyView.clear_cache ()
 
-# getting a list of images
-list_image = astroquery.skyview.SkyView.get_image_list (position=coord, \
-                                                        survey=survey)
+    # getting a list of images
+    list_image = astroquery.skyview.SkyView.get_image_list (position=coord, \
+                                                            survey=survey)
 
-# printing list of images found
-print ("images =", list_image)
+    # printing list of images found
+    print ("images =", list_image)
 
-# getting images
-images = astroquery.skyview.SkyView.get_images (position=coord, \
-                                                survey=survey, \
-                                                pixels=npixel)
+    # getting images
+    images = astroquery.skyview.SkyView.get_images (position=coord, \
+                                                    survey=survey, \
+                                                    pixels=npixel)
 
-# image
-image  = images[0]
-header = image[0].header
-data   = image[0].data
+    # image
+    image  = images[0]
+    header = image[0].header
+    data   = image[0].data
 
-# printing information of image
-print (image.info ())
+    # printing information of image
+    print (image.info ())
 
-# printing status
-print (f'Writing a FITS file "{file_fits}"...')
+    # printing status
+    print (f'Writing a FITS file "{file_fits}"...')
 
-# writing FITS file
-hdu = astropy.io.fits.PrimaryHDU (data=data, header=header)
-hdu.writeto (file_fits, overwrite=True)
+    # writing FITS file
+    hdu = astropy.io.fits.PrimaryHDU (data=data, header=header)
+    hdu.writeto (file_fits, overwrite=True)
 
-# printing status
-print (f'Done!')
+    # printing status
+    print (f'Done!')
 
-# opening FITS file
-with astropy.io.fits.open (file_fits) as hdu_list:
-    # printing HDU information
-    print (hdu_list.info ())
-    
-    # reading FITS header, WCS information, and image data
-    header = hdu_list[0].header
-    wcs    = astropy.wcs.WCS (header)
-    image  = hdu_list[0].data
+    # opening FITS file
+    with astropy.io.fits.open (file_fits) as hdu_list:
+        # printing HDU information
+        print (hdu_list.info ())
 
-# making objects "fig" and "ax"
-fig    = matplotlib.figure.Figure ()
-canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
-ax     = fig.add_subplot (111, projection=wcs)
+        # reading FITS header, WCS information, and image data
+        header = hdu_list[0].header
+        wcs    = astropy.wcs.WCS (header)
+        image  = hdu_list[0].data
 
-# axes
-ax.set_title (object_name)
-ax.set_xlabel ('Right Ascension')
-ax.set_ylabel ('Declination')
+    # making objects "fig" and "ax"
+    fig    = matplotlib.figure.Figure ()
+    canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+    ax     = fig.add_subplot (111, projection=wcs)
 
-# plotting image
-im = ax.imshow (image, origin='lower', cmap=cmap)
-fig.colorbar (im)
+    # axes
+    ax.set_title (object_name)
+    ax.set_xlabel ('Right Ascension')
+    ax.set_ylabel ('Declination')
 
-# saving file
-print (f'{file_fits} ==> {file_png}')
-fig.savefig (file_png, dpi=resolution_dpi)
+    # plotting image
+    im = ax.imshow (image, origin='lower', cmap=cmap)
+    fig.colorbar (im)
+
+    # saving file
+    print (f'{file_fits} ==> {file_png}')
+    fig.savefig (file_png, dpi=resolution_dpi)
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
