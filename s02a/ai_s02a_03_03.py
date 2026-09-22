5f6b81e87043f9bfcbd12033ff297caf097655db
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:27 +0800

    adding "s02a/ai_s02a_03_03.py" on 22/Sep/2026

--- s02a/ai_s02a_03_03.py
+++ s02a/ai_s02a_03_03.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:47 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:49:30 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.time
 
-# getting current date/time
-now = astropy.time.Time.now ()
+# main function
+def main ():
+    # getting current date/time
+    now = astropy.time.Time.now ()
 
-# printing date/time
-print (f'now  = {now}')
-print (f'     = JD  {now.jd:14.6f}')
-print (f'     = MJD {now.mjd:14.6f}')
+    # printing date/time
+    print (f'now  = {now}')
+    print (f'     = JD  {now.jd:14.6f}')
+    print (f'     = MJD {now.mjd:14.6f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
