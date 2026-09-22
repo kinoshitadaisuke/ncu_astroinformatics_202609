43a536c17396ea8b371a7da9aa49a3b2cda75b4d
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:07:04 +0800

    adding "s02a/ai_s02a_04_04.py" on 22/Sep/2026

--- s02a/ai_s02a_04_04.py
+++ s02a/ai_s02a_04_04.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/27 21:40:54 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:52:52 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -9,42 +9,48 @@ import astropy.coordinates
 import astropy.time
 import astropy.units
 
-# using "DE440" for solar system ephemeris
-astropy.coordinates.solar_system_ephemeris.set ('de440')
+# main function
+def main ():
+    # using "DE440" for solar system ephemeris
+    astropy.coordinates.solar_system_ephemeris.set ('de440')
 
-# units
-u_m = astropy.units.m
+    # units
+    u_m = astropy.units.m
 
-# date/time in UTC
-t_str = '2026-05-01 04:00:00'
-t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
+    # date/time in UTC
+    t_str = '2026-05-01 04:00:00'
+    t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
 
-# location of observer: NCU main campus
-longitude = '121d11m12s'
-latitude  = '+24d58m12s'
-height    = 151.6 * u_m
-observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
+    # location of observer: NCU main campus
+    longitude = '121d11m12s'
+    latitude  = '+24d58m12s'
+    height    = 151.6 * u_m
+    observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
 
-# getting position of the Sun
-sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
+    # getting position of the Sun
+    sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
 
-# RA and Dec
-(sun_ra, sun_dec) = sun.to_string ('hmsdms').split ()
+    # RA and Dec
+    (sun_ra, sun_dec) = sun.to_string ('hmsdms').split ()
 
-# conversion from equatorial into ecliptic
-ecliptic     = astropy.coordinates.GeocentricMeanEcliptic (obstime=t_utc)
-sun_ecliptic = sun.transform_to (ecliptic)
-sun_lambda   = sun_ecliptic.lon
-sun_beta     = sun_ecliptic.lat
+    # conversion from equatorial into ecliptic
+    ecliptic     = astropy.coordinates.GeocentricMeanEcliptic (obstime=t_utc)
+    sun_ecliptic = sun.transform_to (ecliptic)
+    sun_lambda   = sun_ecliptic.lon
+    sun_beta     = sun_ecliptic.lat
 
-# conversion from equatorial into horizontal
-altaz     = astropy.coordinates.AltAz (obstime=t_utc, location=observer)
-sun_altaz = sun.transform_to (altaz)
-sun_alt   = sun_altaz.alt
-sun_az    = sun_altaz.az
+    # conversion from equatorial into horizontal
+    altaz     = astropy.coordinates.AltAz (obstime=t_utc, location=observer)
+    sun_altaz = sun.transform_to (altaz)
+    sun_alt   = sun_altaz.alt
+    sun_az    = sun_altaz.az
 
-# printing position of the Sun
-print (f'position of the Sun as observed at NCU main campus at {t_utc}:')
-print (f'  (RA, Dec)      = ({sun_ra}, {sun_dec})')
-print (f'  (lambda, beta) = ({sun_lambda}, {sun_beta})')
-print (f'  (az, alt)      = ({sun_az}, {sun_alt})')
+    # printing position of the Sun
+    print (f'position of the Sun as observed at NCU main campus at {t_utc}:')
+    print (f'  (RA, Dec)      = ({sun_ra}, {sun_dec})')
+    print (f'  (lambda, beta) = ({sun_lambda}, {sun_beta})')
+    print (f'  (az, alt)      = ({sun_az}, {sun_alt})')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
