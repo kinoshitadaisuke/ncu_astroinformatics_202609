207f7788c22903bc101dd41579ebf8e2017d8def
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:05:03 +0800

    adding "s02a/ai_s02a_01_07.py" on 22/Sep/2026

--- s02a/ai_s02a_01_07.py
+++ s02a/ai_s02a_01_07.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:52:47 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:42:00 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_micron   = astropy.units.micron
-u_Hz       = astropy.units.Hz
-u_GHz      = astropy.units.GHz
-u_spectral = astropy.units.spectral ()
+# main function
+def main ():
+    # units
+    u_micron   = astropy.units.micron
+    u_Hz       = astropy.units.Hz
+    u_GHz      = astropy.units.GHz
+    u_spectral = astropy.units.spectral ()
 
-# wavelength
-wl = 850 * u_micron
+    # wavelength
+    wl = 850 * u_micron
 
-# frequency corresponding to EM wave of wavelength 850 micron
-freq     = wl.to (u_Hz, equivalencies=u_spectral)
-freq_GHz = wl.to (u_GHz, equivalencies=u_spectral)
+    # frequency corresponding to EM wave of wavelength 850 micron
+    freq     = wl.to (u_Hz, equivalencies=u_spectral)
+    freq_GHz = wl.to (u_GHz, equivalencies=u_spectral)
 
-# printing result
-print (f'wavelength = {wl:g}  ==>  frequency = {freq:g} = {freq_GHz:g}')
+    # printing result
+    print (f'wavelength = {wl:g}  ==>  frequency = {freq:g} = {freq_GHz:g}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
