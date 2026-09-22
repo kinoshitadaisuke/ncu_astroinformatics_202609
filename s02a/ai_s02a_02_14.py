c3ef10970bf8d20fff6489836064c74fbfd61c61
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:13 +0800

    adding "s02a/ai_s02a_02_14.py" on 22/Sep/2026

--- s02a/ai_s02a_02_14.py
+++ s02a/ai_s02a_02_14.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/27 21:14:29 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:48:18 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.table
 
-# VOTable file
-file_vot = 'exoplanet.vot'
+# main function
+def main ():
+    # VOTable file
+    file_vot = 'exoplanet.vot'
 
-# reading VOTable file and making an Astropy table
-table_exoplanet = astropy.table.Table.read (file_vot)
+    # reading VOTable file and making an Astropy table
+    table_exoplanet = astropy.table.Table.read (file_vot)
 
-# printing information of planet discovered by direct imaging in 2025
-mask = (table_exoplanet["detection_type"] == "Imaging") \
-    & (table_exoplanet["discovered"] == 2025)
-print (table_exoplanet[mask]["name", "mass", "semi_major_axis", \
-                             "orbital_period", "detection_type", \
-                             "discovered"])
+    # printing information of planet discovered by direct imaging in 2025
+    mask = (table_exoplanet["detection_type"] == "Imaging") \
+        & (table_exoplanet["discovered"] == 2025)
+    print (table_exoplanet[mask]["name", "mass", "semi_major_axis", \
+                                 "orbital_period", "detection_type", \
+                                 "discovered"])
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
