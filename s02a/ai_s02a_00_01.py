3415ce35a2a25537cbe168f0d81f7486dc5e90f9
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:13 +0800

    adding "s02a/ai_s02a_00_01.py" on 22/Sep/2026

--- s02a/ai_s02a_00_01.py
+++ s02a/ai_s02a_00_01.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:50:50 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:37:30 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 
-# speeed of light in vacuum
-c = astropy.constants.c
+# main function
+def main ():
+    # speeed of light in vacuum
+    c = astropy.constants.c
 
-# calculation
-v = 0.01 * c
+    # calculation
+    v = 0.01 * c
 
-# printing c and v
-print (f'c = {c}')
-print (f'v = 0.01 * {c}')
-print (f'  = {v}')
+    # printing c and v
+    print (f'c = {c}')
+    print (f'v = 0.01 * {c}')
+    print (f'  = {v}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
