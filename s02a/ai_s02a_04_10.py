ac25ef377b294ee7c5564a24dd1cbc73316f0ead
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:07:25 +0800

    adding "s02a/ai_s02a_04_10.py" on 22/Sep/2026

--- s02a/ai_s02a_04_10.py
+++ s02a/ai_s02a_04_10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:57:48 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:54:42 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -9,39 +9,45 @@ import astropy.coordinates
 import astropy.time
 import astropy.units
 
-# using "DE440" for solar system ephemeris
-astropy.coordinates.solar_system_ephemeris.set ('de440')
+# main function
+def main ():
+    # using "DE440" for solar system ephemeris
+    astropy.coordinates.solar_system_ephemeris.set ('de440')
 
-# units
-u_m = astropy.units.m
+    # units
+    u_m = astropy.units.m
 
-# date/time in UTC
-t_str = '2028-12-31 22:03:00'
-t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
+    # date/time in UTC
+    t_str = '2028-12-31 22:03:00'
+    t_utc = astropy.time.Time (t_str, format='iso', scale='utc')
 
-# location of observer: NCU main campus
-longitude = '121d11m12s'
-latitude  = '+24d58m12s'
-height    = 151.6 * u_m
-observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
+    # location of observer: NCU main campus
+    longitude = '121d11m12s'
+    latitude  = '+24d58m12s'
+    height    = 151.6 * u_m
+    observer  = astropy.coordinates.EarthLocation (longitude, latitude, height)
 
-# getting position of the Sun
-sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
+    # getting position of the Sun
+    sun = astropy.coordinates.get_body ('sun', t_utc, location=observer)
 
-# getting position of the Moon
-moon = astropy.coordinates.get_body ('moon', t_utc, location=observer)
+    # getting position of the Moon
+    moon = astropy.coordinates.get_body ('moon', t_utc, location=observer)
 
-# RA and Dec
-(sun_ra, sun_dec)   = sun.to_string ('hmsdms').split ()
-(moon_ra, moon_dec) = moon.to_string ('hmsdms').split ()
+    # RA and Dec
+    (sun_ra, sun_dec)   = sun.to_string ('hmsdms').split ()
+    (moon_ra, moon_dec) = moon.to_string ('hmsdms').split ()
 
-# calculation of angular distance
-separation = sun.separation (moon)
+    # calculation of angular distance
+    separation = sun.separation (moon)
 
-# printing result of calculation
-print (f'Position of the Sun:')
-print (f'  (RA, Dec) = ({sun_ra}, {sun_dec})')
-print (f'Position of the Moon:')
-print (f'  (RA, Dec) = ({moon_ra}, {moon_dec})')
-print (f'Angular distance between the Sun and the Moon on {t_utc}')
-print (f'  separation = {separation}')
+    # printing result of calculation
+    print (f'Position of the Sun:')
+    print (f'  (RA, Dec) = ({sun_ra}, {sun_dec})')
+    print (f'Position of the Moon:')
+    print (f'  (RA, Dec) = ({moon_ra}, {moon_dec})')
+    print (f'Angular distance between the Sun and the Moon on {t_utc}')
+    print (f'  separation = {separation}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
