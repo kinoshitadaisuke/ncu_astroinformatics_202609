518c74b2a91bef91c56ca36f106bf80685cb0381
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:20 +0800

    adding "s02a/ai_s02a_00_03.py" on 22/Sep/2026

--- s02a/ai_s02a_00_03.py
+++ s02a/ai_s02a_00_03.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:08 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:38:05 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 import astropy.units
 
-# speeed of light in vacuum
-c = astropy.constants.c
+# main function
+def main ():
+    # speeed of light in vacuum
+    c = astropy.constants.c
 
-# calculation
-v = 0.01 * c
+    # calculation
+    v = 0.01 * c
 
-# units of "km" and "sec"
-unit_km         = astropy.units.km
-unit_sec        = astropy.units.s
+    # units of "km" and "sec"
+    unit_km         = astropy.units.km
+    unit_sec        = astropy.units.s
 
-# creating a unit "km/sec"
-unit_km_per_sec = unit_km / unit_sec
+    # creating a unit "km/sec"
+    unit_km_per_sec = unit_km / unit_sec
 
-# conversion of unit
-v2 = v.to (unit_km_per_sec)
+    # conversion of unit
+    v2 = v.to (unit_km_per_sec)
 
-# printing c, v, and v2
-print (f'c = {c}')
-print (f'v = 0.01 * {c}')
-print (f'  = {v}')
-print (f'  = {v2}')
+    # printing c, v, and v2
+    print (f'c = {c}')
+    print (f'v = 0.01 * {c}')
+    print (f'  = {v}')
+    print (f'  = {v2}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
