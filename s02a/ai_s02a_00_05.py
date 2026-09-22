12e3fc392af07bcb95e6b1fa40ff33efb7eabaa0
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:26 +0800

    adding "s02a/ai_s02a_00_05.py" on 22/Sep/2026

--- s02a/ai_s02a_00_05.py
+++ s02a/ai_s02a_00_05.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:22 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:38:40 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 
-# astronomical unit
-au = astropy.constants.au
+# main function
+def main ():
+    # astronomical unit
+    au = astropy.constants.au
 
-# printing au
-print (au, "\n")
+    # printing au
+    print (au, "\n")
 
-# parsec
-pc = astropy.constants.pc
+    # parsec
+    pc = astropy.constants.pc
 
-# km/s
-unit_km = astropy.units.km
+    # km/s
+    unit_km = astropy.units.km
 
-# printing pc
-print (pc, "\n")
+    # printing pc
+    print (pc, "\n")
 
-# 1 au
-print (f'1 au = {au:g}')
-print (f'     = {au.to (unit_km):g}')
+    # 1 au
+    print (f'1 au = {au:g}')
+    print (f'     = {au.to (unit_km):g}')
 
-# 1 pc
-print (f'1 pc = {pc:g}')
-print (f'     = {pc / au} au')
+    # 1 pc
+    print (f'1 pc = {pc:g}')
+    print (f'     = {pc / au} au')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
