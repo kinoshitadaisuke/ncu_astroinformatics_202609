e1dec4fd3591f57021348ae7a55551d19fb716ec
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:23 +0800

    adding "s02a/ai_s02a_03_02.py" on 22/Sep/2026

--- s02a/ai_s02a_03_02.py
+++ s02a/ai_s02a_03_02.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:42 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:49:14 (UT+08:00) daisuke>
 #
 
 # importing datetime module
@@ -10,13 +10,19 @@ import datetime
 # importing astropy module
 import astropy.time
 
-# getting current date/time using datetime module
-now_datetime = datetime.datetime.now (tz=datetime.timezone.utc)
+# main function
+def main ():
+    # getting current date/time using datetime module
+    now_datetime = datetime.datetime.now (tz=datetime.timezone.utc)
 
-# constructing Astropy's Time object from a string
-now = astropy.time.Time (now_datetime, scale='utc')
+    # constructing Astropy's Time object from a string
+    now = astropy.time.Time (now_datetime, scale='utc')
 
-# printing date/time
-print (f'now  = {now}')
-print (f'     = JD  {now.jd:14.6f}')
-print (f'     = MJD {now.mjd:14.6f}')
+    # printing date/time
+    print (f'now  = {now}')
+    print (f'     = JD  {now.jd:14.6f}')
+    print (f'     = MJD {now.mjd:14.6f}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
