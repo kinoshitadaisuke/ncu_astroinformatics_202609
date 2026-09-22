e2e0f832a5babb4de528c40addace36200a2bf69
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:33 +0800

    adding "s02a/ai_s02a_00_07.py" on 22/Sep/2026

--- s02a/ai_s02a_00_07.py
+++ s02a/ai_s02a_00_07.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:37 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:39:17 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 
-# Solar radius
-R_S = astropy.constants.R_sun
+# main function
+def main ():
+    # Solar radius
+    R_S = astropy.constants.R_sun
 
-# Jupiter radius
-R_J = astropy.constants.R_jup
+    # Jupiter radius
+    R_J = astropy.constants.R_jup
 
-# Earth radius
-R_E = astropy.constants.R_earth
+    # Earth radius
+    R_E = astropy.constants.R_earth
 
-# printing Solar radius, Jupiter radius, and Earth radius
-print (R_S, "\n")
-print (R_J, "\n")
-print (R_E, "\n")
+    # printing Solar radius, Jupiter radius, and Earth radius
+    print (R_S, "\n")
+    print (R_J, "\n")
+    print (R_E, "\n")
 
-# value of 1 Earth radius in the unit of Solar radius
-print (f'1 R_E = {R_E:g}')
-print (f'      = {R_E / R_S:g} R_S')
+    # value of 1 Earth radius in the unit of Solar radius
+    print (f'1 R_E = {R_E:g}')
+    print (f'      = {R_E / R_S:g} R_S')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
