f914c4693282cf5f39acb87e1969e25990c95733
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:36 +0800

    adding "s02a/ai_s02a_00_08.py" on 22/Sep/2026

--- s02a/ai_s02a_00_08.py
+++ s02a/ai_s02a_00_08.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:44 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:39:36 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 
-# Solar luminosity
-L_S = astropy.constants.L_sun
+# main function
+def main ():
+    # Solar luminosity
+    L_S = astropy.constants.L_sun
 
-# printing Solar radius, Jupiter radius, and Earth radius
-print (L_S, "\n")
+    # printing Solar radius, Jupiter radius, and Earth radius
+    print (L_S, "\n")
 
-# amount of 10,000 Solar luminosity
-print (f'    1 L_S = {L_S:g}')
-print (f'10000 L_S = {10000 * L_S:g}')
+    # amount of 10,000 Solar luminosity
+    print (f'    1 L_S = {L_S:g}')
+    print (f'10000 L_S = {10000 * L_S:g}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
