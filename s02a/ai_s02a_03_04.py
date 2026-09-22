8e8c50efa8fb0d2fd35524545ea37748b10084ba
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:30 +0800

    adding "s02a/ai_s02a_03_04.py" on 22/Sep/2026

--- s02a/ai_s02a_03_04.py
+++ s02a/ai_s02a_03_04.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:54 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:49:46 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.time
 
-# getting current date/time
-now = astropy.time.Time.now ()
+# main function
+def main ():
+    # getting current date/time
+    now = astropy.time.Time.now ()
 
-# date/time of J2000
-j2000 = astropy.time.Time ('2000-01-01 12:00:00', format='iso', scale='utc')
+    # date/time of J2000
+    j2000 = astropy.time.Time ('2000-01-01 12:00:00', format='iso', scale='utc')
 
-# time between J2000 and now
-delta_t = now - j2000
+    # time between J2000 and now
+    delta_t = now - j2000
 
-# printing date/time
-print (f'j2000       = {j2000}')
-print (f'now         = {now}')
-print (f'now - j2000 = {delta_t} day = {delta_t.sec:g} sec')
+    # printing date/time
+    print (f'j2000       = {j2000}')
+    print (f'now         = {now}')
+    print (f'now - j2000 = {delta_t} day = {delta_t.sec:g} sec')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
