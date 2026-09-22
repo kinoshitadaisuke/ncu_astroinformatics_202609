d003885a7965960d50c7778297ce88b3fa3ef596
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:03 +0800

    adding "s02a/ai_s02a_02_11.py" on 22/Sep/2026

--- s02a/ai_s02a_02_11.py
+++ s02a/ai_s02a_02_11.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:00 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:47:25 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.table
 
-# VOTable file
-file_vot = 'exoplanet.vot'
+# main function
+def main ():
+    # VOTable file
+    file_vot = 'exoplanet.vot'
 
-# reading VOTable file and making an Astropy table
-table_exoplanet = astropy.table.Table.read (file_vot)
+    # reading VOTable file and making an Astropy table
+    table_exoplanet = astropy.table.Table.read (file_vot)
 
-# printing information of planet "51 Peg b"
-for i in range (len (table_exoplanet)):
-    if (table_exoplanet[i]["name"] == "51 Peg b"):
-        print (table_exoplanet[i]["name", "mass", "semi_major_axis", \
-                                  "orbital_period", "detection_type", \
-                                  "discovered"])
+    # printing information of planet "51 Peg b"
+    for i in range (len (table_exoplanet)):
+        if (table_exoplanet[i]["name"] == "51 Peg b"):
+            print (table_exoplanet[i]["name", "mass", "semi_major_axis", \
+                                      "orbital_period", "detection_type", \
+                                      "discovered"])
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
