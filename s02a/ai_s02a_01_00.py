3cd2f67dee893a3a8c95b22e85eec70b3859886d
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:39 +0800

    adding "s02a/ai_s02a_01_00.py" on 22/Sep/2026

--- s02a/ai_s02a_01_00.py
+++ s02a/ai_s02a_01_00.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:56 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:39:57 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_sec = astropy.units.s
+# main function
+def main ():
+    # units
+    u_sec = astropy.units.s
 
-# a quantity object of 900.0 sec
-t = 900.0 * u_sec
+    # a quantity object of 900.0 sec
+    t = 900.0 * u_sec
 
-# printing t
-print (f't = {t}')
+    # printing t
+    print (f't = {t}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
