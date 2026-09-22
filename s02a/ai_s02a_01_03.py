02d8b1ca513c52d89ff0fd8b158d60100c7632de
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:49 +0800

    adding "s02a/ai_s02a_01_03.py" on 22/Sep/2026

--- s02a/ai_s02a_01_03.py
+++ s02a/ai_s02a_01_03.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:52:17 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:40:53 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_sec = astropy.units.s
-u_min = astropy.units.min
-u_hr  = astropy.units.h
+# main function
+def main ():
+    # units
+    u_sec = astropy.units.s
+    u_min = astropy.units.min
+    u_hr  = astropy.units.h
 
-# t1 = 3600 sec
-t1 = 3600.0 * u_sec
+    # t1 = 3600 sec
+    t1 = 3600.0 * u_sec
 
-# t2 = 900 sec
-t2 = 900.0 * u_sec
+    # t2 = 900 sec
+    t2 = 900.0 * u_sec
 
-# calculation of t3 = t1 - t2
-t3 = t1 - t2
+    # calculation of t3 = t1 - t2
+    t3 = t1 - t2
 
-# conversion of unit
-t4 = t3.to (u_min)
-t5 = t3.to (u_hr)
+    # conversion of unit
+    t4 = t3.to (u_min)
+    t5 = t3.to (u_hr)
 
-# printing t1, t2, and t3
-print (f't1 = {t1}')
-print (f't2 = {t2}')
-print (f't3 = t1 - t2 = {t3} = {t4} = {t5}')
+    # printing t1, t2, and t3
+    print (f't1 = {t1}')
+    print (f't2 = {t2}')
+    print (f't3 = t1 - t2 = {t3} = {t4} = {t5}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
