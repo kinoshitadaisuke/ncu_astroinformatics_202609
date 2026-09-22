0c85a81e8cea5d41e9a996db250b283052c0ea1a
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:08:05 +0800

    adding "s02a/ai_s02a_06_04.py" on 22/Sep/2026

--- s02a/ai_s02a_06_04.py
+++ s02a/ai_s02a_06_04.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:59:04 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:57:50 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.io.fits
 
-# input file name
-file_input = 'm3.fits'
+# main function
+def main ():
+    # input file name
+    file_input = 'm3.fits'
 
-# opening FITS file
-with astropy.io.fits.open (file_input) as hdu_list:
-    # printing HDU information
-    print (hdu_list.info ())
+    # opening FITS file
+    with astropy.io.fits.open (file_input) as hdu_list:
+        # printing HDU information
+        print (hdu_list.info ())
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
