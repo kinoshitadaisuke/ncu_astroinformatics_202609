2a17dff9fd6c47501afbcaadd669bd6a7415b046
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:46 +0800

    adding "s02a/ai_s02a_01_02.py" on 22/Sep/2026

--- s02a/ai_s02a_01_02.py
+++ s02a/ai_s02a_01_02.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:52:10 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:40:35 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_sec = astropy.units.s
+# main function
+def main ():
+    # units
+    u_sec = astropy.units.s
 
-# t1 = 3600 sec
-t1 = 3600.0 * u_sec
+    # t1 = 3600 sec
+    t1 = 3600.0 * u_sec
 
-# t2 = 900 sec
-t2 = 900.0 * u_sec
+    # t2 = 900 sec
+    t2 = 900.0 * u_sec
 
-# calculation of t3 = t1 - t2
-t3 = t1 - t2
+    # calculation of t3 = t1 - t2
+    t3 = t1 - t2
 
-# printing t1, t2, and t3
-print (f't1 = {t1}')
-print (f't2 = {t2}')
-print (f't3 = t1 - t2 = {t3}')
+    # printing t1, t2, and t3
+    print (f't1 = {t1}')
+    print (f't2 = {t2}')
+    print (f't3 = t1 - t2 = {t3}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
