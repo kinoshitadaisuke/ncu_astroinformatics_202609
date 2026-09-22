b9167cce4dbcce61d94c7f0ef93292e4efcd37aa
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:04:29 +0800

    adding "s02a/ai_s02a_00_06.py" on 22/Sep/2026

--- s02a/ai_s02a_00_06.py
+++ s02a/ai_s02a_00_06.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:51:30 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:38:58 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.constants
 
-# Solar mass
-M_S = astropy.constants.M_sun
+# main function
+def main ():
+    # Solar mass
+    M_S = astropy.constants.M_sun
 
-# Jupiter mass
-M_J = astropy.constants.M_jup
+    # Jupiter mass
+    M_J = astropy.constants.M_jup
 
-# Earth mass
-M_E = astropy.constants.M_earth
+    # Earth mass
+    M_E = astropy.constants.M_earth
 
-# printing Solar mass, Jupiter mass, and Earth mass
-print (M_S, "\n")
-print (M_J, "\n")
-print (M_E, "\n")
+    # printing Solar mass, Jupiter mass, and Earth mass
+    print (M_S, "\n")
+    print (M_J, "\n")
+    print (M_E, "\n")
 
-# value of Jupiter mass in the unit of Solar mass and Earth mass
-print (f'1 M_J = {M_J}')
-print (f'      = {M_J / M_S:g} M_S')
-print (f'      = {M_J / M_E:g} M_E')
+    # value of Jupiter mass in the unit of Solar mass and Earth mass
+    print (f'1 M_J = {M_J}')
+    print (f'      = {M_J / M_S:g} M_S')
+    print (f'      = {M_J / M_E:g} M_E')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
