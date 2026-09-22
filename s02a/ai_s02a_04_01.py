307646b636684a6e1b4e88dad3bb10635aa02878
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:54 +0800

    adding "s02a/ai_s02a_04_01.py" on 22/Sep/2026

--- s02a/ai_s02a_04_01.py
+++ s02a/ai_s02a_04_01.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:56:45 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:51:57 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.coordinates
 import astropy.units
 
-# units
-u_ha  = astropy.units.hourangle
-u_deg = astropy.units.degree
+# main function
+def main ():
+    # units
+    u_ha  = astropy.units.hourangle
+    u_deg = astropy.units.degree
 
-# coordinate of Sirius
-sirius_ra  = '06:45:08.9'
-sirius_dec = '-16:42:58'
+    # coordinate of Sirius
+    sirius_ra  = '06:45:08.9'
+    sirius_dec = '-16:42:58'
 
-# making a SkyCoord object
-coord = astropy.coordinates.SkyCoord (ra=sirius_ra, dec=sirius_dec, \
-                                      unit=(u_ha, u_deg), frame='icrs')
+    # making a SkyCoord object
+    coord = astropy.coordinates.SkyCoord (ra=sirius_ra, dec=sirius_dec, \
+                                          unit=(u_ha, u_deg), frame='icrs')
 
-# printing coordinate
-print (f'Coordinate of Sirius:')
-print (f'  (RA, Dec) = ({sirius_ra}, {sirius_dec})')
-print (f'            = ({coord.ra}, {coord.dec})')
-print (f'            = ({int (coord.ra.hms.h):02d}', \
-       f':{int (coord.ra.hms.m):02d}:{coord.ra.hms.s:06.3f}, ', \
-       f'{int (coord.dec.dms.d):02d}:{abs (int (coord.dec.dms.m)):02d}:', \
-       f'{abs (coord.dec.dms.s):06.3f})', sep='')
-print (f'            = ({coord.to_string ("decimal")} deg)')
-print (f'            = ({coord.to_string ("hmsdms")})')
+    # printing coordinate
+    print (f'Coordinate of Sirius:')
+    print (f'  (RA, Dec) = ({sirius_ra}, {sirius_dec})')
+    print (f'            = ({coord.ra}, {coord.dec})')
+    print (f'            = ({int (coord.ra.hms.h):02d}', \
+           f':{int (coord.ra.hms.m):02d}:{coord.ra.hms.s:06.3f}, ', \
+           f'{int (coord.dec.dms.d):02d}:{abs (int (coord.dec.dms.m)):02d}:', \
+           f'{abs (coord.dec.dms.s):06.3f})', sep='')
+    print (f'            = ({coord.to_string ("decimal")} deg)')
+    print (f'            = ({coord.to_string ("hmsdms")})')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
