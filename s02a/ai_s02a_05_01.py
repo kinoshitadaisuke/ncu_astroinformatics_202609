8d4804a922bc4bb2c8969e2cc65e6058ca040881
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:07:38 +0800

    adding "s02a/ai_s02a_05_01.py" on 22/Sep/2026

--- s02a/ai_s02a_05_01.py
+++ s02a/ai_s02a_05_01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:58:04 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:55:22 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -10,39 +10,45 @@ import numpy
 # importing astropy module
 import astropy.stats
 
-# parameters for random number generation
-mean   = 1000.0
-stddev = 30.0
-n      = 10**4
+# main function
+def main ():
+    # parameters for random number generation
+    mean   = 1000.0
+    stddev = 30.0
+    n      = 10**4
 
-# generation of a set of random number of Gaussian distribution
-rng  = numpy.random.default_rng ()
-data = rng.normal (loc=mean, scale=stddev, size=n)
+    # generation of a set of random number of Gaussian distribution
+    rng  = numpy.random.default_rng ()
+    data = rng.normal (loc=mean, scale=stddev, size=n)
 
-# printing generated data
-print (f'Genearated random numbers:')
-print (f'{data}')
-print (f'Number of random numbers generated: {len (data)}')
+    # printing generated data
+    print (f'Genearated random numbers:')
+    print (f'{data}')
+    print (f'Number of random numbers generated: {len (data)}')
 
-# printing status
-print (f'Now, adding some outliers to the dataset...')
+    # printing status
+    print (f'Now, adding some outliers to the dataset...')
 
-# adding some outliers
-data[0]    +=  2000.0
-data[1000] +=  3000.0
-data[2000] +=  4000.0
-data[3000] +=  5000.0
-data[4000] +=  6000.0
-data[5000] +=  7000.0
-data[6000] +=  8000.0
-data[7000] +=  9000.0
-data[8000] += 10000.0
-data[9000] += 11000.0
+    # adding some outliers
+    data[0]    +=  2000.0
+    data[1000] +=  3000.0
+    data[2000] +=  4000.0
+    data[3000] +=  5000.0
+    data[4000] +=  6000.0
+    data[5000] +=  7000.0
+    data[6000] +=  8000.0
+    data[7000] +=  9000.0
+    data[8000] += 10000.0
+    data[9000] += 11000.0
 
-# printing status
-print (f'Finished adding some outliers to the dataset!')
+    # printing status
+    print (f'Finished adding some outliers to the dataset!')
 
-# printing data with some outliers
-print (f'After adding some outliers:')
-print (f'{data}')
-print (f'Number of random numbers generated: {len (data)}')
+    # printing data with some outliers
+    print (f'After adding some outliers:')
+    print (f'{data}')
+    print (f'Number of random numbers generated: {len (data)}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
