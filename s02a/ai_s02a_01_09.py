1e49225007c136ddb2cf78804a7c91344a319c89
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:05:09 +0800

    adding "s02a/ai_s02a_01_09.py" on 22/Sep/2026

--- s02a/ai_s02a_01_09.py
+++ s02a/ai_s02a_01_09.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:53:01 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:42:37 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.units
 
-# units
-u_AA       = astropy.units.AA
-u_eV       = astropy.units.eV
-u_keV      = astropy.units.keV
-u_spectral = astropy.units.spectral ()
+# main function
+def main ():
+    # units
+    u_AA       = astropy.units.AA
+    u_eV       = astropy.units.eV
+    u_keV      = astropy.units.keV
+    u_spectral = astropy.units.spectral ()
 
-# wavelength
-wl = 1.0 * u_AA
+    # wavelength
+    wl = 1.0 * u_AA
 
-# energy of a photon corresponding to wavelength of 10 AA
-energy_eV  = wl.to (u_eV, equivalencies=u_spectral)
-energy_keV = wl.to (u_keV, equivalencies=u_spectral)
+    # energy of a photon corresponding to wavelength of 10 AA
+    energy_eV  = wl.to (u_eV, equivalencies=u_spectral)
+    energy_keV = wl.to (u_keV, equivalencies=u_spectral)
 
-# printing result
-print (f'wavelength = {wl:g}  ==>  energy = {energy_eV:g} = {energy_keV:g}')
+    # printing result
+    print (f'wavelength = {wl:g}  ==>  energy = {energy_eV:g} = {energy_keV:g}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
