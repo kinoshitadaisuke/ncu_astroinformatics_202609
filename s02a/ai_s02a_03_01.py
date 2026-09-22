1dd3e8b2b6ee79316d6c19a1f187d2849ccbca7d
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:20 +0800

    adding "s02a/ai_s02a_03_01.py" on 22/Sep/2026

--- s02a/ai_s02a_03_01.py
+++ s02a/ai_s02a_03_01.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:55:36 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:48:56 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.time
 
-# date/time in UT as a string
-time_str = '2026-01-01T12:00:00'
+# main function
+def main ():
+    # date/time in UT as a string
+    time_str = '2026-01-01T12:00:00'
 
-# constructing Astropy's Time object from a string
-time = astropy.time.Time (time_str, format='isot', scale='utc')
+    # constructing Astropy's Time object from a string
+    time = astropy.time.Time (time_str, format='isot', scale='utc')
 
-# calculating JD and MJD
-time_jd  = time.jd
-time_mjd = time.mjd
+    # calculating JD and MJD
+    time_jd  = time.jd
+    time_mjd = time.mjd
 
-# printing JD and MJD
-print (f'{time} (UT) = JD {time_jd} = MJD {time_mjd}')
+    # printing JD and MJD
+    print (f'{time} (UT) = JD {time_jd} = MJD {time_mjd}')
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
