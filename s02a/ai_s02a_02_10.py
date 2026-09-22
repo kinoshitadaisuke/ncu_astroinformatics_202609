ef97f00b3bef82ffac35a2e7710782e7dd011c9f
Daisuke <mhdf23888@gmail.com>
2026-09-22 14:06:00 +0800

    adding "s02a/ai_s02a_02_10.py" on 22/Sep/2026

--- s02a/ai_s02a_02_10.py
+++ s02a/ai_s02a_02_10.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 #
-# Time-stamp: <2026/03/22 12:54:54 (UT+08:00) daisuke>
+# Time-stamp: <2026/09/22 13:47:07 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.table
 
-# VOTable file
-file_vot = 'exoplanet.vot'
+# main function
+def main ():
+    # VOTable file
+    file_vot = 'exoplanet.vot'
 
-# reading VOTable file and making an Astropy table
-table_exoplanet = astropy.table.Table.read (file_vot)
+    # reading VOTable file and making an Astropy table
+    table_exoplanet = astropy.table.Table.read (file_vot)
 
-# printing name, mass, semimajor axis, orbital period, detection type,
-# and year of discovery of exoplanets
-print (table_exoplanet["name", "mass", "semi_major_axis", \
-                       "orbital_period", "detection_type", "discovered"])
+    # printing name, mass, semimajor axis, orbital period, detection type,
+    # and year of discovery of exoplanets
+    print (table_exoplanet["name", "mass", "semi_major_axis", \
+                           "orbital_period", "detection_type", "discovered"])
+
+# execution of main function
+if (__name__ == '__main__'):
+    main ()
